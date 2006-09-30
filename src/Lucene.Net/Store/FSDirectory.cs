8c1298d9acc119acaa77af03cde01dfaaab69d94
George Aroush <aroush@apache.org>
2006-09-30 03:24:48 +0000

    Lucene.Net.Index.Compression.SharpZipLibAdapter -- provide full name-space to ICompressionAdapter Lucene.Net.Store.FSDirectory.RenameFile() -- added a missing while loop Lucene.Net.Store.MultiSearcher.CreateWeight() -- wasn't iterating over the term keys correctly Lucene.Net.Search.Query.Combine() -- wasn't checking for the right key before adding
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/tags/Lucene.Net_1_9_RC1_final@451524 13f79535-47bb-0310-9956-ffa450edef68

--- src/Lucene.Net/Store/FSDirectory.cs
+++ src/Lucene.Net/Store/FSDirectory.cs
@@ -482,8 +482,10 @@ namespace Lucene.Net.Store
 							buffer = new byte[1024];
 						}
 						int len;
-                        len = in_Renamed.Read(buffer, 0, buffer.Length);
-                        out_Renamed.Write(buffer, 0, len);
+						while ((len = in_Renamed.Read(buffer, 0, buffer.Length)) > 0) 
+						{ 
+							out_Renamed.Write(buffer, 0, len); 
+						}
 						
 						// delete the old file.
 						bool tmpBool3;
