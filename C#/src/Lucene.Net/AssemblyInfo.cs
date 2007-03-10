2c76b38539b3fdc01c3f5d7d891e925e6447199f
George Aroush <aroush@apache.org>
2007-03-10 15:59:51 +0000

    Release of Apache Lucene.Net.2.0 build 004 "final".
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk@516738 13f79535-47bb-0310-9956-ffa450edef68

--- C#/src/Lucene.Net/AssemblyInfo.cs
+++ C#/src/Lucene.Net/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.0.003")]
+[assembly: AssemblyVersion("2.0.0.004")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
