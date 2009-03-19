211ee83354fc7566d29dbeef29983d7e75a27ad0
Isik Yigit <digy@apache.org>
2009-03-19 17:59:24 +0000

    AssemblyInfo files 2.3.1.003
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk/C#@756114 13f79535-47bb-0310-9956-ffa450edef68

--- src/Test/AssemblyInfo.cs
+++ src/Test/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.3.1.002")]
+[assembly: AssemblyVersion("2.3.1.003")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
