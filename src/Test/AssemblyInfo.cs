ee87edf2e9186351e3078edfeac9182f15837750
Isik Yigit <digy@apache.org>
2009-03-21 12:57:20 +0000

    Test/AssemblyInfo.cs 2.3.1 --> 2.3.2
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk/C#@756930 13f79535-47bb-0310-9956-ffa450edef68

--- src/Test/AssemblyInfo.cs
+++ src/Test/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDefaultAlias("Lucene.Net")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyInformationalVersionAttribute("2.3.1")]
+[assembly: AssemblyInformationalVersionAttribute("2.3.2")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.3.1.003")]
+[assembly: AssemblyVersion("2.3.2.001")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
