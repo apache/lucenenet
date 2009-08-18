b1dd339da9a59a4a27b2eff05796708a128ccb93
Isik Yigit <digy@apache.org>
2009-08-18 22:53:51 +0000

    New Version 2.4.0.002
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk/C#@805617 13f79535-47bb-0310-9956-ffa450edef68

--- src/Test/AssemblyInfo.cs
+++ src/Test/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.4.0.001")]
+[assembly: AssemblyVersion("2.4.0.002")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
