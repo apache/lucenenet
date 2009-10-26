bac05c3bd801c670487f4c3d5efa6c872e4654f6
George Aroush <aroush@apache.org>
2009-10-26 01:57:00 +0000

    Lucene.Net 2.4.0
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk@829685 13f79535-47bb-0310-9956-ffa450edef68

--- C#/src/Lucene.Net/AssemblyInfo.cs
+++ C#/src/Lucene.Net/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDefaultAlias("Lucene.Net")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyInformationalVersionAttribute("2.4.1")]
+[assembly: AssemblyInformationalVersionAttribute("2.4.0")]
 
 
 //
@@ -47,7 +47,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.4.1.002")]
+[assembly: AssemblyVersion("2.4.0.002")]
 
 
 //
--- C#/src/Test/AssemblyInfo.cs
+++ C#/src/Test/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDefaultAlias("Lucene.Net")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyInformationalVersionAttribute("2.4.1")]
+[assembly: AssemblyInformationalVersionAttribute("2.4.0")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.4.1.002")]
+[assembly: AssemblyVersion("2.4.0.002")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
