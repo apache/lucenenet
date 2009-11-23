8f0f207dd763bf6ae874e5b786411abef223d9aa
George Aroush <aroush@apache.org>
2009-11-23 13:09:10 +0000

    LUCENENET-301: Tests on TestTimeLimitedCollector fail
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk@883332 13f79535-47bb-0310-9956-ffa450edef68

--- C#/src/Test/Search/TestTimeLimitedCollector.cs
+++ C#/src/Test/Search/TestTimeLimitedCollector.cs
@@ -377,6 +377,7 @@ namespace Lucene.Net.Search
 					}
 				}
 				System.Diagnostics.Debug.Assert(docId >= 0, "doc=" + docId);
+                bits.Length = Math.Max(bits.Length, docId + 1);
 				bits.Set(docId, true);
 				lastDocCollected = docId;
 			}
