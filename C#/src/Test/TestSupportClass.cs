a5414ab5e9b8439ebd888335a2f1bddeb5e0b6f9
Isik Yigit <digy@apache.org>
2009-12-07 19:43:32 +0000

    Since SimpleLRUCache is not a direct port from Lucene.Java, I add another test case which I used in testing LUCENENET-190.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk@888107 13f79535-47bb-0310-9956-ffa450edef68

--- C#/src/Test/TestSupportClass.cs
+++ C#/src/Test/TestSupportClass.cs
@@ -718,4 +718,26 @@ namespace Lucene.Net._SupportClass
             }
         }
     }
+
+    [TestFixture]
+    public class TestLRUCache
+    {
+        [Test]
+        public void Test()
+        {
+            Lucene.Net.Util.Cache.SimpleLRUCache cache = new Lucene.Net.Util.Cache.SimpleLRUCache(3);
+            cache.Put("a", "a");
+            cache.Put("b", "b");
+            cache.Put("c", "c");
+            Assert.IsNotNull(cache.Get("a"));
+            Assert.IsNotNull(cache.Get("b"));
+            Assert.IsNotNull(cache.Get("c"));
+            cache.Put("d", "d");
+            Assert.IsNull(cache.Get("a"));
+            Assert.IsNotNull(cache.Get("c"));
+            cache.Put("e", "e");
+            cache.Put("f", "f");
+            Assert.IsNotNull(cache.Get("c"));
+        }
+    }
 }
