96f671293d45e0e12362f6f0ba8572882222ac27
Isik Yigit <digy@apache.org>
2010-05-30 14:20:28 +0000

    LUCENENET-366 Spellchecker issues (SpellChecker 2.9.2)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/lucene.net/trunk@949519 13f79535-47bb-0310-9956-ffa450edef68

--- C#/contrib/SpellChecker.Net/SpellChecker.Net/Spell/StringDistance.cs
+++ C#/contrib/SpellChecker.Net/SpellChecker.Net/Spell/StringDistance.cs
@@ -1,13 +1,13 @@
-/* 
+/**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,20 +16,25 @@
  */
 
 using System;
-
-// org.apache.lucene.index.Term   -> Lucene.Net.Index.Term
-// org.apache.lucene.search.spell -> Lucene.Net.Search.Spell
-
+using System.Collections.Generic;
+using System.Text;
 
 namespace SpellChecker.Net.Search.Spell
 {
-	
-    /// <summary> A simple interface representing a Dictionary</summary>
-    public interface Dictionary
+    /// <summary>
+    /// Interface for string distances.
+    /// </summary>
+    public interface StringDistance
     {
-        /// <summary> return all the words present in the dictionary</summary>
-        /// <returns> Iterator
-        /// </returns>
-        System.Collections.IEnumerator GetWordsIterator();
+        /// <summary>
+        /// Returns a float between 0 and 1 based on how similar the specified strings are to one another.  
+        /// Returning a value of 1 means the specified strings are identical and 0 means the
+        /// string are maximally different.
+        /// </summary>
+        /// <param name="s1">The first string.</param>
+        /// <param name="s2">The second string.</param>
+        /// <returns>a float between 0 and 1 based on how similar the specified strings are to one another.</returns>
+        float GetDistance(String s1, String s2);
+
     }
 }
