cbcd163ad19669955dd52a3af03836af448dad33
Isik Yigit <digy@apache.org>
2011-03-22 07:19:50 +0000

    [LUCENENET-399] AssemblyInfo  files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk@1084086 13f79535-47bb-0310-9956-ffa450edef68

--- C#/src/Lucene.Net/AssemblyInfo.cs
+++ C#/src/Lucene.Net/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDefaultAlias("Lucene.Net")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyInformationalVersionAttribute("2.9.2")]
+[assembly: AssemblyInformationalVersionAttribute("2.9.4")]
 
 
 //
@@ -47,7 +47,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.9.2.002")]
+[assembly: AssemblyVersion("2.9.4.001")]
 
 
 //
