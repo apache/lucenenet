3b835a69abf9665e147415cf284de1781e91c0b2
Troy Howard <thoward@apache.org>
2011-04-19 09:55:07 +0000

    This is a tag of the repo prior to changing the directory structure to the new scheme.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net@1094980 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tags/pre-layout-change/C#/contrib/Analyzers/Lucene.Net.Analyzers/CJK/CJKAnalyzer.cs
@@ -0,0 +1,128 @@
+using System;
+using System.IO;
+using System.Collections;
+using Lucene.Net.Analysis;
+
+namespace Lucene.Net.Analysis.CJK
+{
+	/* ====================================================================
+	 * The Apache Software License, Version 1.1
+	 *
+	 * Copyright (c) 2004 The Apache Software Foundation.  All rights
+	 * reserved.
+	 *
+	 * Redistribution and use in source and binary forms, with or without
+	 * modification, are permitted provided that the following conditions
+	 * are met:
+	 *
+	 * 1. Redistributions of source code must retain the above copyright
+	 *    notice, this list of conditions and the following disclaimer.
+	 *
+	 * 2. Redistributions in binary form must reproduce the above copyright
+	 *    notice, this list of conditions and the following disclaimer in
+	 *    the documentation and/or other materials provided with the
+	 *    distribution.
+	 *
+	 * 3. The end-user documentation included with the redistribution,
+	 *    if any, must include the following acknowledgment:
+	 *       "This product includes software developed by the
+	 *        Apache Software Foundation (http://www.apache.org/)."
+	 *    Alternately, this acknowledgment may appear in the software itself,
+	 *    if and wherever such third-party acknowledgments normally appear.
+	 *
+	 * 4. The names "Apache" and "Apache Software Foundation" and
+	 *    "Apache Lucene" must not be used to endorse or promote products
+	 *    derived from this software without prior written permission. For
+	 *    written permission, please contact apache@apache.org.
+	 *
+	 * 5. Products derived from this software may not be called "Apache",
+	 *    "Apache Lucene", nor may "Apache" appear in their name, without
+	 *    prior written permission of the Apache Software Foundation.
+	 *
+	 * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+	 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+	 * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	 * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+	 * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	 * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	 * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+	 * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	 * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	 * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+	 * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+	 * SUCH DAMAGE.
+	 * ====================================================================
+	 *
+	 * This software consists of voluntary contributions made by many
+	 * individuals on behalf of the Apache Software Foundation.  For more
+	 * information on the Apache Software Foundation, please see
+	 * <http://www.apache.org/>.
+	 *
+	 * $Id: CJKAnalyzer.java,v 1.5 2004/10/17 11:41:41 dnaber Exp $
+	 */
+
+	/// <summary>
+	/// Filters CJKTokenizer with StopFilter.
+	/// 
+	/// <author>Che, Dong</author>
+	/// </summary>
+	public class CJKAnalyzer : Analyzer 
+	{
+		//~ Static fields/initializers ---------------------------------------------
+
+		/// <summary>
+		/// An array containing some common English words that are not usually
+		/// useful for searching. and some double-byte interpunctions.....
+		/// </summary>
+		public static String[] stopWords = 
+		{
+			"a", "and", "are", "as", "at", "be",
+			"but", "by", "for", "if", "in",
+			"into", "is", "it", "no", "not",
+			"of", "on", "or", "s", "such", "t",
+			"that", "the", "their", "then",
+			"there", "these", "they", "this",
+			"to", "was", "will", "with", "",
+			"www"
+		};
+
+		//~ Instance fields --------------------------------------------------------
+
+		/// <summary>
+		/// stop word list
+		/// </summary>
+		private Hashtable stopTable;
+
+		//~ Constructors -----------------------------------------------------------
+
+		/// <summary>
+		/// Builds an analyzer which removes words in STOP_WORDS.
+		/// </summary>
+		public CJKAnalyzer() 
+		{
+			stopTable = StopFilter.MakeStopSet(stopWords);
+		}
+
+		/// <summary>
+		/// Builds an analyzer which removes words in the provided array.
+		/// </summary>
+		/// <param name="stopWords">stop word array</param>
+		public CJKAnalyzer(String[] stopWords) 
+		{
+			stopTable = StopFilter.MakeStopSet(stopWords);
+		}
+
+		//~ Methods ----------------------------------------------------------------
+
+		/// <summary>
+		/// get token stream from input
+		/// </summary>
+		/// <param name="fieldName">lucene field name</param>
+		/// <param name="reader">input reader</param>
+		/// <returns>Token Stream</returns>
+		public override sealed TokenStream TokenStream(String fieldName, TextReader reader) 
+		{
+			return new StopFilter(new CJKTokenizer(reader), stopTable);
+		}
+	}
+}
---
+++ tags/pre-layout-change/C#/src/Test/Search/TestCachingSpanFilter.cs
@@ -0,0 +1,129 @@
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Lucene.Net.Store;
+using Lucene.Net.Index;
+using Lucene.Net.Analysis;
+using Lucene.Net.Documents;
+using Lucene.Net.Search;
+using Lucene.Net.Search.Spans;
+using LuceneTestCase = Lucene.Net.Util.LuceneTestCase;
+
+using NUnit.Framework;
+
+namespace Lucene.Net.Search
+{
+    [TestFixture]
+    public class TestCachingSpanFilter : LuceneTestCase
+    {
+        [Test]
+        public void TestEnforceDeletions()
+        {
+            Directory dir = new MockRAMDirectory();
+            IndexWriter writer = new IndexWriter(dir, new WhitespaceAnalyzer(), IndexWriter.MaxFieldLength.UNLIMITED);
+            IndexReader reader = writer.GetReader();
+            IndexSearcher searcher = new IndexSearcher(reader);
+
+            // add a doc, refresh the reader, and check that its there
+            Document doc = new Document();
+            doc.Add(new Field("id", "1", Field.Store.YES, Field.Index.NOT_ANALYZED));
+            writer.AddDocument(doc);
+
+            reader = RefreshReader(reader);
+            searcher = new IndexSearcher(reader);
+
+            TopDocs docs = searcher.Search(new MatchAllDocsQuery(), 1);
+            Assert.AreEqual(1, docs.TotalHits, "Should find a hit...");
+
+            SpanFilter startFilter = new SpanQueryFilter(new SpanTermQuery(new Term("id", "1")));
+
+            // ignore deletions
+            CachingSpanFilter filter = new CachingSpanFilter(startFilter, CachingWrapperFilter.DeletesMode.IGNORE);
+
+            docs = searcher.Search(new MatchAllDocsQuery(), filter, 1);
+            Assert.AreEqual(1, docs.TotalHits, "[query + filter] Should find a hit...");
+            ConstantScoreQuery constantScore = new ConstantScoreQuery(filter);
+            docs = searcher.Search(constantScore, 1);
+            Assert.AreEqual(1, docs.TotalHits, "[just filter] Should find a hit...");
+
+            // now delete the doc, refresh the reader, and see that it's not there
+            writer.DeleteDocuments(new Term("id", "1"));
+
+            reader = RefreshReader(reader);
+            searcher = new IndexSearcher(reader);
+
+            docs = searcher.Search(new MatchAllDocsQuery(), filter, 1);
+            Assert.AreEqual(0, docs.TotalHits, "[query + filter] Should *not* find a hit...");
+
+            docs = searcher.Search(constantScore, 1);
+            Assert.AreEqual(1, docs.TotalHits, "[just filter] Should find a hit...");
+
+
+            // force cache to regenerate:
+            filter = new CachingSpanFilter(startFilter, CachingWrapperFilter.DeletesMode.RECACHE);
+
+            writer.AddDocument(doc);
+            reader = RefreshReader(reader);
+            searcher = new IndexSearcher(reader);
+
+            docs = searcher.Search(new MatchAllDocsQuery(), filter, 1);
+            Assert.AreEqual(1, docs.TotalHits, "[query + filter] Should find a hit...");
+
+            constantScore = new ConstantScoreQuery(filter);
+            docs = searcher.Search(constantScore, 1);
+            Assert.AreEqual(1, docs.TotalHits, "[just filter] Should find a hit...");
+
+            // make sure we get a cache hit when we reopen readers
+            // that had no new deletions
+            IndexReader newReader = RefreshReader(reader);
+            Assert.IsTrue(reader != newReader);
+            reader = newReader;
+            searcher = new IndexSearcher(reader);
+            int missCount = filter.missCount;
+            docs = searcher.Search(constantScore, 1);
+            Assert.AreEqual(1, docs.TotalHits, "[just filter] Should find a hit...");
+            Assert.AreEqual(missCount, filter.missCount);
+
+            // now delete the doc, refresh the reader, and see that it's not there
+            writer.DeleteDocuments(new Term("id", "1"));
+
+            reader = RefreshReader(reader);
+            searcher = new IndexSearcher(reader);
+
+            docs = searcher.Search(new MatchAllDocsQuery(), filter, 1);
+            Assert.AreEqual(0, docs.TotalHits, "[query + filter] Should *not* find a hit...");
+
+            docs = searcher.Search(constantScore, 1);
+            Assert.AreEqual(0, docs.TotalHits, "[just filter] Should *not* find a hit...");
+        }
+
+        private static IndexReader RefreshReader(IndexReader reader)
+        {
+            IndexReader oldReader = reader;
+            reader = reader.Reopen();
+            if (reader != oldReader)
+            {
+                oldReader.Close();
+            }
+            return reader;
+        }
+    }
+}
