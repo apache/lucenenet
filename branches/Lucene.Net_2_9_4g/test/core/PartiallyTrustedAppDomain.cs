0149cd5a175bfcaa33db41abf000c201d0cbc79a
Isik Yigit <digy@apache.org>
2011-05-03 19:45:21 +0000

    [LUCENENET-412] Test Env. for medium-trust
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net@1099214 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/Lucene.Net_2_9_4g/test/core/PartiallyTrustedAppDomain.cs
@@ -0,0 +1,83 @@
+﻿/* 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+using System;
+using System.IO;
+using System.Security;
+using System.Security.Permissions;
+using System.Security.Policy;
+
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Test
+{
+    public class PartiallyTrustedAppDomain
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="clazz">class to create</param>
+        /// <param name="methodName">method to invoke</param>
+        /// <param name="constructorArgs">constructor's parameters</param>
+        /// <param name="methodArgs">method's parameters</param>
+        public static void Run(Type clazz, string methodName, object[] constructorArgs, object[] methodArgs)
+        {
+            try
+            {
+                AppDomainSetup setup = new AppDomainSetup { ApplicationBase = Environment.CurrentDirectory };
+
+                PermissionSet permissions = new PermissionSet(null);
+                permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+                permissions.AddPermission(new ReflectionPermission(ReflectionPermissionFlag.RestrictedMemberAccess));
+
+                AppDomain appDomain = AppDomain.CreateDomain("PartiallyTrustedAppDomain", null, setup, permissions);
+
+                object p = appDomain.CreateInstanceAndUnwrap(
+                    clazz.Assembly.FullName,
+                    clazz.FullName,
+                    false,
+                    BindingFlags.CreateInstance,
+                    null,
+                    constructorArgs,
+                    System.Globalization.CultureInfo.CurrentCulture,
+                    null);
+                p.GetType().InvokeMember(methodName, BindingFlags.InvokeMethod, null, p, methodArgs);
+            }
+            catch (TypeLoadException tlex)
+            {
+                throw tlex;
+            }
+            catch (TargetInvocationException tiex)
+            {
+                throw tiex.InnerException;
+            }
+            catch (SecurityException secex)
+            {
+                throw secex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
