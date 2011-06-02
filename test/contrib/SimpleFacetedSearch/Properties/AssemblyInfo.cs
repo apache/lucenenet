7e834b38a1c4235ee082f9f77e7bfc671e16cf1e
Isik Yigit <digy@apache.org>
2011-06-02 18:40:19 +0000

    [LUCENENET-415] SimpleFacetedSearch
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk@1130715 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/contrib/SimpleFacetedSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test for Lucene.Net.Search.SimpleFacetedSearch")]
+[assembly: AssemblyDescription("Test for SimpleFacetedSearch for Lucene.Net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The Apache Software Foundation")]
+[assembly: AssemblyProduct("Lucene.Net.SimpleFacetedSearch")]
+[assembly: AssemblyCopyright("Copyright 2006 - 2011 The Apache Software Foundation")]
+[assembly: AssemblyTrademark("Copyright 2006 - 2011 The Apache Software Foundation")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("df0ddee7-24b4-48bd-ba39-21bbd4fc39c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.9.4")]
+[assembly: AssemblyFileVersion("2.9.4")]
