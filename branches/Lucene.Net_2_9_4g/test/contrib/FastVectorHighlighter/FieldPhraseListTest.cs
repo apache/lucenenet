6d485d80536a31065f088e91ac373388f54caedb
Isik Yigit <digy@apache.org>
2011-06-27 06:45:45 +0000

    [LUCENENET-427] Provide limit on phrase analysis in FastVectorHighlighter
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net@1140026 13f79535-47bb-0310-9956-ffa450edef68

--- branches/Lucene.Net_2_9_4g/test/contrib/FastVectorHighlighter/FieldPhraseListTest.cs
+++ branches/Lucene.Net_2_9_4g/test/contrib/FastVectorHighlighter/FieldPhraseListTest.cs
@@ -222,5 +222,35 @@ namespace Lucene.Net.Search.Vectorhighlight
             Assert.AreEqual(1, fpl.phraseList.Count);
             Assert.AreEqual("sppeeeed(1.0)((88,93))", fpl.phraseList.First.Value.ToString());
         }
+        
+      /* This test shows a big speedup from limiting the number of analyzed phrases in 
+       * this bad case for FieldPhraseList */
+      /* But it is not reliable as a unit test since it is timing-dependent
+      public void testManyRepeatedTerms() throws Exception {
+          long t = System.currentTimeMillis();
+          testManyTermsWithLimit (-1);
+          long t1 = System.currentTimeMillis();
+          testManyTermsWithLimit (1);
+          long t2 = System.currentTimeMillis();
+          assertTrue (t2-t1 * 1000 < t1-t);
+      }
+      private void testManyTermsWithLimit (int limit) throws Exception {
+          StringBuilder buf = new StringBuilder ();
+          for (int i = 0; i < 16000; i++) {
+              buf.append("a b c ");
+          }
+          make1d1fIndex( buf.toString());
+
+          Query query = tq("a");
+          FieldQuery fq = new FieldQuery( query, true, true );
+          FieldTermStack stack = new FieldTermStack( reader, 0, F, fq );
+          FieldPhraseList fpl = new FieldPhraseList( stack, fq, limit);
+          if (limit < 0 || limit > 16000)
+              assertEquals( 16000, fpl.phraseList.size() );
+          else
+              assertEquals( limit, fpl.phraseList.size() );
+          assertEquals( "a(1.0)((0,1))", fpl.phraseList.get( 0 ).toString() );      
+      }
+      */
     }
 }
