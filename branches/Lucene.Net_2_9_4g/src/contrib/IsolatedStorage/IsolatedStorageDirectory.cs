7b85b689c4b8aa61cdfd82caf3cd3ac55e84e400
Isik Yigit <digy@apache.org>
2011-08-28 18:32:24 +0000

    [LUCENENET-424] A working copy for IsolatedStorageDirectory (not tested for WP7)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net@1162569 13f79535-47bb-0310-9956-ffa450edef68

--- branches/Lucene.Net_2_9_4g/src/contrib/IsolatedStorage/IsolatedStorageDirectory.cs
+++ branches/Lucene.Net_2_9_4g/src/contrib/IsolatedStorage/IsolatedStorageDirectory.cs
@@ -22,263 +22,128 @@ using System.IO.IsolatedStorage;
 namespace Lucene.Net.Store
 {
 
-    public class IsolatedStorageDirectory : Directory
+    public class IsolatedStorageDirectory : Lucene.Net.Store.Directory
     {
-        private IsolatedStorageFile IsoDirectory = null;
-        private string IsoDirectoryPath = null;
+        System.IO.IsolatedStorage.IsolatedStorageFile _is = null;
+        string _dirName;
 
-        /// <summary>
-        /// Create a new instance of the isolated store for this user, domain, and assembly
-        /// </summary>
-        /// <param name="path">Path within the store for the Lucene indexs</param>
-        public IsolatedStorageDirectory(string path) : this(path, null)
+        public IsolatedStorageDirectory(System.IO.DirectoryInfo dInfo)
+            : this(dInfo.FullName)
         {
         }
 
-        /// <summary>
-        /// Create a new instance of the isolated store for this user, domain, and assembly
-        /// </summary>
-        /// <param name="path">Path within the store for the Lucene indexs</param>
-        /// <param name="lockFactory">Not Implimented</param>
-        /// TODO: Impliment lockFactory
-        public IsolatedStorageDirectory(string path, LockFactory lockFactory)
+        public IsolatedStorageDirectory(string dirName)
         {
-            IsoDirectory = IsolatedStorageFile.GetUserStoreForApplication();
-            
-            if (!IsoDirectory.DirectoryExists(path))
+            _is = System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForAssembly();
+
+            _dirName = dirName;
+            _is.CreateDirectory(dirName);
+        }
+
+        public static void Remove(string dirName)
+        {
+            var dir = new IsolatedStorageDirectory(dirName);
+            dir.Remove();
+            dir.Close();
+        }
+
+        public void Remove()
+        {
+            foreach (var f in ListAll())
             {
-                throw new NoSuchDirectoryException(String.Format("{0} exists but is not a folder", path));
+                for (int i = 0; i < 5; i++)
+                {
+                    try
+                    {
+                        _is.DeleteFile(Path.Combine(_dirName, f));
+                        break;
+                    }
+                    catch
+                    {
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
             }
 
-            IsoDirectoryPath = path.TrimEnd('/');
-        }
-
-        /// <summary>
-        /// Initializes the directory to create a new file with the given name.
-        /// This method should be used in {@link #createOutput}. 
-        /// </summary>
-        protected internal void InitOutput(string name)
-        {
-            var isoFile = GetIsoStoragePath(name);
-
-            if (IsoDirectory.FileExists(isoFile))
+            try
             {
-                IsoDirectory.DeleteFile(isoFile);
+                _is.DeleteDirectory(_dirName);
             }
+            catch { }
         }
 
-        /// <summary>
-        /// Lists all files (not subdirectories) in the directory.
-        /// </summary>
-        public String[] ListAll(string dir)
+        public override Lock MakeLock(string name)
         {
-            if (!IsoDirectory.DirectoryExists(dir))
-            {
-                throw new NoSuchDirectoryException(String.Format("Directory '{0}' does not exist", dir));
-            }
-
-            return IsoDirectory.GetFileNames();
+            return new IsolatedStorageLock(_is, Path.Combine(_dirName, name));
         }
 
-        /// <summary>
-        /// Lists all files (not subdirectories) in the directory.
-        /// </summary>
-        public override String[] ListAll()
+        public override string[] ListAll()
         {
-            return ListAll(IsoDirectoryPath);
+            return _is.GetFileNames(Path.Combine(_dirName, "*"));
         }
 
-        /// <summary>
-        /// Returns true iff a file with the given name exists. 
-        /// </summary>
+        public override string[] List()
+        {
+            return ListAll();
+        }
+
         public override bool FileExists(string name)
         {
-            return IsoDirectory.FileExists(GetIsoStoragePath(name));
+            return _is.FileExists(Path.Combine(_dirName, name));
         }
 
-        /// <summary>
-        /// Returns the time the named file was last modified. 
-        /// </summary>
         public override long FileModified(string name)
         {
-            var file = GetIsoStoragePath(name);
-
-            if (IsoDirectory.FileExists(file))
-            {
-                // {{LUCENENET-353}}
-                return (long) IsoDirectory.GetLastWriteTime(file)
-                    .ToUniversalTime()
-                    .Subtract(new DateTime(1970, 1, 1, 0, 0, 0))
-                    .TotalMilliseconds;
-            }
-
-            throw new FileNotFoundException(name + "not found");
+            return GetDate(_is.GetLastWriteTime(Path.Combine(_dirName, name)).LocalDateTime);
         }
 
-        /// <summary>
-        /// Set the modified time of an existing file to now.
-        /// </summary>
         public override void TouchFile(string name)
         {
-            var file = GetIsoStoragePath(name);
-
-            var f = new IsolatedStorageFileStream(file, FileMode.Append);
-            f.Close(); // TODO: Verify that this updates the modified time of the file
+            _is.OpenFile(name, FileMode.OpenOrCreate).Close();
         }
 
-        /// <summary>
-        /// Returns the length in bytes of a file in the directory
-        /// </summary>
-        public override long FileLength(string name)
-        {
-            var file = GetIsoStoragePath(name);
-            var isoFile = new IsolatedStorageFileStream(file, FileMode.Open);
-            return isoFile.Length;
-        }
-
-        /// <summary>
-        /// Removes an existing file in the directory. 
-        /// </summary>
         public override void DeleteFile(string name)
         {
-            var file = GetIsoStoragePath(name);
-            IsoDirectory.DeleteFile(file);
+            _is.DeleteFile(Path.Combine(_dirName, name));
         }
 
-        /// <summary>
-        /// Sync the file back to IsolatedStorage
-        /// </summary>
-        /// <param name="name"></param>
-        public override void Sync(string name)
-        {
-            var fileName = GetIsoStoragePath(name);
-            var success = false;
-            var retryCount = 0;
-
-            IOException exc = null;
-
-            while (!success && retryCount < 5)
-            {
-                retryCount++;
-                IsolatedStorageFileStream file = null;
-
-                try
-                {
-                    try
-                    {
-                        file = new IsolatedStorageFileStream(fileName,
-                            FileMode.OpenOrCreate,
-                            FileAccess.Write,
-                            FileShare.ReadWrite);
-
-                        file.Flush();
-                        success = true;
-                    }
-                    finally
-                    {
-                        if (file != null)
-                            file.Close();
-                    }
-                }
-                catch (IOException ioe)
-                {
-                    if (exc == null)
-                        exc = ioe;
-                    try
-                    {
-                        // Pause 5 msec
-                        System.Threading.Thread.Sleep(new TimeSpan((Int64)10000 * 5));
-                    }
-                    catch (System.Threading.ThreadInterruptedException ie)
-                    {
-                        Support.ThreadClass.Current().Interrupt();
-                        throw new System.Threading.ThreadInterruptedException(ie.ToString(), ie);
-                    }
-                }
-            }
-
-            if (!success)
-                if (exc != null) throw exc;
-        }
-
-        /// <summary>
-        /// Creates an IndexOutput for the file with the given name.
-        /// <em>In 3.0 this method will become abstract.</em> 
-        /// </summary>
-        public override IndexOutput CreateOutput(string name)
-        {
-            InitOutput(name);
-            return new IsolatedStorageOutputStream(GetIsoStoragePath(name), ref IsoDirectory);
-        }
-
-        /// <summary>
-        /// Returns a stream reading an existing file. 
-        /// </summary>
-        public override IndexInput OpenInput(string name)
-        {
-            return new IsolatedStorageInputStream(GetIsoStoragePath(name), ref IsoDirectory);
-        }
-
-        /// <summary>
-        /// Closes the store to future operations. 
-        /// </summary>
-        public override void Close()
-        {
-            IsoDirectory.Close();
-        }
-
-        /// <summary>
-        /// For .Net Using
-        /// </summary>
-        public override void Dispose()
-        {
-            IsoDirectory.Close();
-        }
-
-        /// <summary>
-        /// For debug output. 
-        /// </summary>
-        public override string ToString()
-        {
-            return GetType().FullName + "@" + IsoDirectoryPath + " lockFactory=" + GetLockFactory();
-        }
-
-        #region Obsolete
-
-        /// <summary>
-        /// Renames an existing file in the directory - overwriting the to file if exists
-        /// </summary>
-        /// <deprecated> 
-        /// </deprecated>
-        [Obsolete("Not Implimented")]
         public override void RenameFile(string from, string to)
         {
-            throw new NotImplementedException("This method (IsolatedStorageDirectory.RenameFile) is Obsolete");
+            _is.MoveFile(Path.Combine(_dirName, from), Path.Combine(_dirName, to));
         }
 
-        /// <summary>
-        /// Lists all files (not subdirectories) in the directory.
-        /// </summary>
-        [Obsolete("Lucene.Net-2.9.1. This method overrides obsolete member Lucene.Net.Store.Directory.List()")]
-        public override String[] List()
+        public override long FileLength(string name)
         {
-            return IsoDirectory.GetFileNames();
+            using (var f = _is.OpenFile(Path.Combine(_dirName, name), FileMode.Open))
+            {
+                return f.Length;
+            }
         }
 
-        #endregion
-
-        #region Private Helpers
-
-        /// <summary>
-        /// Prepends filenames with the correct path
-        /// </summary>
-        /// <param name="file"></param>
-        /// <returns></returns>
-        private string GetIsoStoragePath(string file)
+        public override IndexOutput CreateOutput(string name)
         {
-            return IsoDirectoryPath + '/' + file;
+            return new IsolatedStorageIndexOutput(_is, Path.Combine(_dirName, name));
         }
 
-        #endregion
+        public override IndexInput OpenInput(string name)
+        {
+            return new IsolatedStorageIndexInput(_is, Path.Combine(_dirName, name));
+        }
+
+        public override void Close()
+        {
+            _is.Close();
+        }
+
+        public override void Dispose()
+        {
+            Close();
+        }
+
+
+        private long GetDate(DateTime dt)
+        {
+            return (long)dt.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds;
+        }
     }
 }
---
+++ branches/Lucene.Net_2_9_4g/src/contrib/IsolatedStorage/IsolatedStorageIndexOutput.cs
@@ -0,0 +1,82 @@
+﻿/* 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace Lucene.Net.Store
+{
+    public class IsolatedStorageIndexOutput : Lucene.Net.Store.BufferedIndexOutput, IDisposable
+    {
+        System.IO.IsolatedStorage.IsolatedStorageFileStream _file;
+        bool _isOpen;
+
+        public IsolatedStorageIndexOutput(System.IO.IsolatedStorage.IsolatedStorageFile dir, string name)
+        {
+            try
+            {
+                _file = dir.CreateFile(name);
+                _isOpen = true;
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new AlreadyClosedException(e.Message);
+            }
+        }
+
+
+        public override void FlushBuffer(byte[] b, int offset, int len)
+        {
+            _file.Write(b, offset, len);
+            _file.Flush();
+        }
+
+
+        public override void Close()
+        {
+            if (_isOpen)
+            {
+                _isOpen = false;
+                base.Close();
+                _file.Close();
+            }
+        }
+
+        public void Dispose()
+        {
+            Close();
+        }
+
+
+        public override void Seek(long pos)
+        {
+            base.Seek(pos);
+            _file.Seek(pos, SeekOrigin.Begin);
+        }
+
+        public override long Length()
+        {
+            return _file.Length;
+        }
+
+        public override void SetLength(long length)
+        {
+            _file.SetLength(length);
+        }
+    }
+}
