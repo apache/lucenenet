653c6949cb99bfae774def66c35a868712d5be40
Isik Yigit <digy@apache.org>
2011-08-28 20:23:01 +0000

    [LUCENENET-358] MemoryLeaks See http://groups.google.com/group/ravendb/browse_thread/thread/9c6340987407aece/1d86d6a61175233c and http://mail-archives.apache.org/mod_mbox/lucene-lucene-net-user/201108.mbox/browser
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk@1162583 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/Util/CloseableThreadLocal.cs
+++ src/core/Util/CloseableThreadLocal.cs
@@ -16,79 +16,164 @@
  */
 
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Lucene.Net.Util
 {
-	
-	/// <summary>Java's builtin ThreadLocal has a serious flaw:
-	/// it can take an arbitrarily long amount of time to
-	/// dereference the things you had stored in it, even once the
-	/// ThreadLocal instance itself is no longer referenced.
-	/// This is because there is single, master map stored for
-	/// each thread, which all ThreadLocals share, and that
-	/// master map only periodically purges "stale" entries.
-	/// 
-	/// While not technically a memory leak, because eventually
-	/// the memory will be reclaimed, it can take a long time
-	/// and you can easily hit OutOfMemoryError because from the
-	/// GC's standpoint the stale entries are not reclaimaible.
-	/// 
-	/// This class works around that, by only enrolling
-	/// WeakReference values into the ThreadLocal, and
-	/// separately holding a hard reference to each stored
-	/// value.  When you call {@link #close}, these hard
-	/// references are cleared and then GC is freely able to
-	/// reclaim space by objects stored in it. 
-	/// </summary>
-	
-	public class CloseableThreadLocal
-	{
-		
-		[ThreadStatic]
-        static SupportClass.WeakHashTable slots;
-		
-		public /*protected internal*/ virtual System.Object InitialValue()
-		{
-			return null;
-		}
 
-        public virtual System.Object Get()
+    /// <summary>Java's builtin ThreadLocal has a serious flaw:
+    /// it can take an arbitrarily long amount of time to
+    /// dereference the things you had stored in it, even once the
+    /// ThreadLocal instance itself is no longer referenced.
+    /// This is because there is single, master map stored for
+    /// each thread, which all ThreadLocals share, and that
+    /// master map only periodically purges "stale" entries.
+    /// 
+    /// While not technically a memory leak, because eventually
+    /// the memory will be reclaimed, it can take a long time
+    /// and you can easily hit OutOfMemoryError because from the
+    /// GC's standpoint the stale entries are not reclaimaible.
+    /// 
+    /// This class works around that, by only enrolling
+    /// WeakReference values into the ThreadLocal, and
+    /// separately holding a hard reference to each stored
+    /// value.  When you call {@link #close}, these hard
+    /// references are cleared and then GC is freely able to
+    /// reclaim space by objects stored in it. 
+    /// </summary>
+    /// 
+
+    public class CloseableThreadLocal
+    {
+        private ThreadLocal<WeakReference> t = new ThreadLocal<WeakReference>();
+
+        private Dictionary<Thread, object> hardRefs = new Dictionary<Thread, object>();
+
+
+        public virtual object InitialValue()
         {
-            object value;
+            return null;
+        }
 
-            if (slots == null)
+        public virtual object Get()
+        {
+            WeakReference weakRef = t.Get();
+            if (weakRef == null)
             {
-                value = InitialValue();
-                if (value != null)
-                    Set(value);
-
-                return value;
-            }
-
-            if (slots.ContainsKey(this))
-            {
-                return slots[this];
+                object iv = InitialValue();
+                if (iv != null)
+                {
+                    Set(iv);
+                    return iv;
+                }
+                else
+                    return null;
             }
             else
             {
-                value = InitialValue();
-                slots[this] = value;
-                return value;
+                return weakRef.Get();
             }
         }
-		
-		public virtual void  Set(System.Object object_Renamed)
-		{
-            if (slots == null)
-                slots = new SupportClass.WeakHashTable();
 
-			slots[this] = object_Renamed;	
-		}
-		
-		public virtual void  Close()
-		{
-            if(slots != null)
-                slots.Remove(this);
-		}
-	}
+        public virtual void Set(object @object)
+        {
+            //+-- For Debuging
+            if (SupportClass.CloseableThreadLocalProfiler.EnableCloseableThreadLocalProfiler == true)
+            {
+                lock (SupportClass.CloseableThreadLocalProfiler.Instances)
+                {
+                    SupportClass.CloseableThreadLocalProfiler.Instances.Add(new WeakReference(@object));
+                }
+            }
+            //+--
+
+            t.Set(new WeakReference(@object));
+
+            lock (hardRefs)
+            {
+                //hardRefs[Thread.CurrentThread] = @object;
+                hardRefs.Add(Thread.CurrentThread, @object);
+
+                // Purge dead threads
+                foreach (var thread in new List<Thread>(hardRefs.Keys))
+                {
+                    if (!thread.IsAlive)
+                        hardRefs.Remove(thread);
+                }
+
+            }
+        }
+
+        public virtual void Close()
+        {
+            // Clear the hard refs; then, the only remaining refs to
+            // all values we were storing are weak (unless somewhere
+            // else is still using them) and so GC may reclaim them:
+            hardRefs = null;
+            // Take care of the current thread right now; others will be
+            // taken care of via the WeakReferences.
+            if (t != null)
+            {
+                t.Remove();
+            }
+            t = null;
+        }
+    }
+
+    internal static class CloseableThreadLocalExtensions
+    {
+        public static void Set<T>(this ThreadLocal<T> t, T val)
+        {
+            t.Value = val;
+        }
+
+        public static T Get<T>(this ThreadLocal<T> t)
+        {
+            return t.Value;
+        }
+
+        public static void Remove<T>(this ThreadLocal<T> t)
+        {
+            t.Dispose();
+        }
+
+        public static object Get(this WeakReference w)
+        {
+            return w.Target;
+        }
+    }
+
+    //// {{DIGY}}
+    //// To compile against Framework 2.0
+    //// Uncomment below class
+    //public class ThreadLocal<T> : IDisposable
+    //{
+    //    [ThreadStatic]
+    //    static SupportClass.WeakHashTable slots;
+
+    //    void Init()
+    //    {
+    //        if (slots == null) slots = new SupportClass.WeakHashTable();
+    //    }
+
+    //    public T Value
+    //    {
+    //        set
+    //        {
+    //            Init();
+    //            slots.Add(this, value);
+    //        }
+    //        get
+    //        {
+    //            Init();
+    //            return (T)slots[this];
+    //        }
+    //    }
+
+    //    public void Dispose()
+    //    {
+    //        slots.Remove(this);
+    //    }
+    //}
 }
--- test/core/TestSupportClass.cs
+++ test/core/TestSupportClass.cs
@@ -210,6 +210,65 @@ namespace Lucene.Net._SupportClass
             int memUsage = GetMemUsageInKB();
             if (memUsage > initialMemUsage * 2) Assert.Fail("Memory Leakage.MemUsage = " + memUsage);
         }
+
+    }
+
+    [TestFixture]
+    public class TestCloseableThreadLocal
+    {
+        [Test]
+        public void TestMemLeakage()
+        {
+            SupportClass.CloseableThreadLocalProfiler.EnableCloseableThreadLocalProfiler = true;
+
+            int LoopCount = 100;
+            Analyzer[] analyzers = new Analyzer[LoopCount];
+            RAMDirectory[] dirs = new RAMDirectory[LoopCount];
+            IndexWriter[] indexWriters = new IndexWriter[LoopCount];
+
+            System.Threading.Tasks.Parallel.For(0, LoopCount, (i) =>
+            {
+                analyzers[i] = new Lucene.Net.Analysis.Standard.StandardAnalyzer();
+                dirs[i] = new RAMDirectory();
+                indexWriters[i] = new IndexWriter(dirs[i], analyzers[i], true);
+            });
+
+            System.Threading.Tasks.Parallel.For(0, LoopCount, (i) =>
+            {
+                Document document = new Document();
+                document.Add(new Field("field", "some test", Field.Store.NO, Field.Index.ANALYZED));
+                indexWriters[i].AddDocument(document);
+            });
+
+            System.Threading.Tasks.Parallel.For(0, LoopCount, (i) =>
+            {
+                analyzers[i].Close();
+                indexWriters[i].Close();
+            });
+
+            System.Threading.Tasks.Parallel.For(0, LoopCount, (i) =>
+            {
+                IndexSearcher searcher = new IndexSearcher(dirs[i]);
+                TopDocs d = searcher.Search(new TermQuery(new Term("field", "test")), 10);
+                searcher.Close();
+            });
+
+            System.Threading.Tasks.Parallel.For(0, LoopCount, (i) => dirs[i].Close());
+
+            GC.Collect(GC.MaxGeneration);
+            GC.WaitForPendingFinalizers();
+
+            int aliveObjects = 0;
+            foreach (WeakReference w in SupportClass.CloseableThreadLocalProfiler.Instances)
+            {
+                object o = w.Target;
+                if (o != null) aliveObjects++;
+            }
+
+            SupportClass.CloseableThreadLocalProfiler.EnableCloseableThreadLocalProfiler = false;
+
+            Assert.AreEqual(0, aliveObjects);
+        }
     }
 
     [TestFixture]
