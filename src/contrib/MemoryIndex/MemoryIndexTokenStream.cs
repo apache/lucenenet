a43e91e68714d8cefa9b2fbf45c02b2c72e1097b
Prescott Nasser <pnasser@apache.org>
2012-03-26 05:58:26 +0000

    Adding MemoryIndex to contrib - not even close to compiling, but it's a start.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lucene.net/trunk@1305232 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/contrib/MemoryIndex/MemoryIndexTokenStream.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Tokenattributes;
+
+namespace Lucene.Net.Index.Memory
+{
+    class MemoryIndexTokenStream : TokenStream
+    {
+        private IList<T> _keywords;
+        private int start = 0;
+        private TermAttribute termAtt = AddAttribute(TermAttribute.class);
+        private OffsetAttribute offsetAtt = AddAttribute(OffsetAttribute.class);
+      
+        public MemoryIndexTokenStream(IList<T> keywords)
+        {
+            _keywords = keywords;
+        }
+
+        public override bool IncrementToken()
+        {
+            if (iter.hasNext()) return false;
+        
+            Object obj = iter.Next();
+
+            if (obj == null) 
+                throw new ArgumentException("keyword must not be null");
+        
+            String term = obj.ToString();
+            ClearAttributes();
+            termAtt.SetTermBuffer(term);
+            offsetAtt.SetOffset(start, start+termAtt.TermLength());
+            start += term.Length + 1; // separate words by 1 (blank) character
+            
+            return true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            // do nothing
+        }
+    }
+}
+
+        
+     
