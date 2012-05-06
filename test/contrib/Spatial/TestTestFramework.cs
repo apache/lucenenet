13343c5c868c8cf980bbd5f853476492555f1e9f
Itamar Syn-Hershko <itamar@code972.com>
2012-05-06 22:18:49 +0300

    Porting tests

--- test/contrib/Spatial/TestTestFramework.cs
+++ test/contrib/Spatial/TestTestFramework.cs
@@ -27,26 +27,26 @@ namespace Lucene.Net.Contrib.Spatial.Test
 	/// </summary>
 	public class TestTestFramework : LuceneTestCase
 	{
-		public void testQueries()
-		  {
-    String name = StrategyTestCase.QTEST_Cities_IsWithin_BBox;
+  //      public void testQueries()
+  //        {
+  //  String name = StrategyTestCase.QTEST_Cities_IsWithin_BBox;
 
-    InputStream @in = getClass().getClassLoader().getResourceAsStream(name);
-    SpatialContext ctx = SimpleSpatialContext.GEO_KM;
-    Iterator<SpatialTestQuery> iter = SpatialTestQuery.getTestQueries(
-        new SpatialArgsParser(), ctx, name, in );
-    List<SpatialTestQuery> tests = new ArrayList<SpatialTestQuery>();
-    while( iter.hasNext() ) {
-      tests.add( iter.next() );
-    }
-    Assert.assertEquals( 3, tests.size() );
+  //  InputStream @in = getClass().getClassLoader().getResourceAsStream(name);
+  //  SpatialContext ctx = SimpleSpatialContext.GEO_KM;
+  //  Iterator<SpatialTestQuery> iter = SpatialTestQuery.getTestQueries(
+  //      new SpatialArgsParser(), ctx, name, in );
+  //  List<SpatialTestQuery> tests = new ArrayList<SpatialTestQuery>();
+  //  while( iter.hasNext() ) {
+  //    tests.add( iter.next() );
+  //  }
+  //  Assert.assertEquals( 3, tests.size() );
 
-    SpatialTestQuery sf = tests.get(0);
-   // assert
-    Assert.assertEquals( 1, sf.ids.size() );
-    Assert.assertTrue( sf.ids.get(0).equals( "G5391959" ) );
-    Assert.assertTrue( sf.args.getShape() instanceof Rectangle);
-    Assert.assertEquals( SpatialOperation.IsWithin, sf.args.getOperation() );
-  }
+  //  SpatialTestQuery sf = tests.get(0);
+  // // assert
+  //  Assert.assertEquals( 1, sf.ids.size() );
+  //  Assert.assertTrue( sf.ids.get(0).equals( "G5391959" ) );
+  //  Assert.assertTrue( sf.args.getShape() instanceof Rectangle);
+  //  Assert.assertEquals( SpatialOperation.IsWithin, sf.args.getOperation() );
+  //}
 	}
 }
