9e6bec1b9e053987d68aa6182e567f353a8d5e03
Ayende Rahien <ayende@ayende.com>
2013-01-31 11:15:52 +0200

    Opps, it has to be NaN, not null

--- src/contrib/Spatial/Util/ShapeFieldCacheDistanceValueSource.cs
+++ src/contrib/Spatial/Util/ShapeFieldCacheDistanceValueSource.cs
@@ -76,7 +76,7 @@ namespace Lucene.Net.Spatial.Util
 					// Solr's 'recip' function where v = distance and v > 0.
 					return v > 0 ? 1000 / (1 * v + 1000) : 0;
 				}
-				return null;
+				return Double.NaN;
 			}
 
 			public override string ToString(int doc)
