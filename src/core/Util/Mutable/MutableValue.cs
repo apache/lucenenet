938fa6ed2d1ecf1a3c0351550aa47d05d68bd339
James Blair <jmblair7@gmail.com>
2013-06-11 19:50:08 -0400

    Port: Started Core.Util.Mutable namespace
    added all classes for the Mutable namespace. MutableValue base class is
    complete. Slight discrepancy with porting the public Java "exists" field
    and "Exists()" method. not sure how to handle this properly. For now,
    made protected _exists field and read-only Exists property

---
+++ src/core/Util/Mutable/MutableValue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lucene.Net.Util.Mutable
+{
+    public abstract class MutableValue : IComparable<MutableValue>
+    {
+        protected Boolean _exists = true;
+        public Boolean Exists { get { return this._exists; }}
+
+        public abstract void Copy(MutableValue source);
+        public abstract MutableValue Duplicate();
+        public abstract Boolean EqualsSameType(Object other);
+        public abstract Int32 CompareSameType(Object other);
+        public abstract Object ToObject();
+
+
+        public int CompareTo(MutableValue other)
+        {
+            var c1 = this.GetType();
+            var c2 = other.GetType();
+
+            if (c1 != c2)
+            {
+                var c = c1.GetHashCode() - c2.GetHashCode();
+                if (c == 0)
+                {
+                    c = c1.Name.CompareTo(c1.Name);
+                }
+                return c;
+            }
+            return CompareSameType(other);
+        }
+
+        public Boolean Equals(Object other)
+        {
+            return (this.GetType() == other.GetType() && this.EqualsSameType(other));
+        }
+
+        public abstract int HashCode();
+
+        public override string ToString()
+        {
+            return Exists ? ToObject().ToString() : "(null)";
+        }
+    }
+}
