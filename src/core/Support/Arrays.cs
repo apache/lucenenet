8a8a1a6f78923f38943e651d85260b33d04c2b40
Paul Irwin <paulirwin@gmail.com>
2013-06-12 15:42:22 -0400

    Port: Add DaciukMihovAutomationBuilder

--- src/core/Support/Arrays.cs
+++ src/core/Support/Arrays.cs
@@ -45,5 +45,17 @@ namespace Lucene.Net.Support
 
             return true;
         }
+
+        public static T[] CopyOf<T>(T[] original, int newLength)
+        {
+            T[] newArray = new T[newLength];
+
+            for (int i = 0; i < Math.Min(original.Length, newLength); i++)
+            {
+                newArray[i] = original[i];
+            }
+
+            return newArray;
+        }
     }
 }
