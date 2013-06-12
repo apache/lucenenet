0242c4d8d0443f073f2ff24ee7e73b621b86d65f
Paul Irwin <paulirwin@gmail.com>
2013-06-11 20:34:53 -0400

    Port: Add IMutableBits

---
+++ src/core/Util/IMutableBits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Util
+{
+    /// <summary>
+    /// Extension of IBits for live documents.
+    /// </summary>
+    public interface IMutableBits : IBits
+    {
+        /// <summary>
+        /// Sets the bit specified by <paramref name="index"/> to false.
+        /// </summary>
+        /// <param name="index">index, should be non-negative and &lt; length. </param>
+        public void Clear(int index);
+    }
+}
