efcd753e5186f73bd0844aa60617ffd76bd2fcd2
James Blair <jmblair7@gmail.com>
2013-06-11 22:05:30 -0400

    Port: MutableValueInt, MutableValueFloat

--- src/core/Util/Mutable/MutableValueFloat.cs
+++ src/core/Util/Mutable/MutableValueFloat.cs
@@ -1,11 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Lucene.Net.Util.Mutable
 {
-    public class MutableValueFloat
+    public class MutableValueFloat : MutableValue
     {
+        public Single Value { get; set; }
+
+        public override void Copy(MutableValue source)
+        {
+            var s = source as MutableValueFloat;
+            Value = s.Value;
+            Exists = s.Exists;
+        }
+
+        public override MutableValue Duplicate()
+        {
+            return new MutableValueFloat { Value = Value, Exists = Exists };
+        }
+
+        public override Boolean EqualsSameType(object other)
+        {
+            var b = other as MutableValueFloat;
+            return Value == b.Value && Exists == b.Exists;
+        }
+
+        public override Int32 CompareSameType(object other)
+        {
+            var b = other as MutableValueFloat;
+            var c = Value.CompareTo(b.Value);
+            if (c != 0) return c;
+            if (Exists == b.Exists) return 0;
+            return Exists ? 1 : -1;
+        }
+
+        public override Object ToObject()
+        {
+            return Exists ? Value as Object : null;
+        }
+
+        public override Int32 HashCode()
+        {
+            return (Int32)BitConverter.DoubleToInt64Bits(Value);
+        }
     }
 }
