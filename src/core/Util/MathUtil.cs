c91f506f0c8c2435d2f0a49ff21f3e08cff7639c
Paul Irwin <paulirwin@gmail.com>
2013-06-11 20:30:12 -0400

    Port: Add MathUtil

---
+++ src/core/Util/MathUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Util
+{
+    public static class MathUtil
+    {
+        public static int Log(long x, int @base)
+        {
+            if (@base <= 1)
+            {
+                throw new ArgumentException("base must be > 1");
+            }
+            int ret = 0;
+            while (x >= @base)
+            {
+                x /= @base;
+                ret++;
+            }
+            return ret;
+        }
+    }
+}
