b4cb7f4a49cf5867d4a5cc5594cbb9be8809383f
Paul Irwin <paulirwin@gmail.com>
2013-06-12 12:32:56 -0400

    Port: Placeholders for VirtualMethod and WeakIdentityMap

---
+++ src/core/Util/VirtualMethod.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Util
+{
+    // VirtualMethod may not be needed. If it is needed, please implement here. 
+    // Otherwise, keep this file for comparing to the Java files.
+}
