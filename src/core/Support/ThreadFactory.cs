e7d0b2b10515e0f4482fc924e5f2fcbf004192a6
Paul Irwin <paulirwin@gmail.com>
2013-06-11 21:22:15 -0400

    Port: Add NamedThreadFactory

---
+++ src/core/Support/ThreadFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lucene.Net.Support
+{
+    public abstract class ThreadFactory
+    {
+        public abstract Thread NewThread(IThreadRunnable r);
+    }
+}
