8468a4bf42cb7b5d1b16fbe295ea572c394a2581
Paul Irwin <paulirwin@gmail.com>
2013-06-24 21:31:56 -0400

    Port: NoDeletionPolicy, NoMerge*

---
+++ src/core/Index/NoMergePolicy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MergeTrigger = Lucene.Net.Index.MergePolicy.MergeTrigger;
+using MergeSpecification = Lucene.Net.Index.MergePolicy.MergeSpecification;
+
+namespace Lucene.Net.Index
+{
+    public sealed class NoMergePolicy : MergePolicy
+    {
+        public static readonly MergePolicy NO_COMPOUND_FILES = new NoMergePolicy(false);
+
+        public static readonly MergePolicy COMPOUND_FILES = new NoMergePolicy(true);
+
+        private readonly bool useCompoundFile;
+
+        private NoMergePolicy(bool useCompoundFile)
+        {
+            // prevent instantiation
+            this.useCompoundFile = useCompoundFile;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+        }
+
+        public override MergeSpecification FindMerges(MergeTrigger? mergeTrigger, SegmentInfos segmentInfos)
+        {
+            return null;
+        }
+
+        public override MergeSpecification FindForcedMerges(SegmentInfos segmentInfos, int maxSegmentCount, IDictionary<SegmentInfoPerCommit, bool> segmentsToMerge)
+        {
+            return null;
+        }
+
+        public override MergeSpecification FindForcedDeletesMerges(SegmentInfos segmentInfos)
+        {
+            return null;
+        }
+
+        public override bool UseCompoundFile(SegmentInfos segments, SegmentInfoPerCommit newSegment)
+        {
+            return useCompoundFile;
+        }
+
+        public override void SetIndexWriter(IndexWriter writer)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "NoMergePolicy";
+        }
+    }
+}
---
+++ src/core/Index/NoMergeScheduler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Index
+{
+    public sealed class NoMergeScheduler : MergeScheduler
+    {
+        public static readonly MergeScheduler INSTANCE = new NoMergeScheduler();
+
+        private NoMergeScheduler()
+        {
+            // prevent instantiation
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+        }
+
+        public override void Merge(IndexWriter writer)
+        {
+        }
+
+        public override object Clone()
+        {
+            return this;
+        }
+    }
+}
