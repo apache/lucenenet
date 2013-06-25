a7ecdd1d54a967de51dd0f695431df652ad8dc30
James Blair <jmblair7@gmail.com>
2013-06-25 14:18:27 -0400

    Port: more Similarities classes
    AfterEffect, AfterEffectB, AfterEffectL

---
+++ src/core/Search/Similarities/AfterEffect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Search.Similarities
+{
+    public class AfterEffect
+    {
+        public AfterEffect() { }
+
+        public abstract float Score(BasicStats stats, float tfn);
+
+        public abstract Explanation Explain(BasicStats stats, float tfn);
+
+        public sealed class NoAfterEffect : AfterEffect
+        {
+
+            public NoAfterEffect() { }
+
+            public override sealed float Score(BasicStats stats, float tfn)
+            {
+                return 1f;
+            }
+
+            public override sealed Explanation Explain(BasicStats stats, float tfn)
+            {
+                return new Explanation(1, "no aftereffect");
+            }
+
+            public string ToString()
+            {
+                return "";
+            }
+        }
+
+        public abstract override string ToString();
+    }
+}
---
+++ src/core/Search/Similarities/AfterEffectL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Search.Similarities
+{
+    public class AfterEffectL : AfterEffect
+    {
+        public AfterEffectL() { }
+
+        public override sealed float Score(BasicStats stats, float tfn)
+        {
+            return 1 / (tfn + 1);
+        }
+
+        public override sealed Explanation Explain(BasicStats stats, float tfn)
+        {
+            Explanation result = new Explanation();
+            result.setDescription(this.GetType().Name + ", computed from: ");
+            result.setValue(score(stats, tfn));
+            result.addDetail(new Explanation(tfn, "tfn"));
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return "L";
+        }
+    }
+}
