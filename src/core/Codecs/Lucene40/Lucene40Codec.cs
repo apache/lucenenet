13f9d3dc0e0d3b52462d2a8b0494c6d7524dd620
Paul Irwin <paulirwin@gmail.com>
2013-06-28 15:29:36 -0400

    Placeholder for Lucene40Codec

---
+++ src/core/Codecs/Lucene40/Lucene40Codec.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Codecs.Lucene40
+{
+    [Obsolete]
+    public class Lucene40Codec : Codec
+    {
+    }
+}
