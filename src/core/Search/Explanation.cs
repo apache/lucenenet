27a6bef97cfdd04ade566d725a19d9bd82560470
Paul Irwin <paulirwin@gmail.com>
2013-07-02 18:02:54 -0400

    Port: Explanation

--- src/core/Search/Explanation.cs
+++ src/core/Search/Explanation.cs
@@ -17,65 +17,65 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Lucene.Net.Search
 {
-	
-	/// <summary>Expert: Describes the score computation for document and query. </summary>
-	[Serializable]
-	public class Explanation
-	{
-		private float value;        // the value of this node
-		private System.String description;  // what it represents
-		private List<Explanation> details;  // sub-explanations
-		
-		public Explanation()
-		{
-		}
-		
-		public Explanation(float value, System.String description)
-		{
-			this.value = value;
-			this.description = description;
-		}
+    /// <summary>Expert: Describes the score computation for document and query. </summary>
+    [Serializable]
+    public class Explanation
+    {
+        private float value;        // the value of this node
+        private string description;  // what it represents
+        private List<Explanation> details;  // sub-explanations
 
-	    /// <summary> Indicates whether or not this Explanation models a good match.
-	    /// 
-	    /// <p/>
-	    /// By default, an Explanation represents a "match" if the value is positive.
-	    /// <p/>
-	    /// </summary>
-	    /// <seealso cref="Value">
-	    /// </seealso>
-	    public virtual bool IsMatch
-	    {
-	        get { return (0.0f < Value); }
-	    }
-
-
-	    /// <summary>The value assigned to this explanation node. </summary>
-	    public virtual float Value
-	    {
-	        get { return value; }
-	        set { this.value = value; }
+        public Explanation()
+        {
         }
 
-	    /// <summary>A description of this explanation node. </summary>
-	    public virtual string Description
-	    {
-	        get { return description; }
-	        set { this.description = value; }
+        public Explanation(float value, string description)
+        {
+            this.value = value;
+            this.description = description;
         }
 
-	    /// <summary> A short one line summary which should contain all high level
-	    /// information about this Explanation, without the "Details"
-	    /// </summary>
-	    protected internal virtual string Summary
-	    {
-	        get { return Value + " = " + Description; }
-	    }
+        /// <summary> Indicates whether or not this Explanation models a good match.
+        /// 
+        /// <p/>
+        /// By default, an Explanation represents a "match" if the value is positive.
+        /// <p/>
+        /// </summary>
+        /// <seealso cref="Value">
+        /// </seealso>
+        public virtual bool IsMatch
+        {
+            get { return (0.0f < Value); }
+        }
 
-	    /// <summary>The sub-nodes of this explanation node. </summary>
+
+        /// <summary>The value assigned to this explanation node. </summary>
+        public virtual float Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        /// <summary>A description of this explanation node. </summary>
+        public virtual string Description
+        {
+            get { return description; }
+            set { this.description = value; }
+        }
+
+        /// <summary> A short one line summary which should contain all high level
+        /// information about this Explanation, without the "Details"
+        /// </summary>
+        protected internal virtual string Summary
+        {
+            get { return Value + " = " + Description; }
+        }
+
+        /// <summary>The sub-nodes of this explanation node. </summary>
         public virtual Explanation[] GetDetails()
         {
             if (details == null)
@@ -83,86 +83,65 @@ namespace Lucene.Net.Search
             return details.ToArray();
         }
 
-	    /// <summary>Adds a sub-node to this explanation node. </summary>
-		public virtual void  AddDetail(Explanation detail)
-		{
-			if (details == null)
-				details = new List<Explanation>();
-			details.Add(detail);
-		}
-		
-		/// <summary>Render an explanation as text. </summary>
-		public override System.String ToString()
-		{
-			return ToString(0);
-		}
+        /// <summary>Adds a sub-node to this explanation node. </summary>
+        public virtual void AddDetail(Explanation detail)
+        {
+            if (details == null)
+                details = new List<Explanation>();
+            details.Add(detail);
+        }
 
-		protected internal virtual System.String ToString(int depth)
-		{
-			System.Text.StringBuilder buffer = new System.Text.StringBuilder();
-			for (int i = 0; i < depth; i++)
-			{
-				buffer.Append("  ");
-			}
-			buffer.Append(Summary);
-			buffer.Append("\n");
-			
-			Explanation[] details = GetDetails();
-			if (details != null)
-			{
-				for (int i = 0; i < details.Length; i++)
-				{
-					buffer.Append(details[i].ToString(depth + 1));
-				}
-			}
-			
-			return buffer.ToString();
-		}
-		
-		/// <summary>Render an explanation as HTML. </summary>
-		public virtual System.String ToHtml()
-		{
-			System.Text.StringBuilder buffer = new System.Text.StringBuilder();
-			buffer.Append("<list>\n");
-			
-			buffer.Append("<item>");
-			buffer.Append(Summary);
-			buffer.Append("<br />\n");
-			
-			Explanation[] details = GetDetails();
-			if (details != null)
-			{
-				for (int i = 0; i < details.Length; i++)
-				{
-					buffer.Append(details[i].ToHtml());
-				}
-			}
-			
-			buffer.Append("</item>\n");
-			buffer.Append("</list>\n");
-			
-			return buffer.ToString();
-		}
-		
-		/// <summary> Small Util class used to pass both an idf factor as well as an
-		/// explanation for that factor.
-		/// 
-		/// This class will likely be held on a <see cref="Weight" />, so be aware 
-		/// before storing any large or un-serializable fields.
-		/// 
-		/// </summary>
-		[Serializable]
-		public abstract class IDFExplanation
-		{
-		    /// <value> the idf factor </value>
-		    public abstract float Idf { get; }
+        /// <summary>Render an explanation as text. </summary>
+        public override string ToString()
+        {
+            return ToString(0);
+        }
 
-		    /// <summary> This should be calculated lazily if possible.
-			/// 
-			/// </summary>
-			/// <returns> the explanation for the idf factor.
-			/// </returns>
-			public abstract System.String Explain();
-		}
-	}
+        protected internal virtual string ToString(int depth)
+        {
+            StringBuilder buffer = new StringBuilder();
+            for (int i = 0; i < depth; i++)
+            {
+                buffer.Append("  ");
+            }
+            buffer.Append(Summary);
+            buffer.Append("\n");
+
+            Explanation[] details = GetDetails();
+            if (details != null)
+            {
+                for (int i = 0; i < details.Length; i++)
+                {
+                    buffer.Append(details[i].ToString(depth + 1));
+                }
+            }
+
+            return buffer.ToString();
+        }
+
+        /// <summary>Render an explanation as HTML. </summary>
+        public virtual string ToHtml()
+        {
+            StringBuilder buffer = new StringBuilder();
+            buffer.Append("<ul>\n");
+
+            buffer.Append("<li>");
+            buffer.Append(Summary);
+            buffer.Append("<br />\n");
+
+            Explanation[] details = GetDetails();
+            if (details != null)
+            {
+                for (int i = 0; i < details.Length; i++)
+                {
+                    buffer.Append(details[i].ToHtml());
+                }
+            }
+
+            buffer.Append("</li>\n");
+            buffer.Append("</ul>\n");
+
+            return buffer.ToString();
+        }
+    }
 }
