dd1d5c13c07056fcd634db0e925a3c54efb92af6
Paul Irwin <paulirwin@gmail.com>
2013-07-04 12:02:32 -0400

    Start of unit testing work...
    this is going to be fun. :-(

---
+++ test/test-framework/Support/RandomizedTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.TestFramework.Support
+{
+    public class RandomizedTest
+    {
+        public static bool SystemPropertyAsBoolean(string key, bool defaultValue)
+        {
+            var setting = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrEmpty(setting))
+                return defaultValue;
+
+            bool v;
+
+            if (bool.TryParse(setting, out v))
+                return v;
+
+            return defaultValue;
+        }
+
+        public static int SystemPropertyAsInt(string key, int defaultValue)
+        {
+            var setting = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrEmpty(setting))
+                return defaultValue;
+
+            int v;
+
+            if (int.TryParse(setting, out v))
+                return v;
+
+            return defaultValue;
+        }
+    }
+}
