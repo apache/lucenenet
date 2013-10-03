819bcf47ca95e8358bd439469c51fee1d595ebfe
Paul Irwin <paulirwin@gmail.com>
2013-10-03 12:36:21 -0400

    Port QueryParsers.Ext namespace

---
+++ src/contrib/QueryParsers/Ext/ParserExtension.cs
@@ -0,0 +1,14 @@
+﻿using Lucene.Net.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucene.Net.QueryParsers.Ext
+{
+    public abstract class ParserExtension
+    {
+        public abstract Query Parse(ExtensionQuery query);
+    }
+}
