08d05cc7f71e2bfc872b44f6aa3a8b4a7758a1cf
Russell Trupiano <russelltrupiano@gmail.com>
2014-05-28 18:41:10 -0700

    Added Support files

---
+++ src/core/Support/ICharSequence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Support
+{
+    public interface ICharSequence
+    {
+        int Length { get; }
+
+        char CharAt(int index);
+
+        ICharSequence SubSequence(int start, int end);
+    }
+}
---
+++ src/core/Support/MathExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.Support
+{
+    class MathExtension
+    {
+        /// <summary>
+        /// Convert to Radians.
+        /// </summary>
+        /// <param name="val">The value to convert to radians</param>
+        /// <returns>The value in radians</returns>
+        public static double ToRadians(this double val) {
+            return (Math.PI / 180) * val;
+        }
+    }
+}
