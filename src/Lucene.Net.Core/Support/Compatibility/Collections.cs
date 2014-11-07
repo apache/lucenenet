e07d113722767b141d986ed0c2f73daf2e792198
Itamar Syn-Hershko <itamar@code972.com>
2014-11-07 05:59:40 +0200

    Start bringing in BCL bits to reuse available bits

---
+++ src/Lucene.Net.Core/Support/Compatibility/Collections.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Lucene.Net
+{
+    public static class Collections
+    {
+        public static ISet<T> Singleton<T>(T o)
+        {
+            return ImmutableHashSet.Create(o);
+        }
+    }
+}
