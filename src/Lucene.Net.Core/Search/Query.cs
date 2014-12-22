c0c101953e14398a8e3f01f185a3ecfcecbc1609
Itamar Syn-Hershko <itamar@code972.com>
2014-12-22 15:09:35 +0200

    Bring Query.Equals() to be back in par with the Java impl

--- src/Lucene.Net.Core/Search/Query.cs
+++ src/Lucene.Net.Core/Search/Query.cs
@@ -134,12 +134,17 @@ namespace Lucene.Net.Search
                 return true;
             }
 
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
             var other = obj as Query;
             if (other == null)
             {
                 return false;
             }
-           
+
             if (Number.FloatToIntBits(Boost) != Number.FloatToIntBits(other.Boost))
             {
                 return false;
