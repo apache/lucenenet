b5568a32097a0ea06c7670b808e163c1f225e7e0
Laimonas Simutis <laimis@gmail.com>
2014-12-23 21:43:22 -0500

    formatting

--- src/Lucene.Net.Tests.Classification/Utils/DataSplitterTest.cs
+++ src/Lucene.Net.Tests.Classification/Utils/DataSplitterTest.cs
@@ -1,3 +1,4 @@
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -26,120 +27,120 @@ using NUnit.Framework;
 
 namespace Lucene.Net.Classification
 {
-/**
- * Testcase for {@link org.apache.lucene.classification.utils.DatasetSplitter}
- */
-    public class DataSplitterTest : Util.LuceneTestCase 
+    /**
+     * Testcase for {@link org.apache.lucene.classification.utils.DatasetSplitter}
+     */
+    public class DataSplitterTest : Util.LuceneTestCase
     {
-      private AtomicReader _originalIndex;
-      private RandomIndexWriter _indexWriter;
-      private Directory _dir;
+        private AtomicReader _originalIndex;
+        private RandomIndexWriter _indexWriter;
+        private Directory _dir;
 
-      private String _textFieldName = "text";
-      private String _classFieldName = "class";
-      private String _idFieldName = "id";
+        private String _textFieldName = "text";
+        private String _classFieldName = "class";
+        private String _idFieldName = "id";
 
-      [SetUp]
-      public override void SetUp()
-      {
-        base.SetUp();
-        _dir = NewDirectory();
-        _indexWriter = new RandomIndexWriter(Random(), _dir, new MockAnalyzer(Random()));
-
-        FieldType ft = new FieldType(TextField.TYPE_STORED);
-        ft.StoreTermVectors = true;
-        ft.StoreTermVectorOffsets = true;
-        ft.StoreTermVectorPositions = true;
-
-        Analyzer analyzer = new MockAnalyzer(Random());
-
-        Document doc;
-        for (int i = 0; i < 100; i++) 
+        [SetUp]
+        public override void SetUp()
         {
-          doc = new Document();
-          doc.Add(new Field(_idFieldName, Random().toString(), ft));
-          doc.Add(new Field(_textFieldName, new StringBuilder(Random().toString()).append(Random().toString()).append(
-              Random().toString()).toString(), ft));
-          doc.Add(new Field(_classFieldName, Random().toString(), ft));
-          _indexWriter.AddDocument(doc, analyzer);
+            base.SetUp();
+            _dir = NewDirectory();
+            _indexWriter = new RandomIndexWriter(Random(), _dir, new MockAnalyzer(Random()));
+
+            FieldType ft = new FieldType(TextField.TYPE_STORED);
+            ft.StoreTermVectors = true;
+            ft.StoreTermVectorOffsets = true;
+            ft.StoreTermVectorPositions = true;
+
+            Analyzer analyzer = new MockAnalyzer(Random());
+
+            Document doc;
+            for (int i = 0; i < 100; i++)
+            {
+                doc = new Document();
+                doc.Add(new Field(_idFieldName, Random().toString(), ft));
+                doc.Add(new Field(_textFieldName, new StringBuilder(Random().toString()).append(Random().toString()).append(
+                    Random().toString()).toString(), ft));
+                doc.Add(new Field(_classFieldName, Random().toString(), ft));
+                _indexWriter.AddDocument(doc, analyzer);
+            }
+
+            _indexWriter.Commit();
+
+            _originalIndex = SlowCompositeReaderWrapper.Wrap(_indexWriter.Reader);
         }
 
-        _indexWriter.Commit();
-
-        _originalIndex = SlowCompositeReaderWrapper.Wrap(_indexWriter.Reader);
-      }
-
-      [TearDown]
-      public override void TearDown()
-      {
-        _originalIndex.Dispose();
-        _indexWriter.Dispose();
-        _dir.Dispose();
-        base.TearDown();
-      }
-
-      [Test]
-      public void TestSplitOnAllFields() 
-      {
-        AssertSplit(_originalIndex, 0.1, 0.1);
-      }
-
-
-      [Test]
-      public void TestSplitOnSomeFields() 
-      {
-        AssertSplit(_originalIndex, 0.2, 0.35, _idFieldName, _textFieldName);
-      }
-
-      public static void AssertSplit(AtomicReader originalIndex, double testRatio, double crossValidationRatio, params string[] fieldNames) 
-      {
-        BaseDirectoryWrapper trainingIndex = NewDirectory();
-        BaseDirectoryWrapper testIndex = NewDirectory();
-        BaseDirectoryWrapper crossValidationIndex = NewDirectory();
-
-        try 
+        [TearDown]
+        public override void TearDown()
         {
-          DatasetSplitter datasetSplitter = new DatasetSplitter(testRatio, crossValidationRatio);
-          datasetSplitter.Split(originalIndex, trainingIndex, testIndex, crossValidationIndex, new MockAnalyzer(Random()), fieldNames);
-
-          NotNull(trainingIndex);
-          NotNull(testIndex);
-          NotNull(crossValidationIndex);
-
-          DirectoryReader trainingReader = DirectoryReader.Open(trainingIndex);
-          True((int) (originalIndex.MaxDoc * (1d - testRatio - crossValidationRatio)) == trainingReader.MaxDoc);
-          DirectoryReader testReader = DirectoryReader.Open(testIndex);
-          True((int) (originalIndex.MaxDoc * testRatio) == testReader.MaxDoc);
-          DirectoryReader cvReader = DirectoryReader.Open(crossValidationIndex);
-          True((int) (originalIndex.MaxDoc * crossValidationRatio) == cvReader.MaxDoc);
-
-          trainingReader.Dispose();
-          testReader.Dispose();
-          cvReader.Dispose();
-          CloseQuietly(trainingReader);
-          CloseQuietly(testReader);
-          CloseQuietly(cvReader);
-        } 
-        finally 
-        {
-          trainingIndex.Dispose();
-          testIndex.Dispose();
-          crossValidationIndex.Dispose();
+            _originalIndex.Dispose();
+            _indexWriter.Dispose();
+            _dir.Dispose();
+            base.TearDown();
         }
-      }
 
-      private static void CloseQuietly(IndexReader reader)
-      {
-        try 
+        [Test]
+        public void TestSplitOnAllFields()
         {
-          if (reader != null)
-            reader.Dispose();
-        } 
-        catch (Exception e) 
-        {
-          // do nothing
+            AssertSplit(_originalIndex, 0.1, 0.1);
         }
-      }
+
+
+        [Test]
+        public void TestSplitOnSomeFields()
+        {
+            AssertSplit(_originalIndex, 0.2, 0.35, _idFieldName, _textFieldName);
+        }
+
+        public static void AssertSplit(AtomicReader originalIndex, double testRatio, double crossValidationRatio, params string[] fieldNames)
+        {
+            BaseDirectoryWrapper trainingIndex = NewDirectory();
+            BaseDirectoryWrapper testIndex = NewDirectory();
+            BaseDirectoryWrapper crossValidationIndex = NewDirectory();
+
+            try
+            {
+                DatasetSplitter datasetSplitter = new DatasetSplitter(testRatio, crossValidationRatio);
+                datasetSplitter.Split(originalIndex, trainingIndex, testIndex, crossValidationIndex, new MockAnalyzer(Random()), fieldNames);
+
+                NotNull(trainingIndex);
+                NotNull(testIndex);
+                NotNull(crossValidationIndex);
+
+                DirectoryReader trainingReader = DirectoryReader.Open(trainingIndex);
+                True((int)(originalIndex.MaxDoc * (1d - testRatio - crossValidationRatio)) == trainingReader.MaxDoc);
+                DirectoryReader testReader = DirectoryReader.Open(testIndex);
+                True((int)(originalIndex.MaxDoc * testRatio) == testReader.MaxDoc);
+                DirectoryReader cvReader = DirectoryReader.Open(crossValidationIndex);
+                True((int)(originalIndex.MaxDoc * crossValidationRatio) == cvReader.MaxDoc);
+
+                trainingReader.Dispose();
+                testReader.Dispose();
+                cvReader.Dispose();
+                CloseQuietly(trainingReader);
+                CloseQuietly(testReader);
+                CloseQuietly(cvReader);
+            }
+            finally
+            {
+                trainingIndex.Dispose();
+                testIndex.Dispose();
+                crossValidationIndex.Dispose();
+            }
+        }
+
+        private static void CloseQuietly(IndexReader reader)
+        {
+            try
+            {
+                if (reader != null)
+                    reader.Dispose();
+            }
+            catch (Exception e)
+            {
+                // do nothing
+            }
+        }
     }
 
 }
