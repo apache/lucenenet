f9f2b1313da397a81a7b35deca843a577187d44d
Hakeem S Mohammad <hakeemosrc@gmail.com>
2015-01-23 22:27:46 -0500

    merge conflicts resolved. recursion tests set to ignore

--- src/Lucene.Net.TestFramework/Util/LuceneTestCase.cs
+++ src/Lucene.Net.TestFramework/Util/LuceneTestCase.cs
@@ -15,6 +15,7 @@
 * limitations under the License.
 */
 
+using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
@@ -189,7 +190,7 @@ namespace Lucene.Net.Util
     /// </ul>
     /// </summary>
     [TestFixture]
-    public abstract class LuceneTestCase : Assert // Wait long for leaked threads to complete before failure. zk needs this. -  See LUCENE-3995 for rationale.
+    public abstract partial class LuceneTestCase : Assert // Wait long for leaked threads to complete before failure. zk needs this. -  See LUCENE-3995 for rationale.
     {
         public static System.IO.FileInfo TEMP_DIR;
 
@@ -216,54 +217,7 @@ namespace Lucene.Net.Util
         /// <seealso> cref= #ignoreAfterMaxFailures </seealso>
         public const string SYSPROP_FAILFAST = "tests.failfast";
 
-        public interface Nightly
-        {
-        }
-
-        public interface Weekly
-        {
-        }
-
-        /*/// <summary>
-      /// Annotation for tests that should only be run during nightly builds.
-      /// </summary>
-      public class Nightly : System.Attribute
-      /// <summary>
-      /// Annotation for tests that should only be run during weekly builds
-      /// </summary>
-      {
-          private readonly LuceneTestCase OuterInstance;
-
-          public Nightly(LuceneTestCase outerInstance)
-          {
-              this.OuterInstance = outerInstance;
-          }
-      }
-      public class Weekly : System.Attribute
-      /// <summary>
-      /// Annotation for tests which exhibit a known issue and are temporarily disabled.
-      /// </summary>
-      {
-          private readonly LuceneTestCase OuterInstance;
-
-          public Weekly(LuceneTestCase outerInstance)
-          {
-              this.OuterInstance = outerInstance;
-          }
-      }
-      public class AwaitsFix : System.Attribute
-      {
-          private readonly LuceneTestCase OuterInstance;
-
-          public AwaitsFix(LuceneTestCase outerInstance)
-          {
-              this.OuterInstance = outerInstance;
-          }
-
-        /// <summary>
-        /// Point to JIRA entry. </summary>
-        public string bugUrl();
-      }
+        /*     
 
       /// <summary>
       /// Annotation for tests that are slow. Slow tests do run by default but can be
@@ -485,10 +439,11 @@ namespace Lucene.Net.Util
         /// </summary>
         private static TestRuleSetupAndRestoreClassEnv ClassEnvRule;
 
+        // LUCENENET TODO
         /// <summary>
         /// Suite failure marker (any error in the test or suite scope).
         /// </summary>
-        //public static TestRuleMarkFailure SuiteFailureMarker;
+        public static /*TestRuleMarkFailure*/ bool SuiteFailureMarker = true; // Means: was successful
 
         /// <summary>
         /// Ignore tests after hitting a designated number of initial failures. this
@@ -623,6 +578,7 @@ namespace Lucene.Net.Util
             /* LUCENE TO-DO: Not sure how to convert these
                 ParentChainCallRule.TeardownCalled = true;
                 */
+            CleanupTemporaryFiles();
         }
 
         // -----------------------------------------------------------------
@@ -682,7 +638,6 @@ namespace Lucene.Net.Util
         {
             get
             {
-                //return ClassNameRule.TestClass;
                 return typeof(LuceneTestCase);
             }
         }
@@ -2644,7 +2599,7 @@ namespace Lucene.Net.Util
         /// A queue of temporary resources to be removed after the
         /// suite completes. </summary>
         /// <seealso cref= #registerToRemoveAfterSuite(File) </seealso>
-        private static List<FileSystemInfo> CleanupQueue = new List<FileSystemInfo>();
+        private static readonly ConcurrentQueue<string> CleanupQueue = new ConcurrentQueue<string>();
 
         /// <summary>
         /// Register temporary folder for removal after the suite completes.
@@ -2659,10 +2614,7 @@ namespace Lucene.Net.Util
                 return;
             }
 
-            lock (CleanupQueue)
-            {
-                CleanupQueue.Add(f);
-            }
+            CleanupQueue.Enqueue(f.FullName);
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
@@ -2674,59 +2626,47 @@ namespace Lucene.Net.Util
             return string.Format("{0}+{1}", this.GetType().Name, sf.GetMethod().Name);
         }
 
-        /*private static class TemporaryFilesCleanupRule : TestRuleAdapter
+        private void CleanupTemporaryFiles()
         {
-            protected void Before()
+            // Drain cleanup queue and clear it.
+            var tempDirBasePath = (TempDirBase != null ? TempDirBase.FullName : null);
+            TempDirBase = null;
+
+            // Only check and throw an IOException on un-removable files if the test
+            // was successful. Otherwise just report the path of temporary files
+            // and leave them there.
+            if (LuceneTestCase.SuiteFailureMarker /*.WasSuccessful()*/)
             {
-                base.Before();
-                Debug.Assert(TempDirBase == null);
-            }
-
-            protected void AfterAlways(IList<Exception> errors)
-            {
-                // Drain cleanup queue and clear it.
-                DirectoryInfo[] everything;
-                string tempDirBasePath;
-                lock (CleanupQueue)
-                {
-                    tempDirBasePath = (TempDirBase != null ? TempDirBase.FullName : null);
-                    TempDirBase = null;
-
-                    CleanupQueue.Reverse();
-                    everything = new DirectoryInfo[CleanupQueue.Count];
-                    CleanupQueue.ToArray(everything);
-                    CleanupQueue.Clear();
-                }
-
-                // Only check and throw an IOException on un-removable files if the test
-                // was successful. Otherwise just report the path of temporary files
-                // and leave them there.
-                if (LuceneTestCase.SuiteFailureMarker.WasSuccessful())
+                string f;
+                while (CleanupQueue.TryDequeue(out f))
                 {
                     try
                     {
-                        TestUtil.Rm(everything);
+                        if (System.IO.Directory.Exists(f))
+                            System.IO.Directory.Delete(f, true);
+                        else if (System.IO.File.Exists(f))
+                            File.Delete(f);
                     }
-                    catch (IOException e)
+                    catch (IOException)
                     {
-                        Type suiteClass = RandomizedContext.Current.GetTargetType;
-                        if (suiteClass.isAnnotationPresent(typeof(SuppressTempFileChecks)))
-                        {
-                            Console.Error.WriteLine("WARNING: Leftover undeleted temporary files (bugUrl: " + suiteClass.GetAnnotation(typeof(SuppressTempFileChecks)).bugUrl() + "): " + e.Message);
-                            return;
-                        }
-                        throw e;
-                    }
-                }
-                else
-                {
-                    if (tempDirBasePath != null)
-                    {
-                        Console.Error.WriteLine("NOTE: leaving temporary files on disk at: " + tempDirBasePath);
+                        //                    Type suiteClass = RandomizedContext.Current.GetTargetType;
+                        //                    if (suiteClass.IsAnnotationPresent(typeof(SuppressTempFileChecks)))
+                        //                    {
+                        //                        Console.Error.WriteLine("WARNING: Leftover undeleted temporary files (bugUrl: " + suiteClass.GetAnnotation(typeof(SuppressTempFileChecks)).bugUrl() + "): " + e.Message);
+                        //                        return;
+                        //                    }
+                        throw;
                     }
                 }
             }
-        }*/
+            else
+            {
+                if (tempDirBasePath != null)
+                {
+                    Console.Error.WriteLine("NOTE: leaving temporary files on disk at: " + tempDirBasePath);
+                }
+            }
+        }
     }
 
     /*internal class ReaderClosedListenerAnonymousInnerClassHelper : IndexReader.ReaderClosedListener
@@ -2755,7 +2695,7 @@ namespace Lucene.Net.Util
 
         public virtual int Compare(object arg0, object arg1)
         {
-            return ((IndexableField)arg0).Name().CompareTo(((IndexableField)arg1).Name());
+            return System.String.Compare(((IndexableField)arg0).Name(), ((IndexableField)arg1).Name(), System.StringComparison.Ordinal);
         }
     }
 }
