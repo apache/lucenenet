1c914dc211f01cf1841320873dd1d37561a2a890
Hakeem S Mohammad <hakeemosrc@gmail.com>
2015-01-28 21:38:11 -0500

    indentation

--- src/Lucene.Net.Expressions/ExpressionComparator.cs
+++ src/Lucene.Net.Expressions/ExpressionComparator.cs
@@ -9,86 +9,86 @@ using Lucene.Net.Support;
 
 namespace Lucene.Net.Expressions
 {
-	/// <summary>A custom comparator for sorting documents by an expression</summary>
-	internal class ExpressionComparator : FieldComparator<double>
-	{
-		private readonly double[] values;
+    /// <summary>A custom comparator for sorting documents by an expression</summary>
+    internal class ExpressionComparator : FieldComparator<double>
+    {
+        private readonly double[] values;
 
-		private double bottom;
+        private double bottom;
 
-		private double topValue;
+        private double topValue;
 
-		private ValueSource source;
+        private ValueSource source;
 
-		private FunctionValues scores;
+        private FunctionValues scores;
 
-		private AtomicReaderContext readerContext;
+        private AtomicReaderContext readerContext;
 
-		public ExpressionComparator(ValueSource source, int numHits)
-		{
-			values = new double[numHits];
-			this.source = source;
-		}
+        public ExpressionComparator(ValueSource source, int numHits)
+        {
+            values = new double[numHits];
+            this.source = source;
+        }
 
-		// TODO: change FieldComparator.setScorer to throw IOException and remove this try-catch
-		public override Scorer Scorer
-		{
-		    set
-		    {
-		        base.Scorer = value;
-		        // TODO: might be cleaner to lazy-init 'source' and set scorer after?
-		        
-		        Debug.Assert(readerContext != null);
-		        var context = new Dictionary<string, object>();
-		        Debug.Assert(value != null);
-		        context["scorer"] = value;
-		        scores = source.GetValues(context, readerContext);
-		    }
-		}
+        // TODO: change FieldComparator.setScorer to throw IOException and remove this try-catch
+        public override Scorer Scorer
+        {
+            set
+            {
+                base.Scorer = value;
+                // TODO: might be cleaner to lazy-init 'source' and set scorer after?
 
-		public override int Compare(int slot1, int slot2)
-		{
+                Debug.Assert(readerContext != null);
+                var context = new Dictionary<string, object>();
+                Debug.Assert(value != null);
+                context["scorer"] = value;
+                scores = source.GetValues(context, readerContext);
+            }
+        }
+
+        public override int Compare(int slot1, int slot2)
+        {
             return values[slot1].CompareTo(values[slot2]);
-		}
+        }
 
-		public override int Bottom
-		{
-		    set { bottom = values[value]; }
-		}
+        public override int Bottom
+        {
+            set { bottom = values[value]; }
+        }
 
-		public override object TopValue
-		{
-		    set { topValue = (double) value; }
-		}
+        public override object TopValue
+        {
+            set { topValue = (double)value; }
+        }
 
-		
-		public override int CompareBottom(int doc)
-		{
-			return bottom.CompareTo(scores.DoubleVal(doc));
-		}
 
-		
-		public override void Copy(int slot, int doc)
-		{
-			values[slot] = scores.DoubleVal(doc);
-		}
+        public override int CompareBottom(int doc)
+        {
+            return bottom.CompareTo(scores.DoubleVal(doc));
+        }
 
-		
-		public override FieldComparator SetNextReader(AtomicReaderContext context)
-		{
-			this.readerContext = context;
-			return this;
-		}
 
-		public override IComparable Value(int slot)
-		{
-			return (values[slot]);
-		}
+        public override void Copy(int slot, int doc)
+        {
+            values[slot] = scores.DoubleVal(doc);
+        }
 
-		
-		public override int CompareTop(int doc)
-		{
-			return topValue.CompareTo(scores.DoubleVal(doc));
-		}
-	}
+
+        public override FieldComparator SetNextReader(AtomicReaderContext context)
+        {
+            this.readerContext = context;
+            return this;
+        }
+
+        public override IComparable Value(int slot)
+        {
+            return (values[slot]);
+        }
+
+
+        public override int CompareTop(int doc)
+        {
+            return topValue.CompareTo(scores.DoubleVal(doc));
+        }
+    }
 }
--- src/Lucene.Net.Expressions/ExpressionRescorer.cs
+++ src/Lucene.Net.Expressions/ExpressionRescorer.cs
@@ -5,115 +5,116 @@ using Lucene.Net.Search;
 
 namespace Lucene.Net.Expressions
 {
-	/// <summary>
-	/// A
-	/// <see cref="Lucene.Net.Search.Rescorer">Lucene.Net.Search.Rescorer</see>
-	/// that uses an expression to re-score
-	/// first pass hits.  Functionally this is the same as
-	/// <see cref="Lucene.Net.Search.SortRescorer">Lucene.Net.Search.SortRescorer
-	/// 	</see>
-	/// (if you build the
-	/// <see cref="Lucene.Net.Search.Sort">Lucene.Net.Search.Sort</see>
-	/// using
-	/// <see cref="Expression.GetSortField(Bindings, bool)">Expression.GetSortField(Bindings, bool)
-	/// 	</see>
-	/// ), except for the explain method
-	/// which gives more detail by showing the value of each
-	/// variable.
-	/// </summary>
-	/// <lucene.experimental></lucene.experimental>
-	internal class ExpressionRescorer : SortRescorer
-	{
-		private readonly Expression expression;
+    /// <summary>
+    /// A
+    /// <see cref="Lucene.Net.Search.Rescorer">Lucene.Net.Search.Rescorer</see>
+    /// that uses an expression to re-score
+    /// first pass hits.  Functionally this is the same as
+    /// <see cref="Lucene.Net.Search.SortRescorer">Lucene.Net.Search.SortRescorer
+    /// 	</see>
+    /// (if you build the
+    /// <see cref="Lucene.Net.Search.Sort">Lucene.Net.Search.Sort</see>
+    /// using
+    /// <see cref="Expression.GetSortField(Bindings, bool)">Expression.GetSortField(Bindings, bool)
+    /// 	</see>
+    /// ), except for the explain method
+    /// which gives more detail by showing the value of each
+    /// variable.
+    /// </summary>
+    /// <lucene.experimental></lucene.experimental>
+    internal class ExpressionRescorer : SortRescorer
+    {
+        private readonly Expression expression;
 
-		private readonly Bindings bindings;
+        private readonly Bindings bindings;
 
-		/// <summary>
-		/// Uses the provided
-		/// <see cref="Lucene.Net.Queries.Function.ValueSource">Lucene.Net.Queries.Function.ValueSource
-		/// 	</see>
-		/// to assign second
-		/// pass scores.
-		/// </summary>
-		public ExpressionRescorer(Expression expression, Bindings bindings) : base(new Sort
-			(expression.GetSortField(bindings, true)))
-		{
-			this.expression = expression;
-			this.bindings = bindings;
-		}
+        /// <summary>
+        /// Uses the provided
+        /// <see cref="Lucene.Net.Queries.Function.ValueSource">Lucene.Net.Queries.Function.ValueSource
+        /// 	</see>
+        /// to assign second
+        /// pass scores.
+        /// </summary>
+        public ExpressionRescorer(Expression expression, Bindings bindings)
+            : base(new Sort
+                (expression.GetSortField(bindings, true)))
+        {
+            this.expression = expression;
+            this.bindings = bindings;
+        }
 
-		private class FakeScorer : Scorer
-		{
-			internal float score;
+        private class FakeScorer : Scorer
+        {
+            internal float score;
 
-			internal int doc = -1;
+            internal int doc = -1;
 
-			internal int freq = 1;
+            internal int freq = 1;
 
-			public FakeScorer() : base(null)
-			{
-			}
+            public FakeScorer()
+                : base(null)
+            {
+            }
 
-			public override int Advance(int target)
-			{
-				throw new NotSupportedException("FakeScorer doesn't support advance(int)");
-			}
+            public override int Advance(int target)
+            {
+                throw new NotSupportedException("FakeScorer doesn't support advance(int)");
+            }
 
-			public override int DocID()
-			{
-				return doc;
-			}
+            public override int DocID()
+            {
+                return doc;
+            }
 
-			public override int Freq()
-			{
-				return freq;
-			}
+            public override int Freq()
+            {
+                return freq;
+            }
 
-			public override int NextDoc()
-			{
-				throw new NotSupportedException("FakeScorer doesn't support nextDoc()");
-			}
+            public override int NextDoc()
+            {
+                throw new NotSupportedException("FakeScorer doesn't support nextDoc()");
+            }
 
-			public override float Score()
-			{
-				return score;
-			}
+            public override float Score()
+            {
+                return score;
+            }
 
-			public override long Cost()
-			{
-				return 1;
-			}
+            public override long Cost()
+            {
+                return 1;
+            }
 
-			public override Weight Weight
-			{
-			    get { throw new NotSupportedException(); }
-			}
+            public override Weight Weight
+            {
+                get { throw new NotSupportedException(); }
+            }
 
-			public override ICollection<Scorer.ChildScorer> Children
-			{
-			    get { throw new NotSupportedException(); }
-			}
-		}
+            public override ICollection<Scorer.ChildScorer> Children
+            {
+                get { throw new NotSupportedException(); }
+            }
+        }
 
-		/// <exception cref="System.IO.IOException"></exception>
-		public override Explanation Explain(IndexSearcher searcher, Explanation firstPassExplanation
-			, int docID)
-		{
-			Explanation result = base.Explain(searcher, firstPassExplanation, docID);
-			IList<AtomicReaderContext> leaves = searcher.IndexReader.Leaves;
-			int subReader = ReaderUtil.SubIndex(docID, leaves);
-			AtomicReaderContext readerContext = leaves[subReader];
-			int docIDInSegment = docID - readerContext.DocBase;
-			var context = new Dictionary<string, object>();
-			var fakeScorer = new FakeScorer {score = firstPassExplanation.Value, doc = docIDInSegment};
-		    context["scorer"] = fakeScorer;
-			foreach (string variable in expression.variables)
-			{
-				result.AddDetail(new Explanation((float)bindings.GetValueSource(variable).GetValues
-					(context, readerContext).DoubleVal(docIDInSegment), "variable \"" + variable + "\""
-					));
-			}
-			return result;
-		}
-	}
+
+        public override Explanation Explain(IndexSearcher searcher, Explanation firstPassExplanation, int docID)
+        {
+            Explanation result = base.Explain(searcher, firstPassExplanation, docID);
+            IList<AtomicReaderContext> leaves = searcher.IndexReader.Leaves;
+            int subReader = ReaderUtil.SubIndex(docID, leaves);
+            AtomicReaderContext readerContext = leaves[subReader];
+            int docIDInSegment = docID - readerContext.DocBase;
+            var context = new Dictionary<string, object>();
+            var fakeScorer = new FakeScorer { score = firstPassExplanation.Value, doc = docIDInSegment };
+            context["scorer"] = fakeScorer;
+            foreach (string variable in expression.variables)
+            {
+                result.AddDetail(new Explanation((float)bindings.GetValueSource(variable).GetValues
+                    (context, readerContext).DoubleVal(docIDInSegment), "variable \"" + variable + "\""
+                    ));
+            }
+            return result;
+        }
+    }
 }
