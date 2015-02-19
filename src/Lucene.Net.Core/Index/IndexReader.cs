21633d86c197e7aeabd9b57dfea4bcae66b50834
Chand2048 <Chand@Microsoft.com>
2015-02-18 17:19:12 -0800

    Fix for TestIndexReaderClose.TestCloseUnderException
    The IndexReader.NotifyReaderClosedListeners() did not properly suppress the exceptions thrown from the call to listener.OnClose(). Unfortunately there is no place to track the suppressed exception so it is just dropped.

--- src/Lucene.Net.Core/Index/IndexReader.cs
+++ src/Lucene.Net.Core/Index/IndexReader.cs
@@ -148,22 +148,22 @@ namespace Lucene.Net.Index
             {
                 foreach (ReaderClosedListener listener in ReaderClosedListeners)
                 {
-                    // LUCENENET TODO
-                    /*try
-                    {*/
-                    listener.OnClose(this);
-                    /*}
+                    try
+                    {
+                        listener.OnClose(this);
+                    }
                     catch (Exception t)
                     {
-                      if (th == null)
-                      {
-                        th = t;
-                      }
-                      else
-                      {
-                        th.AddSuppressed(t);
-                      }
-                    }*/
+                        if (th == null)
+                        {
+                            th = t;
+                        }
+                        else
+                        {
+                            //th.AddSuppressed(t);
+                            // No palce to put the suppressed exception.
+                        }
+                    }
                 }
                 IOUtils.ReThrowUnchecked(th);
             }
