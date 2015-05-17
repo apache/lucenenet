ae1fc2a32b4eb3a347b301c8cb62dfad85acd6db
Laimonas Simutis <laimis@gmail.com>
2015-05-17 12:10:26 -0400

    synchronize access to underlying file stream for NIOFSDirectory

--- src/Lucene.Net.Core/Support/FileStreamExtensions.cs
+++ src/Lucene.Net.Core/Support/FileStreamExtensions.cs
@@ -4,31 +4,36 @@ namespace Lucene.Net.Support
 {
     public static class FileStreamExtensions
     {
+        private static object _fsReadLock = new object();
+
         //Reads bytes from the Filestream into the bytebuffer
         public static int Read(this FileStream file, ByteBuffer dst, long position)
         {
-            // TODO: check this logic, could probably optimize
-            if (position >= file.Length)
-                return 0;
-
-            var original = file.Position;
-
-            file.Seek(position, SeekOrigin.Begin);
-
-            int count = 0;
-
-            for (int i = dst.Position; i < dst.Limit; i++)
+            lock (_fsReadLock)
             {
-                int v = file.ReadByte();
-                if (v == -1)
-                    break;
-                dst.Put((byte)v);
-                count++;
+                // TODO: check this logic, could probably optimize
+                if (position >= file.Length)
+                    return 0;
+
+                var original = file.Position;
+
+                file.Seek(position, SeekOrigin.Begin);
+
+                int count = 0;
+
+                for (int i = dst.Position; i < dst.Limit; i++)
+                {
+                    int v = file.ReadByte();
+                    if (v == -1)
+                        break;
+                    dst.Put((byte) v);
+                    count++;
+                }
+
+                file.Seek(original, SeekOrigin.Begin);
+
+                return count;
             }
-
-            file.Seek(original, SeekOrigin.Begin);
-
-            return count;
         }
     }
 }
