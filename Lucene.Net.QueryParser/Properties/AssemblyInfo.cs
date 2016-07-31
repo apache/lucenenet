db3b79bc143adda5df651857bc353a4e5b652eed
Shad Storhaug <shad@shadstorhaug.com>
2016-08-01 01:51:25 +0700

    Added QueryParser.Analyzing namespace + tests.

--- Lucene.Net.QueryParser/Properties/AssemblyInfo.cs
+++ Lucene.Net.QueryParser/Properties/AssemblyInfo.cs
@@ -22,6 +22,9 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7c58cf05-89dd-4c02-a948-c28cdaf05247")]
 
+// for testing
+[assembly: InternalsVisibleTo("Lucene.Net.Tests.QueryParser")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
