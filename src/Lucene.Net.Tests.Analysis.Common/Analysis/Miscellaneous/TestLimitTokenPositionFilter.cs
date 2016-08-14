12d19e453ee9bd517297d11ca2789778b4819d43
Shad Storhaug <shad@shadstorhaug.com>
2016-08-14 15:44:00 +0700

    Ported Analysis.Miscellaneous.TestLimitTokenPositionFilter

--- src/Lucene.Net.Tests.Analysis.Common/Analysis/Miscellaneous/TestLimitTokenPositionFilter.cs
+++ src/Lucene.Net.Tests.Analysis.Common/Analysis/Miscellaneous/TestLimitTokenPositionFilter.cs
@@ -1,6 +1,13 @@
-﻿namespace org.apache.lucene.analysis.miscellaneous
+﻿using Lucene.Net.Analysis.Util;
+using Lucene.Net.Util;
+using NUnit.Framework;
+using System.IO;
+using System;
+using Lucene.Net.Analysis.Synonym;
+
+namespace Lucene.Net.Analysis.Miscellaneous
 {
-	/*
+    /*
 	 * Licensed to the Apache Software Foundation (ASF) under one or more
 	 * contributor license agreements.  See the NOTICE file distributed with
 	 * this work for additional information regarding copyright ownership.
@@ -17,88 +24,78 @@
 	 * limitations under the License.
 	 */
 
-	using SynonymFilter = org.apache.lucene.analysis.synonym.SynonymFilter;
-	using SynonymMap = org.apache.lucene.analysis.synonym.SynonymMap;
-	using CharsRef = org.apache.lucene.util.CharsRef;
-	using Test = org.junit.Test;
+    public class TestLimitTokenPositionFilter : BaseTokenStreamTestCase
+    {
 
+        [Test]
+        public virtual void TestMaxPosition2()
+        {
+            foreach (bool consumeAll in new bool[] { true, false })
+            {
+                Analyzer a = new AnalyzerAnonymousInnerClassHelper(consumeAll);
 
-	public class TestLimitTokenPositionFilter : BaseTokenStreamTestCase
-	{
+                // don't use assertAnalyzesTo here, as the end offset is not the end of the string (unless consumeAll is true, in which case its correct)!
+                AssertTokenStreamContents(a.TokenStream("dummy", "1  2     3  4  5"), new string[] { "1", "2" }, new int[] { 0, 3 }, new int[] { 1, 4 }, consumeAll ? 16 : (int?)null);
+                AssertTokenStreamContents(a.TokenStream("dummy", new StringReader("1 2 3 4 5")), new string[] { "1", "2" }, new int[] { 0, 2 }, new int[] { 1, 3 }, consumeAll ? 9 : (int?)null);
 
-//JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-//ORIGINAL LINE: public void testMaxPosition2() throws java.io.IOException
-	  public virtual void testMaxPosition2()
-	  {
-		foreach (bool consumeAll in new bool[]{true, false})
-		{
-		  Analyzer a = new AnalyzerAnonymousInnerClassHelper(this);
+                // less than the limit, ensure we behave correctly
+                AssertTokenStreamContents(a.TokenStream("dummy", "1  "), new string[] { "1" }, new int[] { 0 }, new int[] { 1 }, consumeAll ? 3 : (int?)null);
 
-		  // don't use assertAnalyzesTo here, as the end offset is not the end of the string (unless consumeAll is true, in which case its correct)!
-		  assertTokenStreamContents(a.tokenStream("dummy", "1  2     3  4  5"), new string[]{"1", "2"}, new int[]{0, 3}, new int[]{1, 4}, consumeAll ? 16 : null);
-		  assertTokenStreamContents(a.tokenStream("dummy", new StringReader("1 2 3 4 5")), new string[]{"1", "2"}, new int[]{0, 2}, new int[]{1, 3}, consumeAll ? 9 : null);
+                // equal to limit
+                AssertTokenStreamContents(a.TokenStream("dummy", "1  2  "), new string[] { "1", "2" }, new int[] { 0, 3 }, new int[] { 1, 4 }, consumeAll ? 6 : (int?)null);
+            }
+        }
 
-		  // less than the limit, ensure we behave correctly
-		  assertTokenStreamContents(a.tokenStream("dummy", "1  "), new string[]{"1"}, new int[]{0}, new int[]{1}, consumeAll ? 3 : null);
+        private class AnalyzerAnonymousInnerClassHelper : Analyzer
+        {
+            private readonly bool consumeAll;
 
-		  // equal to limit
-		  assertTokenStreamContents(a.tokenStream("dummy", "1  2  "), new string[]{"1", "2"}, new int[]{0, 3}, new int[]{1, 4}, consumeAll ? 6 : null);
-		}
-	  }
+            public AnalyzerAnonymousInnerClassHelper(bool consumeAll)
+            {
+                this.consumeAll = consumeAll;
+            }
 
-	  private class AnalyzerAnonymousInnerClassHelper : Analyzer
-	  {
-		  private readonly TestLimitTokenPositionFilter outerInstance;
+            public override TokenStreamComponents CreateComponents(string fieldName, TextReader reader)
+            {
+                MockTokenizer tokenizer = new MockTokenizer(reader, MockTokenizer.WHITESPACE, false);
+                // if we are consuming all tokens, we can use the checks, otherwise we can't
+                tokenizer.EnableChecks = consumeAll;
+                return new TokenStreamComponents(tokenizer, new LimitTokenPositionFilter(tokenizer, 2, consumeAll));
+            }
+        }
 
-		  public AnalyzerAnonymousInnerClassHelper(TestLimitTokenPositionFilter outerInstance)
-		  {
-			  this.outerInstance = outerInstance;
-		  }
+        [Test]
+        public virtual void TestMaxPosition3WithSynomyms()
+        {
+            foreach (bool consumeAll in new bool[] { true, false })
+            {
+                MockTokenizer tokenizer = new MockTokenizer(new StringReader("one two three four five"), MockTokenizer.WHITESPACE, false);
+                // if we are consuming all tokens, we can use the checks, otherwise we can't
+                tokenizer.EnableChecks = consumeAll;
 
-		  protected internal override TokenStreamComponents createComponents(string fieldName, Reader reader)
-		  {
-			MockTokenizer tokenizer = new MockTokenizer(reader, MockTokenizer.WHITESPACE, false);
-			// if we are consuming all tokens, we can use the checks, otherwise we can't
-			tokenizer.EnableChecks = consumeAll;
-			return new TokenStreamComponents(tokenizer, new LimitTokenPositionFilter(tokenizer, 2, consumeAll));
-		  }
-	  }
+                SynonymMap.Builder builder = new SynonymMap.Builder(true);
+                builder.Add(new CharsRef("one"), new CharsRef("first"), true);
+                builder.Add(new CharsRef("one"), new CharsRef("alpha"), true);
+                builder.Add(new CharsRef("one"), new CharsRef("beguine"), true);
+                CharsRef multiWordCharsRef = new CharsRef();
+                SynonymMap.Builder.Join(new string[] { "and", "indubitably", "single", "only" }, multiWordCharsRef);
+                builder.Add(new CharsRef("one"), multiWordCharsRef, true);
+                SynonymMap.Builder.Join(new string[] { "dopple", "ganger" }, multiWordCharsRef);
+                builder.Add(new CharsRef("two"), multiWordCharsRef, true);
+                SynonymMap synonymMap = builder.Build();
+                TokenStream stream = new SynonymFilter(tokenizer, synonymMap, true);
+                stream = new LimitTokenPositionFilter(stream, 3, consumeAll);
 
-//JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-//ORIGINAL LINE: public void testMaxPosition3WithSynomyms() throws java.io.IOException
-	  public virtual void testMaxPosition3WithSynomyms()
-	  {
-		foreach (bool consumeAll in new bool[]{true, false})
-		{
-		  MockTokenizer tokenizer = new MockTokenizer(new StringReader("one two three four five"), MockTokenizer.WHITESPACE, false);
-		  // if we are consuming all tokens, we can use the checks, otherwise we can't
-		  tokenizer.EnableChecks = consumeAll;
+                // "only", the 4th word of multi-word synonym "and indubitably single only" is not emitted, since its position is greater than 3.
+                AssertTokenStreamContents(stream, new string[] { "one", "first", "alpha", "beguine", "and", "two", "indubitably", "dopple", "three", "single", "ganger" }, new int[] { 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0 });
+            }
+        }
 
-		  SynonymMap.Builder builder = new SynonymMap.Builder(true);
-		  builder.add(new CharsRef("one"), new CharsRef("first"), true);
-		  builder.add(new CharsRef("one"), new CharsRef("alpha"), true);
-		  builder.add(new CharsRef("one"), new CharsRef("beguine"), true);
-		  CharsRef multiWordCharsRef = new CharsRef();
-		  SynonymMap.Builder.join(new string[]{"and", "indubitably", "single", "only"}, multiWordCharsRef);
-		  builder.add(new CharsRef("one"), multiWordCharsRef, true);
-		  SynonymMap.Builder.join(new string[]{"dopple", "ganger"}, multiWordCharsRef);
-		  builder.add(new CharsRef("two"), multiWordCharsRef, true);
-		  SynonymMap synonymMap = builder.build();
-		  TokenStream stream = new SynonymFilter(tokenizer, synonymMap, true);
-		  stream = new LimitTokenPositionFilter(stream, 3, consumeAll);
-
-		  // "only", the 4th word of multi-word synonym "and indubitably single only" is not emitted, since its position is greater than 3.
-		  assertTokenStreamContents(stream, new string[]{"one", "first", "alpha", "beguine", "and", "two", "indubitably", "dopple", "three", "single", "ganger"}, new int[]{1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0});
-		}
-	  }
-
-//JAVA TO C# CONVERTER TODO TASK: Most Java annotations will not have direct .NET equivalent attributes:
-//ORIGINAL LINE: @Test(expected = IllegalArgumentException.class) public void testIllegalArguments() throws Exception
-//JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-	  public virtual void testIllegalArguments()
-	  {
-		new LimitTokenPositionFilter(new MockTokenizer(new StringReader("one two three four five")), 0);
-	  }
-	}
-
+        [Test]
+        [ExpectedException(ExpectedException = typeof(ArgumentException))]
+        public virtual void TestIllegalArguments()
+        {
+            new LimitTokenPositionFilter(new MockTokenizer(new StringReader("one two three four five")), 0);
+        }
+    }
 }
