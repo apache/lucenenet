572ad694199d3c7a4832ae12abcbd11d6c960e7d
Shad Storhaug <shad@shadstorhaug.com>
2016-08-18 17:20:57 +0700

    Added missing Core.Util.OfflineSorter members (+ tests) required by Hunspell.

---
+++ src/Lucene.Net.Core/Support/Compatibility/BinaryReaderDataInput.cs
@@ -0,0 +1,34 @@
+﻿using Lucene.Net.Store;
+using System;
+using System.IO;
+
+namespace Lucene.Net.Support.Compatibility
+{
+    public class BinaryReaderDataInput : DataInput, IDisposable
+    {
+        private readonly BinaryReader br;
+        public BinaryReaderDataInput(BinaryReader br)
+        {
+            this.br = br;
+        }
+       
+        public override byte ReadByte()
+        {
+            return br.ReadByte();
+        }
+
+        public override void ReadBytes(byte[] b, int offset, int len)
+        {
+            byte[] temp = br.ReadBytes(len);
+            for (int i = offset; i < (offset + len) && i < temp.Length; i++)
+            {
+                b[i] = temp[i];
+            }
+        }
+
+        public void Dispose()
+        {
+            br.Dispose();
+        }
+    }
+}
