db12bca20cadaf5a0397eb75fd8d0b1145f2525a
Shad Storhaug <shad@shadstorhaug.com>
2016-09-02 02:07:27 +0700

    Ported Lucene.Net.Misc namespace + tests

--- src/Lucene.Net.Misc/Misc/GetTermInfo.cs
+++ src/Lucene.Net.Misc/Misc/GetTermInfo.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Lucene.Net.Index;
+using Lucene.Net.Store;
+using System;
+using System.IO;
 
-namespace org.apache.lucene.misc
+namespace Lucene.Net.Misc
 {
-
-	/*
+    /*
 	 * Licensed to the Apache Software Foundation (ASF) under one or more
 	 * contributor license agreements.  See the NOTICE file distributed with
 	 * this work for additional information regarding copyright ownership.
@@ -20,55 +22,43 @@ namespace org.apache.lucene.misc
 	 * limitations under the License.
 	 */
 
-	using Directory = org.apache.lucene.store.Directory;
-	using FSDirectory = org.apache.lucene.store.FSDirectory;
-	using DirectoryReader = org.apache.lucene.index.DirectoryReader;
-	using IndexReader = org.apache.lucene.index.IndexReader;
-	using Term = org.apache.lucene.index.Term;
+    /// <summary>
+    /// Utility to get document frequency and total number of occurrences (sum of the tf for each doc)  of a term. 
+    /// </summary>
+    public class GetTermInfo
+    {
+        public static void Main(string[] args)
+        {
 
-	/// <summary>
-	/// Utility to get document frequency and total number of occurrences (sum of the tf for each doc)  of a term. 
-	/// </summary>
-	public class GetTermInfo
-	{
+            FSDirectory dir = null;
+            string inputStr = null;
+            string field = null;
 
-//JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-//ORIGINAL LINE: public static void main(String[] args) throws Exception
-	  public static void Main(string[] args)
-	  {
+            if (args.Length == 3)
+            {
+                dir = FSDirectory.Open(new DirectoryInfo(args[0]));
+                field = args[1];
+                inputStr = args[2];
+            }
+            else
+            {
+                Usage();
+                Environment.Exit(1);
+            }
 
-		FSDirectory dir = null;
-		string inputStr = null;
-		string field = null;
+            TermInfo(dir, new Term(field, inputStr));
+        }
 
-		if (args.Length == 3)
-		{
-		  dir = FSDirectory.open(new File(args[0]));
-		  field = args[1];
-		  inputStr = args[2];
-		}
-		else
-		{
-		  usage();
-		  Environment.Exit(1);
-		}
+        public static void TermInfo(Store.Directory dir, Term term)
+        {
+            IndexReader reader = DirectoryReader.Open(dir);
+            Console.WriteLine("{0}:{1} \t totalTF = {2:#,##0} \t doc freq = {3:#,##0} \n", term.Field, term.Text(), reader.TotalTermFreq(term), reader.DocFreq(term));
+        }
 
-		getTermInfo(dir,new Term(field, inputStr));
-	  }
-
-//JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-//ORIGINAL LINE: public static void getTermInfo(org.apache.lucene.store.Directory dir, org.apache.lucene.index.Term term) throws Exception
-	  public static void getTermInfo(Directory dir, Term term)
-	  {
-		IndexReader reader = DirectoryReader.open(dir);
-		System.out.printf(Locale.ROOT, "%s:%s \t totalTF = %,d \t doc freq = %,d \n", term.field(), term.text(), reader.totalTermFreq(term), reader.docFreq(term));
-	  }
-
-	  private static void usage()
-	  {
-//JAVA TO C# CONVERTER WARNING: The .NET Type.FullName property will not always yield results identical to the Java Class.getName method:
-		Console.WriteLine("\n\nusage:\n\t" + "java " + typeof(GetTermInfo).FullName + " <index dir> field term \n\n");
-	  }
-	}
-
+        private static void Usage()
+        {
+            // LUCENENET TODO: Usage info is dependant on packaging this into a standalone console application
+            Console.WriteLine("\n\nusage:\n\t" + "java " + typeof(GetTermInfo).FullName + " <index dir> field term \n\n");
+        }
+    }
 }
