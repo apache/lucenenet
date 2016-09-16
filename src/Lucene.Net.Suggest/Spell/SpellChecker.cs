2a23271882cbcc56e602ad2ae5a3c07812cbd3c9
Shad Storhaug <shad@shadstorhaug.com>
2016-09-17 00:02:59 +0700

    Normalized Suggest license headers.

--- src/Lucene.Net.Suggest/Spell/SpellChecker.cs
+++ src/Lucene.Net.Suggest/Spell/SpellChecker.cs
@@ -8,7 +8,6 @@ using Directory = Lucene.Net.Store.Directory;
 
 namespace Lucene.Net.Search.Spell
 {
-
     /*
      * Licensed to the Apache Software Foundation (ASF) under one or more
      * contributor license agreements.  See the NOTICE file distributed with
