92c3a072b6bbaae15a1291e080d7fbe1b4ac9d36
Shad Storhaug <shad@shadstorhaug.com>
2016-10-10 23:36:55 +0700

    Added Codecs.DiskDv tests to the project

--- src/Lucene.Net.Tests.Codecs/DiskDv/TestDiskDocValuesFormat.cs
+++ src/Lucene.Net.Tests.Codecs/DiskDv/TestDiskDocValuesFormat.cs
@@ -1,41 +1,40 @@
-﻿/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-using Lucene.Net.Codecs.DiskDV;
+﻿using NUnit.Framework;
 
 namespace Lucene.Net.Codecs.DiskDV
 {
+    /*
+     * Licensed to the Apache Software Foundation (ASF) under one or more
+     * contributor license agreements.  See the NOTICE file distributed with
+     * this work for additional information regarding copyright ownership.
+     * The ASF licenses this file to You under the Apache License, Version 2.0
+     * (the "License"); you may not use this file except in compliance with
+     * the License.  You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
 
-	using BaseCompressingDocValuesFormatTestCase = Lucene.Net.Index.BaseCompressingDocValuesFormatTestCase;
-	using TestUtil = Lucene.Net.Util.TestUtil;
+    using BaseCompressingDocValuesFormatTestCase = Lucene.Net.Index.BaseCompressingDocValuesFormatTestCase;
+    using TestUtil = Lucene.Net.Util.TestUtil;
 
-	/// <summary>
-	/// Tests DiskDocValuesFormat
-	/// </summary>
-	public class TestDiskDocValuesFormat : BaseCompressingDocValuesFormatTestCase
-	{
-	  private readonly Codec codec = TestUtil.AlwaysDocValuesFormat(new DiskDocValuesFormat());
+    /// <summary>
+    /// Tests DiskDocValuesFormat
+    /// </summary>
+    public class TestDiskDocValuesFormat : BaseCompressingDocValuesFormatTestCase
+    {
+        private readonly Codec codec = TestUtil.AlwaysDocValuesFormat(new DiskDocValuesFormat());
 
-	  protected override Codec Codec
-	  {
-		  get
-		  {
-			return codec;
-		  }
-	  }
-	}
+        protected override Codec Codec
+        {
+            get
+            {
+                return codec;
+            }
+        }
+    }
 }
