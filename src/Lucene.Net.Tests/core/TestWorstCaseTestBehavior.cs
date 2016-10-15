fc40147ed85e3a81b81f0b1f276ec3f41c01d265
Shad Storhaug <shad@shadstorhaug.com>
2016-10-16 06:48:16 +0700

    Added missing test Core.TestWorstCaseBehavior

--- src/Lucene.Net.Tests/core/TestWorstCaseTestBehavior.cs
+++ src/Lucene.Net.Tests/core/TestWorstCaseTestBehavior.cs
@@ -1,16 +1,12 @@
+using Lucene.Net.Support;
+using Lucene.Net.Util;
+using NUnit.Framework;
 using System;
 using System.Threading;
 
-namespace org.apache.lucene
+namespace Lucene.Net
 {
-
-	using LuceneTestCase = Lucene.Net.Util.LuceneTestCase;
-	//using Ignore = org.junit.Ignore;
-
-	//using RandomizedTest = com.carrotsearch.randomizedtesting.RandomizedTest;
-	//using Timeout = com.carrotsearch.randomizedtesting.annotations.Timeout;
-
-	/*
+    /*
 	 * Licensed to the Apache Software Foundation (ASF) under one or more
 	 * contributor license agreements.  See the NOTICE file distributed with
 	 * this work for additional information regarding copyright ownership.
@@ -27,123 +23,124 @@ namespace org.apache.lucene
 	 * limitations under the License.
 	 */
 
-	public class TestWorstCaseTestBehavior : LuceneTestCase
-	{
-//JAVA TO C# CONVERTER TODO TASK: Most Java annotations will not have direct .NET equivalent attributes:
-//ORIGINAL LINE: @Ignore public void testThreadLeak()
-	  public virtual void TestThreadLeak()
-	  {
-		Thread t = new ThreadAnonymousInnerClassHelper(this);
-		t.Start();
+    public class TestWorstCaseTestBehavior : LuceneTestCase
+    {
+        [Ignore]
+        [Test]
+        public virtual void TestThreadLeak()
+        {
+            ThreadClass t = new ThreadAnonymousInnerClassHelper(this);
+            t.Start();
 
-		while (!t.IsAlive)
-		{
-		  Thread.@Yield();
-		}
+            while (!t.IsAlive)
+            {
+                Thread.@Yield();
+            }
 
-		// once alive, leave it to run outside of the test scope.
-	  }
+            // once alive, leave it to run outside of the test scope.
+        }
 
-	  private class ThreadAnonymousInnerClassHelper : Thread
-	  {
-		  private readonly TestWorstCaseTestBehavior OuterInstance;
+        private class ThreadAnonymousInnerClassHelper : ThreadClass
+        {
+            private readonly TestWorstCaseTestBehavior OuterInstance;
 
-		  public ThreadAnonymousInnerClassHelper(TestWorstCaseTestBehavior outerInstance)
-		  {
-			  this.OuterInstance = outerInstance;
-		  }
+            public ThreadAnonymousInnerClassHelper(TestWorstCaseTestBehavior outerInstance)
+            {
+                this.OuterInstance = outerInstance;
+            }
 
-		  public override void Run()
-		  {
-			try
-			{
-			  Thread.Sleep(10000);
-			}
-			catch (InterruptedException e)
-			{
-			  // Ignore.
-			}
-		  }
-	  }
+            public override void Run()
+            {
+                try
+                {
+                    Thread.Sleep(10000);
+                }
+                catch (ThreadInterruptedException e)
+                {
+                    // Ignore.
+                }
+            }
+        }
 
-//JAVA TO C# CONVERTER TODO TASK: Most Java annotations will not have direct .NET equivalent attributes:
-//ORIGINAL LINE: @Ignore public void testLaaaaaargeOutput() throws Exception
-	  public virtual void TestLaaaaaargeOutput()
-	  {
-		string message = "I will not OOM on large output";
-		int howMuch = 250 * 1024 * 1024;
-		for (int i = 0; i < howMuch; i++)
-		{
-		  if (i > 0)
-		  {
-			  Console.Write(",\n");
-		  }
-		  Console.Write(message);
-		  howMuch -= message.Length; // approximately.
-		}
-		Console.WriteLine(".");
-	  }
+        [Ignore]
+        [Test]
+        public virtual void TestLaaaaaargeOutput()
+        {
+            string message = "I will not OOM on large output";
+            int howMuch = 250 * 1024 * 1024;
+            for (int i = 0; i < howMuch; i++)
+            {
+                if (i > 0)
+                {
+                    Console.Write(",\n");
+                }
+                Console.Write(message);
+                howMuch -= message.Length; // approximately.
+            }
+            Console.WriteLine(".");
+        }
 
-//JAVA TO C# CONVERTER TODO TASK: Most Java annotations will not have direct .NET equivalent attributes:
-//ORIGINAL LINE: @Ignore public void testProgressiveOutput() throws Exception
-	  public virtual void TestProgressiveOutput()
-	  {
-		for (int i = 0; i < 20; i++)
-		{
-		  Console.WriteLine("Emitting sysout line: " + i);
-		  Console.Error.WriteLine("Emitting syserr line: " + i);
-		  System.out.flush();
-		  System.err.flush();
-		  RandomizedTest.sleep(1000);
-		}
-	  }
 
-//JAVA TO C# CONVERTER TODO TASK: Most Java annotations will not have direct .NET equivalent attributes:
-//ORIGINAL LINE: @Ignore public void testUncaughtException() throws Exception
-	  public virtual void TestUncaughtException()
-	  {
-		Thread t = new ThreadAnonymousInnerClassHelper2(this);
-		t.Start();
-		t.Join();
-	  }
+        [Ignore]
+        [Test]
+        public virtual void TestProgressiveOutput()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Emitting sysout line: " + i);
+                Console.Error.WriteLine("Emitting syserr line: " + i);
+                Console.Out.Flush();
+                Console.Error.Flush();
+                Thread.Sleep(1000);
+            }
+        }
 
-	  private class ThreadAnonymousInnerClassHelper2 : System.Threading.Thread
-	  {
-		  private readonly TestWorstCaseTestBehavior OuterInstance;
+        [Ignore]
+        [Test]
+        public virtual void TestUncaughtException()
+        {
+            ThreadClass t = new ThreadAnonymousInnerClassHelper2(this);
+            t.Start();
+            t.Join();
+        }
 
-		  public ThreadAnonymousInnerClassHelper2(TestWorstCaseTestBehavior outerInstance)
-		  {
-			  this.OuterInstance = outerInstance;
-		  }
+        private class ThreadAnonymousInnerClassHelper2 : ThreadClass
+        {
+            private readonly TestWorstCaseTestBehavior OuterInstance;
 
-		  public override void Run()
-		  {
-			throw new Exception("foobar");
-		  }
-	  }
+            public ThreadAnonymousInnerClassHelper2(TestWorstCaseTestBehavior outerInstance)
+            {
+                this.OuterInstance = outerInstance;
+            }
 
-//JAVA TO C# CONVERTER TODO TASK: Most Java annotations will not have direct .NET equivalent attributes:
-//ORIGINAL LINE: @Ignore @Timeout(millis = 500) public void testTimeout() throws Exception
-	  public virtual void TestTimeout()
-	  {
-		Thread.Sleep(5000);
-	  }
+            public override void Run()
+            {
+                throw new Exception("foobar");
+            }
+        }
 
-//JAVA TO C# CONVERTER TODO TASK: Most Java annotations will not have direct .NET equivalent attributes:
-//ORIGINAL LINE: @Ignore @Timeout(millis = 1000) public void testZombie() throws Exception
-	  public virtual void TestZombie()
-	  {
-		while (true)
-		{
-		  try
-		  {
-			Thread.Sleep(1000);
-		  }
-		  catch (InterruptedException e)
-		  {
-		  }
-		}
-	  }
-	}
+        [Ignore]
+        [Test, Timeout(500)]
+        public virtual void TestTimeout()
+        {
+            Thread.Sleep(5000);
+        }
 
+
+        [Ignore]
+        [Test, Timeout(1000)]
+        public virtual void TestZombie()
+        {
+            while (true)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch (ThreadInterruptedException e)
+                {
+                }
+            }
+        }
+    }
 }
