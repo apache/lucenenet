d992e661630e41ed708fe560039795ea9d952d8e
Shad Storhaug <shad@shadstorhaug.com>
2016-10-15 21:37:54 +0700

    Removed LongRunningTest attribute from Core.Util.Automaton.TestDeterminism.TestRegexps() because it only takes a few milliseconds

--- src/Lucene.Net.Tests/core/Util/Automaton/TestDeterminism.cs
+++ src/Lucene.Net.Tests/core/Util/Automaton/TestDeterminism.cs
@@ -29,7 +29,7 @@ namespace Lucene.Net.Util.Automaton
     {
         /// <summary>
         /// test a bunch of random regular expressions </summary>
-        [Test, LongRunningTest]
+        [Test]
         public virtual void TestRegexps()
         {
             int num = AtLeast(500);
