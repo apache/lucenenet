0a3ca025b12aa18708dceee04ed4692e8ff39099
Shad Storhaug <shad@shadstorhaug.com>
2016-12-28 12:39:35 +0700

    Lucene.Net.Core.Util.Automaton.RegExp refactor: Identifiers > GetIdentifiers() (makes a conversion)

--- src/Lucene.Net.Core/Util/Automaton/RegExp.cs
+++ src/Lucene.Net.Core/Util/Automaton/RegExp.cs
@@ -746,14 +746,11 @@ namespace Lucene.Net.Util.Automaton
         /// <summary>
         /// Returns set of automaton identifiers that occur in this regular expression.
         /// </summary>
-        public virtual ISet<string> Identifiers // LUCENENET TODO: Change to GetIdentifiers() (makes conversion)
+        public virtual ISet<string> GetIdentifiers()
         {
-            get
-            {
-                HashSet<string> set = new HashSet<string>();
-                GetIdentifiers(set);
-                return set;
-            }
+            HashSet<string> set = new HashSet<string>();
+            GetIdentifiers(set);
+            return set;
         }
 
         internal virtual void GetIdentifiers(ISet<string> set)
