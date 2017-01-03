22521178076d8cd23d52c20460a683f12607ff99
Shad Storhaug <shad@shadstorhaug.com>
2017-01-04 02:14:30 +0700

    Lucene.Net.Core.Util.Automaton.UTF32ToUTF8 refactor: renamed private/internal fields camelCase

--- src/Lucene.Net.Core/Util/Automaton/UTF32ToUTF8.cs
+++ src/Lucene.Net.Core/Util/Automaton/UTF32ToUTF8.cs
@@ -33,9 +33,9 @@ namespace Lucene.Net.Util.Automaton
     public sealed class UTF32ToUTF8
     {
         // Unicode boundaries for UTF8 bytes 1,2,3,4
-        private static readonly int[] StartCodes = new int[] { 0, 128, 2048, 65536 };
+        private static readonly int[] startCodes = new int[] { 0, 128, 2048, 65536 };
 
-        private static readonly int[] EndCodes = new int[] { 127, 2047, 65535, 1114111 };
+        private static readonly int[] endCodes = new int[] { 127, 2047, 65535, 1114111 };
 
         internal static int[] MASKS = new int[32];
 
@@ -62,26 +62,26 @@ namespace Lucene.Net.Util.Automaton
         // TODO: maybe move to UnicodeUtil?
         private class UTF8Sequence
         {
-            private readonly UTF8Byte[] Bytes;
-            internal int Len;
+            private readonly UTF8Byte[] bytes;
+            internal int len;
 
             public UTF8Sequence()
             {
-                Bytes = new UTF8Byte[4];
+                bytes = new UTF8Byte[4];
                 for (int i = 0; i < 4; i++)
                 {
-                    Bytes[i] = new UTF8Byte();
+                    bytes[i] = new UTF8Byte();
                 }
             }
 
             public virtual int ByteAt(int idx)
             {
-                return Bytes[idx].Value;
+                return bytes[idx].Value;
             }
 
             public virtual int NumBits(int idx)
             {
-                return Bytes[idx].Bits;
+                return bytes[idx].Bits;
             }
 
             internal virtual void Set(int code)
@@ -89,33 +89,33 @@ namespace Lucene.Net.Util.Automaton
                 if (code < 128)
                 {
                     // 0xxxxxxx
-                    Bytes[0].Value = code;
-                    Bytes[0].Bits = 7;
-                    Len = 1;
+                    bytes[0].Value = code;
+                    bytes[0].Bits = 7;
+                    len = 1;
                 }
                 else if (code < 2048)
                 {
                     // 110yyyxx 10xxxxxx
-                    Bytes[0].Value = (6 << 5) | (code >> 6);
-                    Bytes[0].Bits = 5;
+                    bytes[0].Value = (6 << 5) | (code >> 6);
+                    bytes[0].Bits = 5;
                     SetRest(code, 1);
-                    Len = 2;
+                    len = 2;
                 }
                 else if (code < 65536)
                 {
                     // 1110yyyy 10yyyyxx 10xxxxxx
-                    Bytes[0].Value = (14 << 4) | (code >> 12);
-                    Bytes[0].Bits = 4;
+                    bytes[0].Value = (14 << 4) | (code >> 12);
+                    bytes[0].Bits = 4;
                     SetRest(code, 2);
-                    Len = 3;
+                    len = 3;
                 }
                 else
                 {
                     // 11110zzz 10zzyyyy 10yyyyxx 10xxxxxx
-                    Bytes[0].Value = (30 << 3) | (code >> 18);
-                    Bytes[0].Bits = 3;
+                    bytes[0].Value = (30 << 3) | (code >> 18);
+                    bytes[0].Bits = 3;
                     SetRest(code, 3);
-                    Len = 4;
+                    len = 4;
                 }
             }
 
@@ -123,8 +123,8 @@ namespace Lucene.Net.Util.Automaton
             {
                 for (int i = 0; i < numBytes; i++)
                 {
-                    Bytes[numBytes - i].Value = 128 | (code & MASKS[5]);
-                    Bytes[numBytes - i].Bits = 6;
+                    bytes[numBytes - i].Value = 128 | (code & MASKS[5]);
+                    bytes[numBytes - i].Bits = 6;
                     code = code >> 6;
                 }
             }
@@ -132,32 +132,32 @@ namespace Lucene.Net.Util.Automaton
             public override string ToString()
             {
                 StringBuilder b = new StringBuilder();
-                for (int i = 0; i < Len; i++)
+                for (int i = 0; i < len; i++)
                 {
                     if (i > 0)
                     {
                         b.Append(' ');
                     }
-                    b.Append(Number.ToBinaryString(Bytes[i].Value));
+                    b.Append(Number.ToBinaryString(bytes[i].Value));
                 }
                 return b.ToString();
             }
         }
 
-        private readonly UTF8Sequence StartUTF8 = new UTF8Sequence();
-        private readonly UTF8Sequence EndUTF8 = new UTF8Sequence();
+        private readonly UTF8Sequence startUTF8 = new UTF8Sequence();
+        private readonly UTF8Sequence endUTF8 = new UTF8Sequence();
 
-        private readonly UTF8Sequence TmpUTF8a = new UTF8Sequence();
-        private readonly UTF8Sequence TmpUTF8b = new UTF8Sequence();
+        private readonly UTF8Sequence tmpUTF8a = new UTF8Sequence();
+        private readonly UTF8Sequence tmpUTF8b = new UTF8Sequence();
 
         // Builds necessary utf8 edges between start & end
         internal void ConvertOneEdge(State start, State end, int startCodePoint, int endCodePoint)
         {
-            StartUTF8.Set(startCodePoint);
-            EndUTF8.Set(endCodePoint);
+            startUTF8.Set(startCodePoint);
+            endUTF8.Set(endCodePoint);
             //System.out.println("start = " + startUTF8);
             //System.out.println("  end = " + endUTF8);
-            Build(start, end, StartUTF8, EndUTF8, 0);
+            Build(start, end, startUTF8, endUTF8, 0);
         }
 
         private void Build(State start, State end, UTF8Sequence startUTF8, UTF8Sequence endUTF8, int upto)
@@ -166,7 +166,7 @@ namespace Lucene.Net.Util.Automaton
             if (startUTF8.ByteAt(upto) == endUTF8.ByteAt(upto))
             {
                 // Degen case: lead with the same byte:
-                if (upto == startUTF8.Len - 1 && upto == endUTF8.Len - 1)
+                if (upto == startUTF8.len - 1 && upto == endUTF8.len - 1)
                 {
                     // Super degen: just single edge, one UTF8 byte:
                     start.AddTransition(new Transition(startUTF8.ByteAt(upto), endUTF8.ByteAt(upto), end));
@@ -174,8 +174,8 @@ namespace Lucene.Net.Util.Automaton
                 }
                 else
                 {
-                    Debug.Assert(startUTF8.Len > upto + 1);
-                    Debug.Assert(endUTF8.Len > upto + 1);
+                    Debug.Assert(startUTF8.len > upto + 1);
+                    Debug.Assert(endUTF8.len > upto + 1);
                     State n = NewUTF8State();
 
                     // Single value leading edge
@@ -185,9 +185,9 @@ namespace Lucene.Net.Util.Automaton
                     Build(n, end, startUTF8, endUTF8, 1 + upto);
                 }
             }
-            else if (startUTF8.Len == endUTF8.Len)
+            else if (startUTF8.len == endUTF8.len)
             {
-                if (upto == startUTF8.Len - 1)
+                if (upto == startUTF8.len - 1)
                 {
                     start.AddTransition(new Transition(startUTF8.ByteAt(upto), endUTF8.ByteAt(upto), end)); // type=startend
                 }
@@ -197,7 +197,7 @@ namespace Lucene.Net.Util.Automaton
                     if (endUTF8.ByteAt(upto) - startUTF8.ByteAt(upto) > 1)
                     {
                         // There is a middle
-                        All(start, end, startUTF8.ByteAt(upto) + 1, endUTF8.ByteAt(upto) - 1, startUTF8.Len - upto - 1);
+                        All(start, end, startUTF8.ByteAt(upto) + 1, endUTF8.ByteAt(upto) - 1, startUTF8.len - upto - 1);
                     }
                     End(start, end, endUTF8, upto, false);
                 }
@@ -208,15 +208,15 @@ namespace Lucene.Net.Util.Automaton
                 Start(start, end, startUTF8, upto, true);
 
                 // possibly middle, spanning multiple num bytes
-                int byteCount = 1 + startUTF8.Len - upto;
-                int limit = endUTF8.Len - upto;
+                int byteCount = 1 + startUTF8.len - upto;
+                int limit = endUTF8.len - upto;
                 while (byteCount < limit)
                 {
                     // wasteful: we only need first byte, and, we should
                     // statically encode this first byte:
-                    TmpUTF8a.Set(StartCodes[byteCount - 1]);
-                    TmpUTF8b.Set(EndCodes[byteCount - 1]);
-                    All(start, end, TmpUTF8a.ByteAt(0), TmpUTF8b.ByteAt(0), TmpUTF8a.Len - 1);
+                    tmpUTF8a.Set(startCodes[byteCount - 1]);
+                    tmpUTF8b.Set(endCodes[byteCount - 1]);
+                    All(start, end, tmpUTF8a.ByteAt(0), tmpUTF8b.ByteAt(0), tmpUTF8a.len - 1);
                     byteCount++;
                 }
 
@@ -227,7 +227,7 @@ namespace Lucene.Net.Util.Automaton
 
         private void Start(State start, State end, UTF8Sequence utf8, int upto, bool doAll)
         {
-            if (upto == utf8.Len - 1)
+            if (upto == utf8.len - 1)
             {
                 // Done recursing
                 start.AddTransition(new Transition(utf8.ByteAt(upto), utf8.ByteAt(upto) | MASKS[utf8.NumBits(upto) - 1], end)); // type=start
@@ -240,14 +240,14 @@ namespace Lucene.Net.Util.Automaton
                 int endCode = utf8.ByteAt(upto) | MASKS[utf8.NumBits(upto) - 1];
                 if (doAll && utf8.ByteAt(upto) != endCode)
                 {
-                    All(start, end, utf8.ByteAt(upto) + 1, endCode, utf8.Len - upto - 1);
+                    All(start, end, utf8.ByteAt(upto) + 1, endCode, utf8.len - upto - 1);
                 }
             }
         }
 
         private void End(State start, State end, UTF8Sequence utf8, int upto, bool doAll)
         {
-            if (upto == utf8.Len - 1)
+            if (upto == utf8.len - 1)
             {
                 // Done recursing
                 start.AddTransition(new Transition(utf8.ByteAt(upto) & (~MASKS[utf8.NumBits(upto) - 1]), utf8.ByteAt(upto), end)); // type=end
@@ -268,7 +268,7 @@ namespace Lucene.Net.Util.Automaton
                 }
                 if (doAll && utf8.ByteAt(upto) != startCode)
                 {
-                    All(start, end, startCode, utf8.ByteAt(upto) - 1, utf8.Len - upto - 1);
+                    All(start, end, startCode, utf8.ByteAt(upto) - 1, utf8.len - upto - 1);
                 }
                 State n = NewUTF8State();
                 start.AddTransition(new Transition(utf8.ByteAt(upto), n)); // type=end
@@ -297,8 +297,8 @@ namespace Lucene.Net.Util.Automaton
             }
         }
 
-        private State[] Utf8States;
-        private int Utf8StateCount;
+        private State[] utf8States;
+        private int utf8StateCount;
 
         /// <summary>
         /// Converts an incoming utf32 automaton to an equivalent
@@ -323,11 +323,11 @@ namespace Lucene.Net.Util.Automaton
 
             State utf8State = utf8.GetInitialState();
 
-            Utf8States = new State[5];
-            Utf8StateCount = 0;
-            utf8State.number = Utf8StateCount;
-            Utf8States[Utf8StateCount] = utf8State;
-            Utf8StateCount++;
+            utf8States = new State[5];
+            utf8StateCount = 0;
+            utf8State.number = utf8StateCount;
+            utf8States[utf8StateCount] = utf8State;
+            utf8StateCount++;
 
             utf8State.Accept = utf32State.Accept;
 
@@ -354,7 +354,7 @@ namespace Lucene.Net.Util.Automaton
                 }
             }
 
-            utf8.SetNumberedStates(Utf8States, Utf8StateCount);
+            utf8.SetNumberedStates(utf8States, utf8StateCount);
 
             return utf8;
         }
@@ -362,15 +362,15 @@ namespace Lucene.Net.Util.Automaton
         private State NewUTF8State()
         {
             State s = new State();
-            if (Utf8StateCount == Utf8States.Length)
+            if (utf8StateCount == utf8States.Length)
             {
-                State[] newArray = new State[ArrayUtil.Oversize(1 + Utf8StateCount, RamUsageEstimator.NUM_BYTES_OBJECT_REF)];
-                Array.Copy(Utf8States, 0, newArray, 0, Utf8StateCount);
-                Utf8States = newArray;
+                State[] newArray = new State[ArrayUtil.Oversize(1 + utf8StateCount, RamUsageEstimator.NUM_BYTES_OBJECT_REF)];
+                Array.Copy(utf8States, 0, newArray, 0, utf8StateCount);
+                utf8States = newArray;
             }
-            Utf8States[Utf8StateCount] = s;
-            s.number = Utf8StateCount;
-            Utf8StateCount++;
+            utf8States[utf8StateCount] = s;
+            s.number = utf8StateCount;
+            utf8StateCount++;
             return s;
         }
     }
