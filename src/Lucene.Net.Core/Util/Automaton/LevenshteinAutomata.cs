47a207b9ba688cbd21b8cdbfa8f4252ba24aceae
Shad Storhaug <shad@shadstorhaug.com>
2017-01-04 01:55:41 +0700

    Lucene.Net.Core.Util.Automaton.ParametricDescription refactor: renamed private/internal fields camelCase

--- src/Lucene.Net.Core/Util/Automaton/LevenshteinAutomata.cs
+++ src/Lucene.Net.Core/Util/Automaton/LevenshteinAutomata.cs
@@ -37,18 +37,18 @@ namespace Lucene.Net.Util.Automaton
         public const int MAXIMUM_SUPPORTED_DISTANCE = 2;
 
         /* input word */
-        internal readonly int[] Word;
+        internal readonly int[] word;
         /* the automata alphabet. */
-        internal readonly int[] Alphabet;
+        internal readonly int[] alphabet;
         /* the maximum symbol in the alphabet (e.g. 255 for UTF-8 or 10FFFF for UTF-32) */
-        internal readonly int AlphaMax;
+        internal readonly int alphaMax;
 
         /* the ranges outside of alphabet */
-        internal readonly int[] RangeLower;
-        internal readonly int[] RangeUpper;
-        internal int NumRanges = 0;
+        internal readonly int[] rangeLower;
+        internal readonly int[] rangeUpper;
+        internal int numRanges = 0;
 
-        internal ParametricDescription[] Descriptions;
+        internal ParametricDescription[] descriptions;
 
         /// <summary>
         /// Create a new LevenshteinAutomata for some input String.
@@ -65,8 +65,8 @@ namespace Lucene.Net.Util.Automaton
         /// </summary>
         public LevenshteinAutomata(int[] word, int alphaMax, bool withTranspositions)
         {
-            this.Word = word;
-            this.AlphaMax = alphaMax;
+            this.word = word;
+            this.alphaMax = alphaMax;
 
             // calculate the alphabet
             SortedSet<int> set = new SortedSet<int>();
@@ -79,39 +79,39 @@ namespace Lucene.Net.Util.Automaton
                 }
                 set.Add(v);
             }
-            Alphabet = new int[set.Count];
+            alphabet = new int[set.Count];
             IEnumerator<int> iterator = set.GetEnumerator();
-            for (int i = 0; i < Alphabet.Length; i++)
+            for (int i = 0; i < alphabet.Length; i++)
             {
                 iterator.MoveNext();
-                Alphabet[i] = iterator.Current;
+                alphabet[i] = iterator.Current;
             }
 
-            RangeLower = new int[Alphabet.Length + 2];
-            RangeUpper = new int[Alphabet.Length + 2];
+            rangeLower = new int[alphabet.Length + 2];
+            rangeUpper = new int[alphabet.Length + 2];
             // calculate the unicode range intervals that exclude the alphabet
             // these are the ranges for all unicode characters not in the alphabet
             int lower = 0;
-            for (int i = 0; i < Alphabet.Length; i++)
+            for (int i = 0; i < alphabet.Length; i++)
             {
-                int higher = Alphabet[i];
+                int higher = alphabet[i];
                 if (higher > lower)
                 {
-                    RangeLower[NumRanges] = lower;
-                    RangeUpper[NumRanges] = higher - 1;
-                    NumRanges++;
+                    rangeLower[numRanges] = lower;
+                    rangeUpper[numRanges] = higher - 1;
+                    numRanges++;
                 }
                 lower = higher + 1;
             }
             /* add the final endpoint */
             if (lower <= alphaMax)
             {
-                RangeLower[NumRanges] = lower;
-                RangeUpper[NumRanges] = alphaMax;
-                NumRanges++;
+                rangeLower[numRanges] = lower;
+                rangeUpper[numRanges] = alphaMax;
+                numRanges++;
             }
 
-            Descriptions = new ParametricDescription[] {
+            descriptions = new ParametricDescription[] {
             null,
             withTranspositions ? (ParametricDescription)new Lev1TParametricDescription(word.Length) : new Lev1ParametricDescription(word.Length),
             withTranspositions ? (ParametricDescription)new Lev2TParametricDescription(word.Length) : new Lev2ParametricDescription(word.Length)
@@ -145,16 +145,16 @@ namespace Lucene.Net.Util.Automaton
         {
             if (n == 0)
             {
-                return BasicAutomata.MakeString(Word, 0, Word.Length);
+                return BasicAutomata.MakeString(word, 0, word.Length);
             }
 
-            if (n >= Descriptions.Length)
+            if (n >= descriptions.Length)
             {
                 return null;
             }
 
             int range = 2 * n + 1;
-            ParametricDescription description = Descriptions[n];
+            ParametricDescription description = descriptions[n];
             // the number of states is based on the length of the word and n
             State[] states = new State[description.Size];
             // create all states, and mark as accept states if appropriate
@@ -172,11 +172,11 @@ namespace Lucene.Net.Util.Automaton
                 {
                     continue;
                 }
-                int end = xpos + Math.Min(Word.Length - xpos, range);
+                int end = xpos + Math.Min(word.Length - xpos, range);
 
-                for (int x = 0; x < Alphabet.Length; x++)
+                for (int x = 0; x < alphabet.Length; x++)
                 {
-                    int ch = Alphabet[x];
+                    int ch = alphabet[x];
                     // get the characteristic vector at this position wrt ch
                     int cvec = GetVector(ch, xpos, end);
                     int dest = description.Transition(k, xpos, cvec);
@@ -191,9 +191,9 @@ namespace Lucene.Net.Util.Automaton
                 int dest_ = description.Transition(k, xpos, 0); // by definition
                 if (dest_ >= 0)
                 {
-                    for (int r = 0; r < NumRanges; r++)
+                    for (int r = 0; r < numRanges; r++)
                     {
-                        states[k].AddTransition(new Transition(RangeLower[r], RangeUpper[r], states[dest_]));
+                        states[k].AddTransition(new Transition(rangeLower[r], rangeUpper[r], states[dest_]));
                     }
                 }
             }
@@ -221,7 +221,7 @@ namespace Lucene.Net.Util.Automaton
             for (int i = pos; i < end; i++)
             {
                 vector <<= 1;
-                if (Word[i] == x)
+                if (word[i] == x)
                 {
                     vector |= 1;
                 }
@@ -245,13 +245,13 @@ namespace Lucene.Net.Util.Automaton
         {
             protected internal readonly int w;
             protected internal readonly int n;
-            private readonly int[] MinErrors;
+            private readonly int[] minErrors;
 
             internal ParametricDescription(int w, int n, int[] minErrors)
             {
                 this.w = w;
                 this.n = n;
-                this.MinErrors = minErrors;
+                this.minErrors = minErrors;
             }
 
             /// <summary>
@@ -259,7 +259,7 @@ namespace Lucene.Net.Util.Automaton
             /// </summary>
             internal virtual int Size // LUCENENET TODO: rename Count
             {
-                get { return MinErrors.Length * (w + 1); }
+                get { return minErrors.Length * (w + 1); }
             }
 
             /// <summary>
@@ -271,7 +271,7 @@ namespace Lucene.Net.Util.Automaton
                 int state = absState / (w + 1);
                 int offset = absState % (w + 1);
                 Debug.Assert(offset >= 0);
-                return w - offset + MinErrors[state] <= n;
+                return w - offset + minErrors[state] <= n;
             }
 
             /// <summary>
