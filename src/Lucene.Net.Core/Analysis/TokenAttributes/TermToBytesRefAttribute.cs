43d79122440cd3ff151b14a834424ad44870a45d
Shad Storhaug <shad@shadstorhaug.com>
2017-01-04 21:00:39 +0700

    Lucene.Net.Core.Analysis.TokenAttributes.TermToBytesRefAttribute refactor: renamed private/internal fields camelCase and commented class because it didn't exist in Java

--- src/Lucene.Net.Core/Analysis/TokenAttributes/TermToBytesRefAttribute.cs
+++ src/Lucene.Net.Core/Analysis/TokenAttributes/TermToBytesRefAttribute.cs
@@ -1,29 +1,29 @@
-﻿using Lucene.Net.Util;
-using System;
-using Attribute = Lucene.Net.Util.Attribute;
+﻿//using Lucene.Net.Util;
+//using System;
+//using Attribute = Lucene.Net.Util.Attribute;
 
-namespace Lucene.Net.Analysis.TokenAttributes
-{
-    // LUCENENET TODO: This class does not exist in Java - Remove?
-    public class TermToBytesRefAttribute : Attribute, ITermToBytesRefAttribute
-    {
-        private BytesRef Bytes;
+//namespace Lucene.Net.Analysis.TokenAttributes
+//{
+//    // LUCENENET TODO: This class does not exist in Java - Remove?
+//    public class TermToBytesRefAttribute : Attribute, ITermToBytesRefAttribute
+//    {
+//        private BytesRef bytes;
 
-        public void FillBytesRef()
-        {
-            throw new NotImplementedException("I'm not sure what this should do");
-        }
+//        public void FillBytesRef()
+//        {
+//            throw new NotImplementedException("I'm not sure what this should do");
+//        }
 
-        public BytesRef BytesRef { get; set; }
+//        public BytesRef BytesRef { get; set; }
 
-        public override void Clear()
-        {
-        }
+//        public override void Clear()
+//        {
+//        }
 
-        public override void CopyTo(IAttribute target)
-        {
-            TermToBytesRefAttribute other = (TermToBytesRefAttribute)target;
-            other.Bytes = Bytes;
-        }
-    }
-}
+//        public override void CopyTo(IAttribute target)
+//        {
+//            TermToBytesRefAttribute other = (TermToBytesRefAttribute)target;
+//            other.bytes = bytes;
+//        }
+//    }
+//}
