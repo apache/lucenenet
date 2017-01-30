1a46f413e89093887125fc967ffb62962380aefd
Shad Storhaug <shad@shadstorhaug.com>
2017-01-30 19:03:50 +0700

    Lucene.Net.Core.Support.Arrays: Copied documentation comments from CollectionHelper.Equals(Array, Array) to Arrays.Equals(T[], T[])

--- src/Lucene.Net.Core/Support/Arrays.cs
+++ src/Lucene.Net.Core/Support/Arrays.cs
@@ -41,6 +41,19 @@ namespace Lucene.Net.Support
             }
         }
 
+        /// <summary>
+        /// Compares the entire members of one array whith the other one.
+        /// </summary>
+        /// <param name="a">The array to be compared.</param>
+        /// <param name="b">The array to be compared with.</param>
+        /// <returns>Returns true if the two specified arrays of Objects are equal
+        /// to one another. The two arrays are considered equal if both arrays
+        /// contain the same number of elements, and all corresponding pairs of
+        /// elements in the two arrays are equal. Two objects e1 and e2 are
+        /// considered equal if (e1==null ? e2==null : e1.equals(e2)). In other
+        /// words, the two arrays are equal if they contain the same elements in
+        /// the same order. Also, two array references are considered equal if
+        /// both are null.</returns>
         public static bool Equals<T>(T[] a, T[] b)
         {
             if (a == null)
