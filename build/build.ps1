873d94e8c81d44cc65c3037306a5abd4c4100dc0
Shad Storhaug <shad@shadstorhaug.com>
2017-04-23 16:14:21 +0700

    Revert "HACK: build\build.ps1: Removing --no-build switch when running .NET core tests because the test TeamCity build is not locating the dotnet-test-nunit-teamcity dependency"
    This reverts commit 4b7ab658023498d949024e166a8eae01d6a03f01.

--- build/build.ps1
+++ build/build.ps1
@@ -125,9 +125,7 @@ task Test -description "This task runs the tests" {
 			Write-Host "Directory: $projectDirectory" -ForegroundColor Green
 
 			if ($framework.StartsWith("netcore")) {
-				#$testExpression = "dotnet.exe test '$projectDirectory\project.json' --configuration $configuration --framework $framework --no-build"
-				# HACK: taking off the no-build option because this command isn't finding the dependencies when running tests and dotnet restore doesn't help
-				$testExpression = "dotnet.exe test '$projectDirectory\project.json' --configuration $configuration --framework $framework"
+				$testExpression = "dotnet.exe test '$projectDirectory\project.json' --configuration $configuration --framework $framework --no-build"
 			} else {
 				$binaryRoot = "$projectDirectory\bin\$configuration\$framework"
 
