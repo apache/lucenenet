66e6b7bac722865243be2320a1fa88f012571488
Stefan Bodewig <bodewig@apache.org>
2017-04-26 13:10:47 +0200

    add license headers and license references where needed

--- src/Lucene.Net.Tests/RectangularArrays.cs
+++ src/Lucene.Net.Tests/RectangularArrays.cs
@@ -1,5 +1,26 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
 //----------------------------------------------------------------------------------------
-//	Copyright © 2007 - 2013 Tangible Software Solutions Inc.
+//	Copyright ï¿½ 2007 - 2013 Tangible Software Solutions Inc.
 //	this class can be used by anyone provided that the copyright notice remains intact.
 //
 //	this class provides the logic to simulate Java rectangular arrays, which are jagged
