d0e5d981b8fa4ab80c21c4aa57622484475ac4d0
Shad Storhaug <shad@shadstorhaug.com>
2017-07-11 12:51:00 +0700

    Lucene.Net.Tests.Cli: Added ComonAssemblyInfo.cs reference, moved build options to common section of project.json and fixed AssemblyInfo attributes to remove conflicts.

--- src/tools/Lucene.Net.Tests.Cli/Properties/AssemblyInfo.cs
+++ src/tools/Lucene.Net.Tests.Cli/Properties/AssemblyInfo.cs
@@ -22,10 +22,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Lucene.Net.Tests.Cli")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lucene.Net.Tests.Cli")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
