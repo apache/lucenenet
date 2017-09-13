681430e44ce316e18b788598ed002f6f14dd3822
Shad Storhaug <shad@shadstorhaug.com>
2017-09-14 04:12:33 +0700

    Revert "Lucene.Net.Store.FSDirectory: Removed Fsync() method and m_staleFiles variable and all references to them" to prevent widespread 'file in use by another process' error.
    This reverts commit bc295b0e79c153d9dfa7e9f99a144db135a014f5.

--- src/Lucene.Net/Store/FSDirectory.cs
+++ src/Lucene.Net/Store/FSDirectory.cs
@@ -92,10 +92,7 @@ namespace Lucene.Net.Store
         public const int DEFAULT_READ_CHUNK_SIZE = 8192;
 
         protected readonly DirectoryInfo m_directory; // The underlying filesystem directory
-
-        // LUCENENET specific: No such thing as "stale files" in .NET, since Flush(true) writes everything to disk before
-        // our FileStream is disposed.
-        //protected readonly ISet<string> m_staleFiles = new ConcurrentHashSet<string>(); // Files written, but not yet sync'ed
+        protected readonly ISet<string> m_staleFiles = new ConcurrentHashSet<string>(); // Files written, but not yet sync'ed
 #pragma warning disable 612, 618
         private int chunkSize = DEFAULT_READ_CHUNK_SIZE;
 #pragma warning restore 612, 618
@@ -313,9 +310,7 @@ namespace Lucene.Net.Store
             {
                 throw new IOException("Cannot delete " + file, e);
             }
-            // LUCENENET specific: No such thing as "stale files" in .NET, since Flush(true) writes everything to disk before
-            // our FileStream is disposed.
-            //m_staleFiles.Remove(name);
+            m_staleFiles.Remove(name);
         }
 
         /// <summary>
@@ -358,35 +353,30 @@ namespace Lucene.Net.Store
 
         protected virtual void OnIndexOutputClosed(FSIndexOutput io)
         {
-            // LUCENENET specific: No such thing as "stale files" in .NET, since Flush(true) writes everything to disk before
-            // our FileStream is disposed.
-            //m_staleFiles.Add(io.name);
+            m_staleFiles.Add(io.name);
         }
 
         public override void Sync(ICollection<string> names)
         {
             EnsureOpen();
+            ISet<string> toSync = new HashSet<string>(names);
+            toSync.IntersectWith(m_staleFiles);
 
-            // LUCENENET specific: No such thing as "stale files" in .NET, since Flush(true) writes everything to disk before
-            // our FileStream is disposed. Therefore, there is nothing else to do in this method.
-            //ISet<string> toSync = new HashSet<string>(names);
-            //toSync.IntersectWith(m_staleFiles);
-
-            //// LUCENENET specific: Fsync breaks concurrency here.
-            //// Part of a solution suggested by Vincent Van Den Berghe: http://apache.markmail.org/message/hafnuhq2ydhfjmi2
-            ////foreach (var name in toSync)
-            ////{
-            ////    Fsync(name);
-            ////}
-
-            //// fsync the directory itsself, but only if there was any file fsynced before
-            //// (otherwise it can happen that the directory does not yet exist)!
-            //if (toSync.Count > 0)
+            // LUCENENET specific: Fsync breaks concurrency here.
+            // Part of a solution suggested by Vincent Van Den Berghe: http://apache.markmail.org/message/hafnuhq2ydhfjmi2
+            //foreach (var name in toSync)
             //{
-            //    IOUtils.Fsync(m_directory.FullName, true);
+            //    Fsync(name);
             //}
 
-            //m_staleFiles.ExceptWith(toSync);
+            // fsync the directory itsself, but only if there was any file fsynced before
+            // (otherwise it can happen that the directory does not yet exist)!
+            if (toSync.Count > 0)
+            {
+                IOUtils.Fsync(m_directory.FullName, true);
+            }
+
+            m_staleFiles.ExceptWith(toSync);
         }
 
         public override string GetLockID()
@@ -545,12 +535,9 @@ namespace Lucene.Net.Store
             // LUCENENET NOTE: FileStream doesn't have a way to set length
         }
 
-        // LUCENENET specific: Fsync is pointless in .NET, since we are 
-        // calling FileStream.Flush(true) before the stream is disposed
-        // which means we never need it at the point in Java where it is called.
-        //protected virtual void Fsync(string name)
-        //{
-        //    IOUtils.Fsync(Path.Combine(m_directory.FullName, name), false);            
-        //}
+        protected virtual void Fsync(string name)
+        {
+            IOUtils.Fsync(Path.Combine(m_directory.FullName, name), false);            
+        }
     }
 }
