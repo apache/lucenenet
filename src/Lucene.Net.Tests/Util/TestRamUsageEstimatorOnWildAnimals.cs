4854038058d384703ed7219e9661764f1f23fff6
Shad Storhaug <shad@shadstorhaug.com>
2017-09-22 16:51:04 +0700

    Lucene.Net.Tests.Util.TestRamUsageEstimatorOnWildAnimals: Reworked test so it exists in .NET Standard but does not run

--- src/Lucene.Net.Tests/Util/TestRamUsageEstimatorOnWildAnimals.cs
+++ src/Lucene.Net.Tests/Util/TestRamUsageEstimatorOnWildAnimals.cs
@@ -25,15 +25,18 @@ namespace Lucene.Net.Util
     [TestFixture]
     public class TestRamUsageEstimatorOnWildAnimals : LuceneTestCase
     {
-#if !NETSTANDARD // StackOverflowException cannot be caught in .NET Core. This test does not apply.
         public class ListElement
         {
             internal ListElement Next;
         }
 
+
         [Test]
         public virtual void TestOverflowMaxChainLength()
         {
+#if NETSTANDARD
+            AssumeTrue("StackOverflowException cannot be caught in .NET Core. This test does not apply.", false);
+#endif
             int UPPERLIMIT = 100000;
             int lower = 0;
             int upper = UPPERLIMIT;
@@ -65,6 +68,5 @@ namespace Lucene.Net.Util
                 Assert.Fail("Max object chain length till stack overflow: " + lower);
             }
         }
-#endif
     }
 }
