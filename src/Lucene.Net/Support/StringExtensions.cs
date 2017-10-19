a343b9832fb2807fb680b9717d111041ec951e9c
Shad Storhaug <shad@shadstorhaug.com>
2017-10-19 22:22:33 +0700

    BREAKING: Lucene.Net.Support: Removed CharAt() method from StringCharSequenceWrapper.

--- src/Lucene.Net/Support/StringExtensions.cs
+++ src/Lucene.Net/Support/StringExtensions.cs
@@ -97,7 +97,7 @@ namespace Lucene.Net.Support
 
         /// <summary>
         /// Convenience method to wrap a string in a <see cref="StringCharSequenceWrapper"/>
-        /// so a string can be used as <see cref="ICharSequence"/> in .NET.
+        /// so a <see cref="string"/> can be used as <see cref="ICharSequence"/> in .NET.
         /// </summary>
         public static ICharSequence ToCharSequence(this string str)
         {
