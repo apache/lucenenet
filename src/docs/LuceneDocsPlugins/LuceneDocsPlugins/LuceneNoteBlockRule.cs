80c5f20494423051edff64febd23de47832c2ee4
Shannon <sdeminick@gmail.com>
2018-08-21 17:08:57 +1000

    Revert "Attempting to modify the markdig markdown engine to process special tags inside the triple-slash comments ... but can't get it to work so will revert back to dfm, just keeping this here for history"
    This reverts commit efb0b002c5f35c990cffd0b3badffdaefb3e82f1.

---
+++ src/docs/LuceneDocsPlugins/LuceneDocsPlugins/LuceneNoteBlockRule.cs
@@ -0,0 +1,26 @@
+using System.Text.RegularExpressions;
+using Microsoft.DocAsCode.MarkdownLite;
+
+namespace LuceneDocsPlugins
+{
+    /// <summary>
+    /// The regex rule to parse out the custom Lucene tokens
+    /// </summary>
+    public class LuceneNoteBlockRule : IMarkdownRule
+    {       
+        public virtual Regex LabelRegex { get; } = new Regex("^@lucene\\.(?<notetype>(experimental|internal))$");
+
+        public virtual IMarkdownToken TryMatch(IMarkdownParser parser, IMarkdownParsingContext context)
+        {
+            var match = LabelRegex.Match(context.CurrentMarkdown);
+            if (match.Length == 0)
+            {
+                return null;
+            }
+            var sourceInfo = context.Consume(match.Length);
+            return new LuceneNoteBlockToken(this, parser.Context, match.Groups[1].Value, sourceInfo);
+        }
+
+        public virtual string Name => "LuceneNote";
+    }
+}
---
+++ src/docs/LuceneDocsPlugins/LuceneDocsPlugins/LuceneRendererPartProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using Microsoft.DocAsCode.Dfm;
+
+namespace LuceneDocsPlugins
+{
+    /// <summary>
+    /// Exports our custom renderer via MEF to DocFx
+    /// </summary>
+    [Export(typeof(IDfmCustomizedRendererPartProvider))]
+    public class LuceneRendererPartProvider : IDfmCustomizedRendererPartProvider
+    {
+        public IEnumerable<IDfmCustomizedRendererPart> CreateParts(IReadOnlyDictionary<string, object> parameters)
+        {
+            yield return new LuceneTokenRendererPart();
+        }
+    }
+}
