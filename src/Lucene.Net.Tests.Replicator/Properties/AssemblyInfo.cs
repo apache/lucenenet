3a79b9bd03555c8843573bbab9f4a418a23dc19e
Shad Storhaug <shad@shadstorhaug.com>
2019-07-14 01:27:05 +0700

    Lucene.Net.Tests.Replicator: Timeout doesn't exist on .NET Standard 2.0 (as of NUnit 3.9.0) either, patching build

--- src/Lucene.Net.Tests.Replicator/Properties/AssemblyInfo.cs
+++ src/Lucene.Net.Tests.Replicator/Properties/AssemblyInfo.cs
@@ -36,11 +36,23 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("418e9d8e-2369-4b52-8d2f-5a987213999b")]
 
-#if !NETSTANDARD1_6
+#if !NETSTANDARD
 // LUCENENET TODO: 2019-07-14 - Had a hanging test in Lucene.Net.Tests.Replicator 
 // on .NET Framwork 4.5 (not sure which one),
 // but unable to repeat. Adding this timeout (with at least 10x the time it usually takes
 // to run all of these tests) to ensure if we get a hang again, the hanging test will fail
 // so we know which test to investigate.
 [assembly: NUnit.Framework.Timeout(120000)]
+
+All we know for certain is that it wasn't any of these tests
+
+√ TestBasic [1s 220ms]
+√ TestNoUpdateThread [313ms]
+√ TestRecreateTaxonomy [233ms]
+√ TestRestart [380ms]
+√ TestUpdateThread [314ms]
+√ TestNoCommit [26ms]
+√ TestOpen [31ms]
+√ TestRevisionRelease [5ms]
+√ TestSegmentsFileLast [3ms]
 #endif
