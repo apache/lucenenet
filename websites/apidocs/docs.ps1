4e27f809fcdad6dcaca28103a3bd4c4365f17c91
Shannon <sdeminick@gmail.com>
2019-08-16 12:46:07 +1000

    updates doc build

--- websites/apidocs/docs.ps1
+++ websites/apidocs/docs.ps1
@@ -111,7 +111,7 @@ New-Item $PluginsFolder -type directory -force
 # NOTE: There's a ton of Lucene docs that we want to copy and re-format. I'm not sure if we can really automate this 
 # in a great way since the docs seem to be in many places, for example:
 # https://github.com/dotnet/docfx/issues/1969
-# Then it turns out we also need 2015 build tools installed, wat!? 
+# Then it turns out we also need 2017 build tools installed, wat!? 
 # https://www.microsoft.com/en-us/download/details.aspx?id=48159
 # Home page - 	https://github.com/apache/lucene-solr/blob/branch_4x/lucene/site/xsl/index.xsl
 # Wiki docs - 	https://wiki.apache.org/lucene-java/FrontPage?action=show&redirect=FrontPageEN - not sure where the source is for this
@@ -119,6 +119,7 @@ New-Item $PluginsFolder -type directory -force
 # UPDATE: Interestingly it now works by passing in the most recent msbuild target...
 # TODO: Need to upgrade to latest docfx and figure out why there are issues.
 
+[Environment]::SetEnvironmentVariable("VSINSTALLDIR", $msbuild)
 [Environment]::SetEnvironmentVariable("VisualStudioVersion", "15.0")
 
 $DocFxJson = Join-Path -Path $ApiDocsFolder "docfx.json"
