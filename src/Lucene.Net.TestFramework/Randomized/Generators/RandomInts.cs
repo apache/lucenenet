e1105ae6dbd4a63e630f076ca14563da73f87e9a
Shad Storhaug <shad@shadstorhaug.com>
2019-08-18 19:32:29 +0700

    Lucene.Net.TestFramework.Randomized.Generators.RandomInts: Removed duplicate NextBoolean(), NextSingle(), and NextInt64() implementations. NOTE: The NextInt64 implementation here was flawed in that it was always positive and had a very narrow range of values, thus giving us poor random distribution.

--- src/Lucene.Net.TestFramework/Randomized/Generators/RandomInts.cs
+++ src/Lucene.Net.TestFramework/Randomized/Generators/RandomInts.cs
@@ -1,32 +1,25 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-*/
-
 using Lucene.Net.Support;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
 namespace Lucene.Net.Randomized.Generators
 {
+    /*
+     * Licensed to the Apache Software Foundation (ASF) under one or more
+     * contributor license agreements.  See the NOTICE file distributed with
+     * this work for additional information regarding copyright ownership.
+     * The ASF licenses this file to You under the Apache License, Version 2.0
+     * (the "License"); you may not use this file except in compliance with
+     * the License.  You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
 
     /// <summary>
     /// Utility class for random integer and integer sequences.
@@ -47,24 +40,6 @@ namespace Lucene.Net.Randomized.Generators
             return min + (int)Math.Round(random.NextDouble() * range);
         }
 
-        public static Boolean NextBoolean(this Random random)
-        {
-            return random.NextDouble() > 0.5;
-        }
-
-        public static float NextSingle(this Random random)
-        {
-            return (float)random.NextDouble();
-        }
-
         /* .NET has random.Next(max) which negates the need for randomInt(Random random, int max) as  */
-
-        public static long NextInt64(this Random random)
-        {
-            int i1 = random.Next();
-            int i2 = random.Next();
-            long l12 = ((i1 << 32) | i2);
-            return l12;
-        }
     }
 }
