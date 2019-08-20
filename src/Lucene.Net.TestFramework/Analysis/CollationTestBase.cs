974c6154be0dd87b736a3fe6bd59e0910ac4a216
Shad Storhaug <shad@shadstorhaug.com>
2019-08-20 19:29:42 +0700

    Lucene.Net.TestFramework.Analysis.CollationTestBase: Added using blocks for disposable types.

--- src/Lucene.Net.TestFramework/Analysis/CollationTestBase.cs
+++ src/Lucene.Net.TestFramework/Analysis/CollationTestBase.cs
@@ -65,86 +65,95 @@ namespace Lucene.Net.Analysis
 
         public virtual void TestFarsiRangeFilterCollating(Analyzer analyzer, BytesRef firstBeg, BytesRef firstEnd, BytesRef secondBeg, BytesRef secondEnd)
         {
-            Directory dir = NewDirectory();
-            IndexWriter writer = new IndexWriter(dir, new IndexWriterConfig(LuceneTestCase.TEST_VERSION_CURRENT, analyzer));
-            Document doc = new Document();
-            doc.Add(new TextField("content", "\u0633\u0627\u0628", Field.Store.YES));
-            doc.Add(new StringField("body", "body", Field.Store.YES));
-            writer.AddDocument(doc);
-            writer.Dispose();
-            IndexReader reader = DirectoryReader.Open(dir);
-            IndexSearcher searcher = new IndexSearcher(reader);
-            Search.Query query = new TermQuery(new Term("body", "body"));
+            using (Directory dir = NewDirectory())
+            {
+                using (IndexWriter writer = new IndexWriter(dir, new IndexWriterConfig(LuceneTestCase.TEST_VERSION_CURRENT, analyzer)))
+                {
+                    Document doc = new Document();
+                    doc.Add(new TextField("content", "\u0633\u0627\u0628", Field.Store.YES));
+                    doc.Add(new StringField("body", "body", Field.Store.YES));
+                    writer.AddDocument(doc);
+                } // writer.Dispose();
+                using (IndexReader reader = DirectoryReader.Open(dir))
+                {
+                    IndexSearcher searcher = new IndexSearcher(reader);
+                    Search.Query query = new TermQuery(new Term("body", "body"));
 
-            // Unicode order would include U+0633 in [ U+062F - U+0698 ], but Farsi
-            // orders the U+0698 character before the U+0633 character, so the single
-            // index Term below should NOT be returned by a TermRangeFilter with a Farsi
-            // Collator (or an Arabic one for the case when Farsi searcher not
-            // supported).
-            ScoreDoc[] result = searcher.Search(query, new TermRangeFilter("content", firstBeg, firstEnd, true, true), 1).ScoreDocs;
-            Assert.AreEqual(0, result.Length, "The index Term should not be included.");
+                    // Unicode order would include U+0633 in [ U+062F - U+0698 ], but Farsi
+                    // orders the U+0698 character before the U+0633 character, so the single
+                    // index Term below should NOT be returned by a TermRangeFilter with a Farsi
+                    // Collator (or an Arabic one for the case when Farsi searcher not
+                    // supported).
+                    ScoreDoc[] result = searcher.Search(query, new TermRangeFilter("content", firstBeg, firstEnd, true, true), 1).ScoreDocs;
+                    Assert.AreEqual(0, result.Length, "The index Term should not be included.");
 
-            result = searcher.Search(query, new TermRangeFilter("content", secondBeg, secondEnd, true, true), 1).ScoreDocs;
-            Assert.AreEqual(1, result.Length, "The index Term should be included.");
+                    result = searcher.Search(query, new TermRangeFilter("content", secondBeg, secondEnd, true, true), 1).ScoreDocs;
+                    Assert.AreEqual(1, result.Length, "The index Term should be included.");
 
-            reader.Dispose();
-            dir.Dispose();
+                } // reader.Dispose();
+            } // dir.Dispose();
         }
 
         public virtual void TestFarsiRangeQueryCollating(Analyzer analyzer, BytesRef firstBeg, BytesRef firstEnd, BytesRef secondBeg, BytesRef secondEnd)
         {
-            Directory dir = NewDirectory();
-            IndexWriter writer = new IndexWriter(dir, new IndexWriterConfig(LuceneTestCase.TEST_VERSION_CURRENT, analyzer));
-            Document doc = new Document();
+            using (Directory dir = NewDirectory())
+            {
+                using (IndexWriter writer = new IndexWriter(dir, new IndexWriterConfig(LuceneTestCase.TEST_VERSION_CURRENT, analyzer)))
+                {
+                    Document doc = new Document();
 
-            // Unicode order would include U+0633 in [ U+062F - U+0698 ], but Farsi
-            // orders the U+0698 character before the U+0633 character, so the single
-            // index Term below should NOT be returned by a TermRangeQuery with a Farsi
-            // Collator (or an Arabic one for the case when Farsi is not supported).
-            doc.Add(new TextField("content", "\u0633\u0627\u0628", Field.Store.YES));
-            writer.AddDocument(doc);
-            writer.Dispose();
-            IndexReader reader = DirectoryReader.Open(dir);
-            IndexSearcher searcher = new IndexSearcher(reader);
+                    // Unicode order would include U+0633 in [ U+062F - U+0698 ], but Farsi
+                    // orders the U+0698 character before the U+0633 character, so the single
+                    // index Term below should NOT be returned by a TermRangeQuery with a Farsi
+                    // Collator (or an Arabic one for the case when Farsi is not supported).
+                    doc.Add(new TextField("content", "\u0633\u0627\u0628", Field.Store.YES));
+                    writer.AddDocument(doc);
+                } // writer.Dispose();
+                using (IndexReader reader = DirectoryReader.Open(dir))
+                {
+                    IndexSearcher searcher = new IndexSearcher(reader);
 
-            Search.Query query = new TermRangeQuery("content", firstBeg, firstEnd, true, true);
-            ScoreDoc[] hits = searcher.Search(query, null, 1000).ScoreDocs;
-            AreEqual(0, hits.Length, "The index Term should not be included.");
+                    Search.Query query = new TermRangeQuery("content", firstBeg, firstEnd, true, true);
+                    ScoreDoc[] hits = searcher.Search(query, null, 1000).ScoreDocs;
+                    AreEqual(0, hits.Length, "The index Term should not be included.");
 
-            query = new TermRangeQuery("content", secondBeg, secondEnd, true, true);
-            hits = searcher.Search(query, null, 1000).ScoreDocs;
-            Assert.AreEqual(1, hits.Length, "The index Term should be included.");
-            reader.Dispose();
-            dir.Dispose();
+                    query = new TermRangeQuery("content", secondBeg, secondEnd, true, true);
+                    hits = searcher.Search(query, null, 1000).ScoreDocs;
+                    Assert.AreEqual(1, hits.Length, "The index Term should be included.");
+                } // reader.Dispose();
+            } // dir.Dispose();
         }
 
         public virtual void TestFarsiTermRangeQuery(Analyzer analyzer, BytesRef firstBeg, BytesRef firstEnd, BytesRef secondBeg, BytesRef secondEnd)
         {
-            Directory farsiIndex = NewDirectory();
-            IndexWriter writer = new IndexWriter(farsiIndex, new IndexWriterConfig(LuceneTestCase.TEST_VERSION_CURRENT, analyzer));
-            Document doc = new Document();
-            doc.Add(new TextField("content", "\u0633\u0627\u0628", Field.Store.YES));
-            doc.Add(new StringField("body", "body", Field.Store.YES));
-            writer.AddDocument(doc);
-            writer.Dispose();
+            using (Directory farsiIndex = NewDirectory())
+            {
+                using (IndexWriter writer = new IndexWriter(farsiIndex, new IndexWriterConfig(LuceneTestCase.TEST_VERSION_CURRENT, analyzer)))
+                {
+                    Document doc = new Document();
+                    doc.Add(new TextField("content", "\u0633\u0627\u0628", Field.Store.YES));
+                    doc.Add(new StringField("body", "body", Field.Store.YES));
+                    writer.AddDocument(doc);
+                } // writer.Dispose();
 
-            IndexReader reader = DirectoryReader.Open(farsiIndex);
-            IndexSearcher search = this.NewSearcher(reader);
+                using (IndexReader reader = DirectoryReader.Open(farsiIndex))
+                {
+                    IndexSearcher search = this.NewSearcher(reader);
 
-            // Unicode order would include U+0633 in [ U+062F - U+0698 ], but Farsi
-            // orders the U+0698 character before the U+0633 character, so the single
-            // index Term below should NOT be returned by a TermRangeQuery
-            // with a Farsi Collator (or an Arabic one for the case when Farsi is
-            // not supported).
-            Search.Query csrq = new TermRangeQuery("content", firstBeg, firstEnd, true, true);
-            ScoreDoc[] result = search.Search(csrq, null, 1000).ScoreDocs;
-            Assert.AreEqual(0, result.Length, "The index Term should not be included.");
+                    // Unicode order would include U+0633 in [ U+062F - U+0698 ], but Farsi
+                    // orders the U+0698 character before the U+0633 character, so the single
+                    // index Term below should NOT be returned by a TermRangeQuery
+                    // with a Farsi Collator (or an Arabic one for the case when Farsi is
+                    // not supported).
+                    Search.Query csrq = new TermRangeQuery("content", firstBeg, firstEnd, true, true);
+                    ScoreDoc[] result = search.Search(csrq, null, 1000).ScoreDocs;
+                    Assert.AreEqual(0, result.Length, "The index Term should not be included.");
 
-            csrq = new TermRangeQuery("content", secondBeg, secondEnd, true, true);
-            result = search.Search(csrq, null, 1000).ScoreDocs;
-            Assert.AreEqual(1, result.Length, "The index Term should be included.");
-            reader.Dispose();
-            farsiIndex.Dispose();
+                    csrq = new TermRangeQuery("content", secondBeg, secondEnd, true, true);
+                    result = search.Search(csrq, null, 1000).ScoreDocs;
+                    Assert.AreEqual(1, result.Length, "The index Term should be included.");
+                } // reader.Dispose();
+            } // farsiIndex.Dispose();
         }
 
         /// <summary>
@@ -166,61 +175,63 @@ namespace Lucene.Net.Analysis
                                                 string svResult, 
                                                 string dkResult)
         {
-            Directory indexStore = NewDirectory();
-            IndexWriter writer = new IndexWriter(indexStore, new IndexWriterConfig(LuceneTestCase.TEST_VERSION_CURRENT, new MockAnalyzer(Random, MockTokenizer.WHITESPACE, false)));
-
-            // document data:
-            // the tracer field is used to determine which document was hit
-            string[][] sortData = new string[][] { new string[] { "A", "x", "p\u00EAche", "p\u00EAche", "p\u00EAche", "p\u00EAche" }, new string[] { "B", "y", "HAT", "HAT", "HAT", "HAT" }, new string[] { "C", "x", "p\u00E9ch\u00E9", "p\u00E9ch\u00E9", "p\u00E9ch\u00E9", "p\u00E9ch\u00E9" }, new string[] { "D", "y", "HUT", "HUT", "HUT", "HUT" }, new string[] { "E", "x", "peach", "peach", "peach", "peach" }, new string[] { "F", "y", "H\u00C5T", "H\u00C5T", "H\u00C5T", "H\u00C5T" }, new string[] { "G", "x", "sin", "sin", "sin", "sin" }, new string[] { "H", "y", "H\u00D8T", "H\u00D8T", "H\u00D8T", "H\u00D8T" }, new string[] { "I", "x", "s\u00EDn", "s\u00EDn", "s\u00EDn", "s\u00EDn" }, new string[] { "J", "y", "HOT", "HOT", "HOT", "HOT" } };
-
-            FieldType customType = new FieldType();
-            customType.IsStored = true;
-
-            for (int i = 0; i < sortData.Length; ++i)
+            using (Directory indexStore = NewDirectory())
             {
-                Document doc = new Document();
-                doc.Add(new Field("tracer", sortData[i][0], customType));
-                doc.Add(new TextField("contents", sortData[i][1], Field.Store.NO));
-                if (sortData[i][2] != null)
+                using (IndexWriter writer = new IndexWriter(indexStore, new IndexWriterConfig(LuceneTestCase.TEST_VERSION_CURRENT, new MockAnalyzer(Random, MockTokenizer.WHITESPACE, false))))
                 {
-                    doc.Add(new TextField("US", usAnalyzer.GetTokenStream("US", new StringReader(sortData[i][2]))));
-                }
-                if (sortData[i][3] != null)
+                    // document data:
+                    // the tracer field is used to determine which document was hit
+                    string[][] sortData = new string[][] { new string[] { "A", "x", "p\u00EAche", "p\u00EAche", "p\u00EAche", "p\u00EAche" }, new string[] { "B", "y", "HAT", "HAT", "HAT", "HAT" }, new string[] { "C", "x", "p\u00E9ch\u00E9", "p\u00E9ch\u00E9", "p\u00E9ch\u00E9", "p\u00E9ch\u00E9" }, new string[] { "D", "y", "HUT", "HUT", "HUT", "HUT" }, new string[] { "E", "x", "peach", "peach", "peach", "peach" }, new string[] { "F", "y", "H\u00C5T", "H\u00C5T", "H\u00C5T", "H\u00C5T" }, new string[] { "G", "x", "sin", "sin", "sin", "sin" }, new string[] { "H", "y", "H\u00D8T", "H\u00D8T", "H\u00D8T", "H\u00D8T" }, new string[] { "I", "x", "s\u00EDn", "s\u00EDn", "s\u00EDn", "s\u00EDn" }, new string[] { "J", "y", "HOT", "HOT", "HOT", "HOT" } };
+
+                    FieldType customType = new FieldType();
+                    customType.IsStored = true;
+
+                    for (int i = 0; i < sortData.Length; ++i)
+                    {
+                        Document doc = new Document();
+                        doc.Add(new Field("tracer", sortData[i][0], customType));
+                        doc.Add(new TextField("contents", sortData[i][1], Field.Store.NO));
+                        if (sortData[i][2] != null)
+                        {
+                            doc.Add(new TextField("US", usAnalyzer.GetTokenStream("US", new StringReader(sortData[i][2]))));
+                        }
+                        if (sortData[i][3] != null)
+                        {
+                            doc.Add(new TextField("France", franceAnalyzer.GetTokenStream("France", new StringReader(sortData[i][3]))));
+                        }
+                        if (sortData[i][4] != null)
+                        {
+                            doc.Add(new TextField("Sweden", swedenAnalyzer.GetTokenStream("Sweden", new StringReader(sortData[i][4]))));
+                        }
+                        if (sortData[i][5] != null)
+                        {
+                            doc.Add(new TextField("Denmark", denmarkAnalyzer.GetTokenStream("Denmark", new StringReader(sortData[i][5]))));
+                        }
+                        writer.AddDocument(doc);
+                    }
+                    writer.ForceMerge(1);
+                } // writer.Dispose();
+                using (IndexReader reader = DirectoryReader.Open(indexStore))
                 {
-                    doc.Add(new TextField("France", franceAnalyzer.GetTokenStream("France", new StringReader(sortData[i][3]))));
-                }
-                if (sortData[i][4] != null)
-                {
-                    doc.Add(new TextField("Sweden", swedenAnalyzer.GetTokenStream("Sweden", new StringReader(sortData[i][4]))));
-                }
-                if (sortData[i][5] != null)
-                {
-                    doc.Add(new TextField("Denmark", denmarkAnalyzer.GetTokenStream("Denmark", new StringReader(sortData[i][5]))));
-                }
-                writer.AddDocument(doc);
-            }
-            writer.ForceMerge(1);
-            writer.Dispose();
-            IndexReader reader = DirectoryReader.Open(indexStore);
-            IndexSearcher searcher = new IndexSearcher(reader);
+                    IndexSearcher searcher = new IndexSearcher(reader);
 
-            Sort sort = new Sort();
-            Search.Query queryX = new TermQuery(new Term("contents", "x"));
-            Search.Query queryY = new TermQuery(new Term("contents", "y"));
+                    Sort sort = new Sort();
+                    Search.Query queryX = new TermQuery(new Term("contents", "x"));
+                    Search.Query queryY = new TermQuery(new Term("contents", "y"));
 
-            sort.SetSort(new SortField("US", SortFieldType.STRING));
-            this.AssertMatches(searcher, queryY, sort, usResult);
+                    sort.SetSort(new SortField("US", SortFieldType.STRING));
+                    this.AssertMatches(searcher, queryY, sort, usResult);
 
-            sort.SetSort(new SortField("France", SortFieldType.STRING));
-            this.AssertMatches(searcher, queryX, sort, frResult);
+                    sort.SetSort(new SortField("France", SortFieldType.STRING));
+                    this.AssertMatches(searcher, queryX, sort, frResult);
 
-            sort.SetSort(new SortField("Sweden", SortFieldType.STRING));
-            this.AssertMatches(searcher, queryY, sort, svResult);
+                    sort.SetSort(new SortField("Sweden", SortFieldType.STRING));
+                    this.AssertMatches(searcher, queryY, sort, svResult);
 
-            sort.SetSort(new SortField("Denmark", SortFieldType.STRING));
-            this.AssertMatches(searcher, queryY, sort, dkResult);
-            reader.Dispose();
-            indexStore.Dispose();
+                    sort.SetSort(new SortField("Denmark", SortFieldType.STRING));
+                    this.AssertMatches(searcher, queryY, sort, dkResult);
+                } // reader.Dispose();
+            } // indexStore.Dispose();
         }
 
         /// <summary>
