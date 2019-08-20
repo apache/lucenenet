ed13844ec2b9921964c709cf3b06b2f706e26830
Shad Storhaug <shad@shadstorhaug.com>
2019-08-20 08:51:38 +0700

    Lucene.Net.TestFramework.Analysis.MockPayloadAnalyzer: Fixed issue with string being passed into BytesRef instead of UTF-8 encoded bytes as was done in Java

--- src/Lucene.Net.TestFramework/Analysis/MockPayloadAnalyzer.cs
+++ src/Lucene.Net.TestFramework/Analysis/MockPayloadAnalyzer.cs
@@ -1,25 +1,25 @@
 using Lucene.Net.Analysis.TokenAttributes;
+using System.IO;
+using System.Text;
 
 namespace Lucene.Net.Analysis
 {
-    using System.IO;
-
     /*
-         * Licensed to the Apache Software Foundation (ASF) under one or more
-         * contributor license agreements.  See the NOTICE file distributed with
-         * this work for additional information regarding copyright ownership.
-         * The ASF licenses this file to You under the Apache License, Version 2.0
-         * (the "License"); you may not use this file except in compliance with
-         * the License.  You may obtain a copy of the License at
-         *
-         *     http://www.apache.org/licenses/LICENSE-2.0
-         *
-         * Unless required by applicable law or agreed to in writing, software
-         * distributed under the License is distributed on an "AS IS" BASIS,
-         * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-         * See the License for the specific language governing permissions and
-         * limitations under the License.
-         */
+    * Licensed to the Apache Software Foundation (ASF) under one or more
+    * contributor license agreements.  See the NOTICE file distributed with
+    * this work for additional information regarding copyright ownership.
+    * The ASF licenses this file to You under the Apache License, Version 2.0
+    * (the "License"); you may not use this file except in compliance with
+    * the License.  You may obtain a copy of the License at
+    *
+    *     http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+    */
 
     using BytesRef = Lucene.Net.Util.BytesRef;
 
@@ -67,7 +67,7 @@ namespace Lucene.Net.Analysis
         {
             if (m_input.IncrementToken())
             {
-                payloadAttr.Payload = new BytesRef(("pos: " + pos)/*.getBytes(IOUtils.CHARSET_UTF_8)*/); // LUCENENET TODO: UTF8 conversion
+                payloadAttr.Payload = new BytesRef(Encoding.UTF8.GetBytes("pos: " + pos));
                 int posIncr;
                 if (pos == 0 || i % 2 == 1)
                 {
