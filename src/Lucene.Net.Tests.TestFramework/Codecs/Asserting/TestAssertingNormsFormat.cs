5f5627371158d77d945bc3574ecede133d6030cd
Shad Storhaug <shad@shadstorhaug.com>
2019-09-19 12:52:41 +0700

    Lucene.Net.Tests.TestFramework.Codecs.Asserting: Added TestAssertingNormsFormat

---
+++ src/Lucene.Net.Tests.TestFramework/Codecs/Asserting/TestAssertingNormsFormat.cs
@@ -0,0 +1,53 @@
+﻿// Lucene version compatibility level 8.2.0
+using Lucene.Net.Index;
+using Lucene.Net.TestFramework;
+
+namespace Lucene.Net.Codecs.Asserting
+{
+    /*
+    * Licensed to the Apache Software Foundation (ASF) under one or more
+    * contributor license agreements.  See the NOTICE file distributed with
+    * this work for additional information regarding copyright ownership.
+    * The ASF licenses this file to You under the Apache License, Version 2.0
+    * (the "License"); you may not use this file except in compliance with
+    * the License.  You may obtain a copy of the License at
+    *
+    *     http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+    */
+
+    /// <summary>
+    /// Test <see cref="AssertingNormsFormat"/> directly
+    /// </summary>
+#if TESTFRAMEWORK_MSTEST
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute]
+#endif
+    public class TestAssertingNormsFormat : BaseTermVectorsFormatTestCase
+    {
+#if TESTFRAMEWORK_MSTEST
+        [Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute]
+        new public static void BeforeClass(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext context)
+        {
+            Lucene.Net.Util.LuceneTestCase.BeforeClass(context);
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.ClassCleanupAttribute]
+        new public static void AfterClass()
+        {
+            Lucene.Net.Util.LuceneTestCase.AfterClass();
+        }
+#endif
+
+        private readonly Codec codec = new AssertingCodec();
+
+        protected override Codec GetCodec()
+        {
+            return codec;
+        }
+    }
+}
