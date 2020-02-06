3eb9d39d62193ed81482601d74c08ebb8f69c62c
Shad Storhaug <shad@shadstorhaug.com>
2020-02-06 20:29:43 +0700

    BREAKING: Lucene.Net.Support.Threading: Marked ICompletionService and TaskSchedulerCompletionService internal

--- src/Lucene.Net/Support/Threading/TaskSchedulerCompletionService.cs
+++ src/Lucene.Net/Support/Threading/TaskSchedulerCompletionService.cs
@@ -4,23 +4,23 @@ using System.Threading.Tasks;
 namespace Lucene.Net.Support.Threading
 {
     /*
-	 * Licensed to the Apache Software Foundation (ASF) under one or more
-	 * contributor license agreements.  See the NOTICE file distributed with
-	 * this work for additional information regarding copyright ownership.
-	 * The ASF licenses this file to You under the Apache License, Version 2.0
-	 * (the "License"); you may not use this file except in compliance with
-	 * the License.  You may obtain a copy of the License at
-	 *
-	 *     http://www.apache.org/licenses/LICENSE-2.0
-	 *
-	 * Unless required by applicable law or agreed to in writing, software
-	 * distributed under the License is distributed on an "AS IS" BASIS,
-	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	 * See the License for the specific language governing permissions and
-	 * limitations under the License.
-	 */
+     * Licensed to the Apache Software Foundation (ASF) under one or more
+     * contributor license agreements.  See the NOTICE file distributed with
+     * this work for additional information regarding copyright ownership.
+     * The ASF licenses this file to You under the Apache License, Version 2.0
+     * (the "License"); you may not use this file except in compliance with
+     * the License.  You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
 
-    public class TaskSchedulerCompletionService<T> : ICompletionService<T>
+    internal class TaskSchedulerCompletionService<T> : ICompletionService<T>
     {
         private readonly TaskFactory<T> factory;
         private readonly Queue<Task<T>> taskQueue = new Queue<Task<T>>();
