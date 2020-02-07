c3c7f7a53736e43979f26f10ef37809b987e0912
Shad Storhaug <shad@shadstorhaug.com>
2020-02-07 10:13:17 +0700

    BREAKING: Lucene.Net.Support.IO: Marked FileStreamExtensions and StreamExtensions internal

--- src/Lucene.Net/Support/IO/StreamExtensions.cs
+++ src/Lucene.Net/Support/IO/StreamExtensions.cs
@@ -3,21 +3,21 @@
 namespace Lucene.Net.Support.IO
 {
     /*
-	 * Licensed to the Apache Software Foundation (ASF) under one or more
-	 * contributor license agreements.  See the NOTICE file distributed with
-	 * this work for additional information regarding copyright ownership.
-	 * The ASF licenses this file to You under the Apache License, Version 2.0
-	 * (the "License"); you may not use this file except in compliance with
-	 * the License.  You may obtain a copy of the License at
-	 *
-	 *     http://www.apache.org/licenses/LICENSE-2.0
-	 *
-	 * Unless required by applicable law or agreed to in writing, software
-	 * distributed under the License is distributed on an "AS IS" BASIS,
-	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	 * See the License for the specific language governing permissions and
-	 * limitations under the License.
-	 */
+     * Licensed to the Apache Software Foundation (ASF) under one or more
+     * contributor license agreements.  See the NOTICE file distributed with
+     * this work for additional information regarding copyright ownership.
+     * The ASF licenses this file to You under the Apache License, Version 2.0
+     * (the "License"); you may not use this file except in compliance with
+     * the License.  You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
 
     /// <summary>
     /// Extension methods that make a <see cref="Stream"/> effectively into a
@@ -30,7 +30,7 @@ namespace Lucene.Net.Support.IO
     /// <para/>
     /// Byte order is little-endian (same as <see cref="BinaryReader"/> and <see cref="BinaryWriter"/>).
     /// </summary>
-    public static class StreamExtensions
+    internal static class StreamExtensions
     {
         public static void Write(this Stream stream, char[] chars)
         {
