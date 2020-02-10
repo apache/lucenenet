b5a61c81ba1808d0969e5b6ae8a76070a8a92b70
Shad Storhaug <shad@shadstorhaug.com>
2020-02-11 05:10:01 +0700

    BUG: Lucene.Net.Index.DocumentsWriterDeleteQueue::DeleteSlice(): Extra Debug.Assert() statement that wasn't in Lucene causes Lucene.Net.Index.TestIndexWriterUnicode::TestEmbeddedFFFF() test to fail when running in debug mode

--- src/Lucene.Net/Index/DocumentsWriterDeleteQueue.cs
+++ src/Lucene.Net/Index/DocumentsWriterDeleteQueue.cs
@@ -294,7 +294,6 @@ namespace Lucene.Net.Index
             internal DeleteSlice(Node currentTail)
             {
                 Debug.Assert(currentTail != null);
-                Debug.Assert(currentTail.next == null);
                 /*
                  * Initially this is a 0 length slice pointing to the 'current' tail of
                  * the queue. Once we update the slice we only need to assign the tail and
