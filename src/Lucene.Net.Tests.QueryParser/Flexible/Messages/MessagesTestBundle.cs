60d1b82832b0b8696e31576de3698730deb0cc11
Shad Storhaug <shad@shadstorhaug.com>
2021-05-01 06:32:57 +0700

    BREAKING: Lucene.Net.QueryParsers.Flexible.Messages: Removed entire namespace, as we have refactored to use .NET localization rather than NLS

--- src/Lucene.Net.Tests.QueryParser/Flexible/Messages/MessagesTestBundle.cs
+++ src/Lucene.Net.Tests.QueryParser/Flexible/Messages/MessagesTestBundle.cs
@@ -1,45 +1,48 @@
-﻿using System;
+﻿// LUCENENET specific - Factored out NLS so end users can elect to use .NET localization or not
+// rather than forcing them to use it.
 
-namespace Lucene.Net.QueryParsers.Flexible.Messages
-{
-    /*
-     * Licensed to the Apache Software Foundation (ASF) under one or more
-     * contributor license agreements.  See the NOTICE file distributed with
-     * this work for additional information regarding copyright ownership.
-     * The ASF licenses this file to You under the Apache License, Version 2.0
-     * (the "License"); you may not use this file except in compliance with
-     * the License.  You may obtain a copy of the License at
-     *
-     *     http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
+//using System;
 
-    public class MessagesTestBundle : NLS
-    {
-        private static readonly string BUNDLE_NAME = typeof(MessagesTestBundle).Name;
+//namespace Lucene.Net.QueryParsers.Flexible.Messages
+//{
+//    /*
+//     * Licensed to the Apache Software Foundation (ASF) under one or more
+//     * contributor license agreements.  See the NOTICE file distributed with
+//     * this work for additional information regarding copyright ownership.
+//     * The ASF licenses this file to You under the Apache License, Version 2.0
+//     * (the "License"); you may not use this file except in compliance with
+//     * the License.  You may obtain a copy of the License at
+//     *
+//     *     http://www.apache.org/licenses/LICENSE-2.0
+//     *
+//     * Unless required by applicable law or agreed to in writing, software
+//     * distributed under the License is distributed on an "AS IS" BASIS,
+//     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     * See the License for the specific language governing permissions and
+//     * limitations under the License.
+//     */
 
-        private MessagesTestBundle()
-        {
-            // should never be instantiated
-        }
+//    public class MessagesTestBundle : NLS
+//    {
+//        private static readonly string BUNDLE_NAME = typeof(MessagesTestBundle).Name;
 
-        static MessagesTestBundle()
-        {
-            // register all string ids with NLS class and initialize static string
-            // values
-            NLS.InitializeMessages(BUNDLE_NAME, typeof(MessagesTestBundle));
-        }
+//        private MessagesTestBundle()
+//        {
+//            // should never be instantiated
+//        }
 
-        // static string must match the strings in the property files.
-        public static String Q0001E_INVALID_SYNTAX;
-        public static String Q0004E_INVALID_SYNTAX_ESCAPE_UNICODE_TRUNCATION;
+//        static MessagesTestBundle()
+//        {
+//            // register all string ids with NLS class and initialize static string
+//            // values
+//            NLS.InitializeMessages(BUNDLE_NAME, typeof(MessagesTestBundle));
+//        }
 
-        // this message is missing from the properties file
-        public static String Q0005E_MESSAGE_NOT_IN_BUNDLE;
-    }
-}
+//        // static string must match the strings in the property files.
+//        public static String Q0001E_INVALID_SYNTAX;
+//        public static String Q0004E_INVALID_SYNTAX_ESCAPE_UNICODE_TRUNCATION;
+
+//        // this message is missing from the properties file
+//        public static String Q0005E_MESSAGE_NOT_IN_BUNDLE;
+//    }
+//}
--- src/Lucene.Net.Tests.QueryParser/Flexible/Messages/TestNLS.cs
+++ src/Lucene.Net.Tests.QueryParser/Flexible/Messages/TestNLS.cs
@@ -1,129 +1,132 @@
-﻿using Lucene.Net.Util;
-using NUnit.Framework;
-using System;
-using System.Globalization;
+﻿// LUCENENET specific - Factored out NLS so end users can elect to use .NET localization or not
+// rather than forcing them to use it.
 
-namespace Lucene.Net.QueryParsers.Flexible.Messages
-{
-    /*
-     * Licensed to the Apache Software Foundation (ASF) under one or more
-     * contributor license agreements.  See the NOTICE file distributed with
-     * this work for additional information regarding copyright ownership.
-     * The ASF licenses this file to You under the Apache License, Version 2.0
-     * (the "License"); you may not use this file except in compliance with
-     * the License.  You may obtain a copy of the License at
-     *
-     *     http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
+//using Lucene.Net.Util;
+//using NUnit.Framework;
+//using System;
+//using System.Globalization;
 
-    public class TestNLS : LuceneTestCase
-    {
-        [Test]
-        public void TestMessageLoading()
-        {
-            IMessage invalidSyntax = new Message(
-                MessagesTestBundle.Q0001E_INVALID_SYNTAX, "XXX");
-            /* 
-             * if the default locale is ja, you get ja as a fallback:
-             * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
-             */
-            if (!CultureInfo.CurrentUICulture.Equals(new CultureInfo("ja")))
-                assertEquals("Syntax Error: XXX", invalidSyntax.GetLocalizedMessage(new CultureInfo("en")));
-        }
+//namespace Lucene.Net.QueryParsers.Flexible.Messages
+//{
+//    /*
+//     * Licensed to the Apache Software Foundation (ASF) under one or more
+//     * contributor license agreements.  See the NOTICE file distributed with
+//     * this work for additional information regarding copyright ownership.
+//     * The ASF licenses this file to You under the Apache License, Version 2.0
+//     * (the "License"); you may not use this file except in compliance with
+//     * the License.  You may obtain a copy of the License at
+//     *
+//     *     http://www.apache.org/licenses/LICENSE-2.0
+//     *
+//     * Unless required by applicable law or agreed to in writing, software
+//     * distributed under the License is distributed on an "AS IS" BASIS,
+//     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     * See the License for the specific language governing permissions and
+//     * limitations under the License.
+//     */
 
-        [Test]
-        public void TestMessageLoading_ja()
-        {
-            IMessage invalidSyntax = new Message(
-                MessagesTestBundle.Q0001E_INVALID_SYNTAX, "XXX");
-            assertEquals("構文エラー: XXX", invalidSyntax
-                .GetLocalizedMessage(new CultureInfo("ja")));
-        }
+//    public class TestNLS : LuceneTestCase
+//    {
+//        [Test]
+//        public void TestMessageLoading()
+//        {
+//            IMessage invalidSyntax = new Message(
+//                MessagesTestBundle.Q0001E_INVALID_SYNTAX, "XXX");
+//            /* 
+//             * if the default locale is ja, you get ja as a fallback:
+//             * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
+//             */
+//            if (!CultureInfo.CurrentUICulture.Equals(new CultureInfo("ja")))
+//                assertEquals("Syntax Error: XXX", invalidSyntax.GetLocalizedMessage(new CultureInfo("en")));
+//        }
 
-        [Test]
-        public void TestNLSLoading()
-        {
-            String message = NLS
-                .GetLocalizedMessage(MessagesTestBundle.Q0004E_INVALID_SYNTAX_ESCAPE_UNICODE_TRUNCATION, new CultureInfo("en"));
-            /* 
-             * if the default locale is ja, you get ja as a fallback:
-             * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
-             */
-            if (!CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.Equals("ja", StringComparison.OrdinalIgnoreCase))
-                assertEquals("Truncated unicode escape sequence.", message);
+//        [Test]
+//        public void TestMessageLoading_ja()
+//        {
+//            IMessage invalidSyntax = new Message(
+//                MessagesTestBundle.Q0001E_INVALID_SYNTAX, "XXX");
+//            assertEquals("構文エラー: XXX", invalidSyntax
+//                .GetLocalizedMessage(new CultureInfo("ja")));
+//        }
 
-            message = NLS.GetLocalizedMessage(MessagesTestBundle.Q0001E_INVALID_SYNTAX, new CultureInfo("en"),
-                "XXX");
-            /* 
-             * if the default locale is ja, you get ja as a fallback:
-             * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
-             */
-            if (!CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.Equals("ja", StringComparison.OrdinalIgnoreCase))
-                assertEquals("Syntax Error: XXX", message);
-        }
+//        [Test]
+//        public void TestNLSLoading()
+//        {
+//            String message = NLS
+//                .GetLocalizedMessage(MessagesTestBundle.Q0004E_INVALID_SYNTAX_ESCAPE_UNICODE_TRUNCATION, new CultureInfo("en"));
+//            /* 
+//             * if the default locale is ja, you get ja as a fallback:
+//             * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
+//             */
+//            if (!CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.Equals("ja", StringComparison.OrdinalIgnoreCase))
+//                assertEquals("Truncated unicode escape sequence.", message);
 
-        [Test]
-        public void TestNLSLoading_ja()
-        {
-            String message = NLS.GetLocalizedMessage(
-                MessagesTestBundle.Q0004E_INVALID_SYNTAX_ESCAPE_UNICODE_TRUNCATION,
-                new CultureInfo("ja-JP"));
-            assertEquals("切り捨てられたユニコード・エスケープ・シーケンス。", message);
+//            message = NLS.GetLocalizedMessage(MessagesTestBundle.Q0001E_INVALID_SYNTAX, new CultureInfo("en"),
+//                "XXX");
+//            /* 
+//             * if the default locale is ja, you get ja as a fallback:
+//             * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
+//             */
+//            if (!CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.Equals("ja", StringComparison.OrdinalIgnoreCase))
+//                assertEquals("Syntax Error: XXX", message);
+//        }
 
-            message = NLS.GetLocalizedMessage(MessagesTestBundle.Q0001E_INVALID_SYNTAX,
-                new CultureInfo("ja-JP"), "XXX");
-            assertEquals("構文エラー: XXX", message);
-        }
+//        [Test]
+//        public void TestNLSLoading_ja()
+//        {
+//            String message = NLS.GetLocalizedMessage(
+//                MessagesTestBundle.Q0004E_INVALID_SYNTAX_ESCAPE_UNICODE_TRUNCATION,
+//                new CultureInfo("ja-JP"));
+//            assertEquals("切り捨てられたユニコード・エスケープ・シーケンス。", message);
 
-        [Test]
-        public void TestNLSLoading_xx_XX()
-        {
-            try
-            {
-                CultureInfo locale = new CultureInfo("xx-XX");
-                String message = NLS.GetLocalizedMessage(
-                    MessagesTestBundle.Q0004E_INVALID_SYNTAX_ESCAPE_UNICODE_TRUNCATION,
-                    locale);
-                /* 
-                 * if the default locale is ja, you get ja as a fallback:
-                 * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
-                 */
-                if (!CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.Equals("ja", StringComparison.OrdinalIgnoreCase))
-                    assertEquals("Truncated unicode escape sequence.", message);
+//            message = NLS.GetLocalizedMessage(MessagesTestBundle.Q0001E_INVALID_SYNTAX,
+//                new CultureInfo("ja-JP"), "XXX");
+//            assertEquals("構文エラー: XXX", message);
+//        }
 
-                message = NLS.GetLocalizedMessage(MessagesTestBundle.Q0001E_INVALID_SYNTAX,
-                    locale, "XXX");
-                /* 
-                 * if the default locale is ja, you get ja as a fallback:
-                 * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
-                 */
-                if (!CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.Equals("ja", StringComparison.OrdinalIgnoreCase))
-                    assertEquals("Syntax Error: XXX", message);
-            }
-            catch (CultureNotFoundException)
-            {
-                // ignore
+//        [Test]
+//        public void TestNLSLoading_xx_XX()
+//        {
+//            try
+//            {
+//                CultureInfo locale = new CultureInfo("xx-XX");
+//                String message = NLS.GetLocalizedMessage(
+//                    MessagesTestBundle.Q0004E_INVALID_SYNTAX_ESCAPE_UNICODE_TRUNCATION,
+//                    locale);
+//                /* 
+//                 * if the default locale is ja, you get ja as a fallback:
+//                 * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
+//                 */
+//                if (!CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.Equals("ja", StringComparison.OrdinalIgnoreCase))
+//                    assertEquals("Truncated unicode escape sequence.", message);
 
-                // LUCENENET NOTE: .NET Core throws a CultureNotFoundException in this case.
-                // There doesn't seem to be a reasonable way to test this as a result.
-            }
-        }
+//                message = NLS.GetLocalizedMessage(MessagesTestBundle.Q0001E_INVALID_SYNTAX,
+//                    locale, "XXX");
+//                /* 
+//                 * if the default locale is ja, you get ja as a fallback:
+//                 * see ResourceBundle.html#getBundle(java.lang.String, java.util.Locale, java.lang.ClassLoader)
+//                 */
+//                if (!CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.Equals("ja", StringComparison.OrdinalIgnoreCase))
+//                    assertEquals("Syntax Error: XXX", message);
+//            }
+//            catch (CultureNotFoundException)
+//            {
+//                // ignore
 
-        [Test]
-        public void TestMissingMessage()
-        {
-            CultureInfo locale = new CultureInfo("en");
-            String message = NLS.GetLocalizedMessage(
-                MessagesTestBundle.Q0005E_MESSAGE_NOT_IN_BUNDLE, locale);
+//                // LUCENENET NOTE: .NET Core throws a CultureNotFoundException in this case.
+//                // There doesn't seem to be a reasonable way to test this as a result.
+//            }
+//        }
 
-            assertEquals("Message with key:Q0005E_MESSAGE_NOT_IN_BUNDLE and locale: "
-                + locale.toString() + " not found.", message);
-        }
-    }
-}
+//        [Test]
+//        public void TestMissingMessage()
+//        {
+//            CultureInfo locale = new CultureInfo("en");
+//            String message = NLS.GetLocalizedMessage(
+//                MessagesTestBundle.Q0005E_MESSAGE_NOT_IN_BUNDLE, locale);
+
+//            assertEquals("Message with key:Q0005E_MESSAGE_NOT_IN_BUNDLE and locale: "
+//                + locale.toString() + " not found.", message);
+//        }
+//    }
+//}
