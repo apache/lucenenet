0219868b850efee067074279b112f3f3f85b25ad
Ron Clabo <ron.git@GiftOasis.com>
2021-08-18 11:03:10 -0400

    Minor tweak to fix a change I didn't mean to commit

--- src/Lucene.Net.Tests/Search/TestControlledRealTimeReopenThread.cs
+++ src/Lucene.Net.Tests/Search/TestControlledRealTimeReopenThread.cs
@@ -657,7 +657,7 @@ namespace Lucene.Net.Search
             Directory dir = new NRTCachingDirectory(NewFSDirectory(CreateTempDir("nrt")), 5, 128);
             IndexWriterConfig config = new IndexWriterConfig(
 #pragma warning disable 612, 618
-                Version.LUCENE_48,
+                Version.LUCENE_46,
 #pragma warning restore 612, 618
                 new MockAnalyzer(Random));
             config.SetIndexDeletionPolicy(sdp);
