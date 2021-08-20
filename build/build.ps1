774bc7566969296a65ac2e43edb68fc71ff08fcc
Shad Storhaug <shad@shadstorhaug.com>
2021-08-20 10:38:56 +0700

    SWEEP: Changed test framework for .NET Standard 2.0 from netcoreapp2.1 to net461

--- build/build.ps1
+++ build/build.ps1
@@ -49,7 +49,7 @@ properties {
     [int]$maximumParalellJobs = 8
     
     #test paramters
-    [string]$frameworks_to_test = "net5.0,netcoreapp3.1,netcoreapp2.1,net48"
+    [string]$frameworks_to_test = "net5.0,netcoreapp3.1,net48,net461"
     [string]$where = ""
 }
 
@@ -295,13 +295,6 @@ task Test -depends InstallSDK, UpdateLocalSDKVersion, Restore -description "This
             
             # Special case - OpenNLP.NET only supports .NET Framework
             if ($testName.Contains("Tests.Analysis.OpenNLP") -and (!$framework.StartsWith("net4"))) {
-                $totalProjects--
-                $remainingProjects--
-                continue
-            }
-
-            # Special case - Morfologik doesn't support .NET Standard 1.x
-            if ($testName.Contains("Tests.Analysis.Morfologik") -and ($framework.StartsWith("netcoreapp1."))) {
                 $totalProjects--
                 $remainingProjects--
                 continue
