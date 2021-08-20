1c1f5326642f29e1799f4e39e94862814849a95b
Shad Storhaug <shad@shadstorhaug.com>
2021-08-21 05:58:43 +0700

    Revert "Lucene.Net.Tests.Search.TestControlledRealTimeReopenThread: Added Ignore and AwaitsFix attributes to TestStraightForwardDemonstration() and TestMultithreadedWaitForGeneration(), since they are failing on macOS"
    This reverts commit 96b9b7b302f293e605c80cf3eb1f90e5787af690.

--- src/Lucene.Net.Tests/Search/TestControlledRealTimeReopenThread.cs
+++ src/Lucene.Net.Tests/Search/TestControlledRealTimeReopenThread.cs
@@ -753,8 +753,6 @@ namespace Lucene.Net.Search
         /// </summary>
         // LUCENENET specific - An extra test to demonstrate use of ControlledRealTimeReopen.
         [Test]
-        [Ignore("LUCENENET TODO: this test fails on macOS")]
-        [AwaitsFix(BugUrl = "https://github.com/apache/lucenenet/issues/513")] // LUCENENET TODO: this test fails on macOS
         public void TestStraightForwardDemonstration()
         {
 
@@ -875,8 +873,6 @@ namespace Lucene.Net.Search
         /// </summary>
         // LUCENENET specific - An extra test to test multithreaded use of ControlledRealTimeReopen.
         [Test]
-        [Ignore("LUCENENET TODO: this test fails on macOS")]
-        [AwaitsFix(BugUrl = "https://github.com/apache/lucenenet/issues/513")] // LUCENENET TODO: this test fails on macOS
         public void TestMultithreadedWaitForGeneration()
         {
             Thread CreateWorker(int threadNum, ControlledRealTimeReopenThread<IndexSearcher> controlledReopen, long generation,
