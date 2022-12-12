e60335087979e5d6afe7ac9d5b261acc326dd06a
chen__h <chenhh0409@126.com>
2022-12-12 23:32:59 +0800

    Respect ignoreCase flag in CommonGramsFilterFactory

--- src/Lucene.Net.Analysis.Common/Analysis/CommonGrams/CommonGramsFilterFactory.cs
+++ src/Lucene.Net.Analysis.Common/Analysis/CommonGrams/CommonGramsFilterFactory.cs
@@ -36,7 +36,7 @@ namespace Lucene.Net.Analysis.CommonGrams
     /// </summary>
     public class CommonGramsFilterFactory : TokenFilterFactory, IResourceLoaderAware
     {
-        // TODO: shared base class for Stop/Keep/CommonGrams? 
+        // TODO: shared base class for Stop/Keep/CommonGrams?
         private CharArraySet commonWords;
         private readonly string commonWordFiles;
         private readonly string format;
@@ -71,7 +71,7 @@ namespace Lucene.Net.Analysis.CommonGrams
             }
             else
             {
-                commonWords = StopAnalyzer.ENGLISH_STOP_WORDS_SET;
+                commonWords = new CharArraySet(m_luceneMatchVersion,StopAnalyzer.ENGLISH_STOP_WORDS_SET, ignoreCase);
             }
         }
 
