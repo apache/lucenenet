87add0272d1da1eaee68ca95e37841e90ec07346
Laimonas Simutis <laimis@gmail.com>
2023-04-13 09:38:17 -0700

    add ignore attributes (#823)

--- src/Lucene.Net/Util/OpenBitSetDISI.cs
+++ src/Lucene.Net/Util/OpenBitSetDISI.cs
@@ -1,21 +1,22 @@
 namespace Lucene.Net.Util
 {
+    using System.Diagnostics.CodeAnalysis;
     /*
-     * Licensed to the Apache Software Foundation (ASF) under one or more
-     * contributor license agreements.  See the NOTICE file distributed with
-     * this work for additional information regarding copyright ownership.
-     * The ASF licenses this file to You under the Apache License, Version 2.0
-     * (the "License"); you may not use this file except in compliance with
-     * the License.  You may obtain a copy of the License at
-     *
-     *     http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
     using DocIdSetIterator = Lucene.Net.Search.DocIdSetIterator;
 
@@ -31,6 +32,8 @@ namespace Lucene.Net.Util
         /// Also give a maximum size one larger than the largest doc id for which a
         /// bit may ever be set on this <see cref="OpenBitSetDISI"/>.
         /// </summary>
+        [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "This is a SonarCloud issue")]
+        [SuppressMessage("CodeQuality", "S1699:Constructors should only call non-overridable methods", Justification = "This class will get removed in later versions of Lucene, see LUCENE-6010")]
         public OpenBitSetDISI(DocIdSetIterator disi, int maxSize)
             : base(maxSize)
         {
