a9c8388aa9fc696ec14f1aa88daa0512f804be32
Shad Storhaug <shad@shadstorhaug.com>
2023-05-14 23:02:52 +0700

    Lucene.Net.Store.RateLimitedIndexOutput: Allow double-dispose calls and guard against usage after Dispose(). See #265.

--- src/Lucene.Net/Store/RateLimitedIndexOutput.cs
+++ src/Lucene.Net/Store/RateLimitedIndexOutput.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace Lucene.Net.Store
 {
@@ -30,6 +31,7 @@ namespace Lucene.Net.Store
         private readonly IndexOutput @delegate;
         private readonly BufferedIndexOutput bufferedDelegate;
         private readonly RateLimiter rateLimiter;
+        private int disposed = 0; // LUCENENET specific - allow double-dispose
 
         internal RateLimitedIndexOutput(RateLimiter rateLimiter, IndexOutput @delegate)
         {
@@ -62,15 +64,21 @@ namespace Lucene.Net.Store
 
         public override long Length
         {
-            get => @delegate.Length;
+            get
+            {
+                EnsureOpen(); // LUCENENET specific - ensure we can't be abused after dispose
+                return @delegate.Length;
+            }
             set
             {
+                // LUCENENET: Intentionally blank
             }
         }
 
         [Obsolete("(4.1) this method will be removed in Lucene 5.0")]
         public override void Seek(long pos)
         {
+            EnsureOpen(); // LUCENENET specific - ensure we can't be abused after dispose
             Flush();
             @delegate.Seek(pos);
         }
@@ -90,6 +98,8 @@ namespace Lucene.Net.Store
 
         protected override void Dispose(bool disposing)
         {
+            if (0 != Interlocked.CompareExchange(ref this.disposed, 1, 0)) return; // LUCENENET specific - allow double-dispose
+
             if (disposing)
             {
                 try
@@ -100,6 +110,18 @@ namespace Lucene.Net.Store
                 {
                     @delegate.Dispose();
                 }
+            }
+        }
+
+        // LUCENENET specific - ensure we can't be abused after dispose
+        private bool IsOpen => Interlocked.CompareExchange(ref this.disposed, 0, 0) == 0 ? true : false;
+
+        // LUCENENET specific - ensure we can't be abused after dispose
+        private void EnsureOpen()
+        {
+            if (!IsOpen)
+            {
+                throw AlreadyClosedException.Create(this.GetType().FullName, "this IndexOutput is disposed.");
             }
         }
     }
