415dfa390f78c4404474bc52e09933809aad94e9
Shad Storhaug <shad@shadstorhaug.com>
2023-10-19 20:10:46 +0700

    Lucene.Net.Support.RectangularArrays: Fixed encoding issue that was preventing SonarCloud from analyzing the file (#880)

--- src/Lucene.Net/Support/RectangularArrays.cs
+++ src/Lucene.Net/Support/RectangularArrays.cs
@@ -1,9 +1,9 @@
-﻿//----------------------------------------------------------------------------------------
-//	Copyright � 2007 - 2013 Tangible Software Solutions Inc.
-//	this class can be used by anyone provided that the copyright notice remains intact.
+//----------------------------------------------------------------------------------------
+//  Copyright © 2007 - 2013 Tangible Software Solutions Inc.
+//  this class can be used by anyone provided that the copyright notice remains intact.
 //
-//	this class provides the logic to simulate Java rectangular arrays, which are jagged
-//	arrays with inner arrays of the same length. A size of -1 indicates unknown length.
+//  this class provides the logic to simulate Java rectangular arrays, which are jagged
+//  arrays with inner arrays of the same length. A size of -1 indicates unknown length.
 //----------------------------------------------------------------------------------------
 
 namespace Lucene.Net.Support
