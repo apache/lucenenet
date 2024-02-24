e564fe64a720e17b04382818c5aafba12efbf855
Paul Irwin <paulirwin@gmail.com>
2024-02-24 16:29:51 -0700

    Fix seealso cref to CheckHits.CheckNoMatchExplanations

--- src/Lucene.Net.Tests/Search/TestComplexExplanationsOfNonMatches.cs
+++ src/Lucene.Net.Tests/Search/TestComplexExplanationsOfNonMatches.cs
@@ -26,7 +26,7 @@ namespace Lucene.Net.Search
         /// <summary>
         /// Overrides superclass to ignore matches and focus on non-matches
         /// </summary>
-        /// <seealso cref="CheckHits.CheckNoMatchExplanations" />
+        /// <seealso cref="CheckHits.CheckNoMatchExplanations(Query, string, IndexSearcher, int[])" />
         public override void Qtest(Query q, int[] expDocNrs)
         {
             CheckHits.CheckNoMatchExplanations(q, FIELD, searcher, expDocNrs);
