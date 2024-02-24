e00ad3c35e9170eb6fe90ad4e0220babcccb5cb7
Paul Irwin <paulirwin@gmail.com>
2024-02-24 16:42:12 -0700

    Fix XML doc comment paragraph tag in TestFieldCacheRangeFilter

--- src/Lucene.Net.Tests/Search/TestFieldCacheRangeFilter.cs
+++ src/Lucene.Net.Tests/Search/TestFieldCacheRangeFilter.cs
@@ -36,7 +36,7 @@ namespace Lucene.Net.Search
     /// <summary>
     /// A basic 'positive' Unit test class for the FieldCacheRangeFilter class.
     ///
-    /// <p>
+    /// <para />
     /// NOTE: at the moment, this class only tests for 'positive' results,
     /// it does not verify the results to ensure there are no 'false positives',
     /// nor does it adequately test 'negative' results.  It also does not test
