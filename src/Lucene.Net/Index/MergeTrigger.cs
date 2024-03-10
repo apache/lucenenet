cd8e057e08b8eca454711687b4cf96a8c7476910
Paul Irwin <paulirwin@gmail.com>
2024-03-10 10:49:39 -0600

    Replace int.MinValue with MergeTrigger.NONE
    The original Java code uses null, but that causes problems if you make
    the enum nullable. This is a cleaner solution although it is an API
    mismatch from the Java semantics.

--- src/Lucene.Net/Index/MergeTrigger.cs
+++ src/Lucene.Net/Index/MergeTrigger.cs
@@ -25,6 +25,11 @@ namespace Lucene.Net.Index
     public enum MergeTrigger
     {
         /// <summary>
+        /// LUCENENET-specific value to be used instead of null.
+        /// </summary>
+        NONE = -1,
+
+        /// <summary>
         /// Merge was triggered by a segment flush.
         /// </summary>
         SEGMENT_FLUSH,
