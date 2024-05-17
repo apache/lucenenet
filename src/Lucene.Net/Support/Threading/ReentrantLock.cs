3abbb73838683d15a4c2b0e87934fcfa81eee3d4
Shad Storhaug <shad@shadstorhaug.com>
2024-05-18 01:31:55 +0700

    Lucene.Net.Index.DocumentsWriterPerThreadPool: Removed MinContendedThreadState() method because it has no callers. This simplifies the design of ReentrantLock, since we don't need to artificially keep track of "queued threads".

--- src/Lucene.Net/Support/Threading/ReentrantLock.cs
+++ src/Lucene.Net/Support/Threading/ReentrantLock.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading;
-
-namespace Lucene.Net.Support.Threading
+﻿namespace Lucene.Net.Support.Threading
 {
     /*
      * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -24,20 +22,10 @@ namespace Lucene.Net.Support.Threading
         // .NET Port: lock object used to emulate ReentrantLock
         private readonly object _lock = new object();
 
-        // .NET Port: Estimated monitor queue length
-        private int _queueLength = 0;
-
         // .NET Port: mimic ReentrantLock -- Monitor is re-entrant
         public void Lock()
         {
-            // note about queue length: in java's ReentrantLock, getQueueLength() returns the number
-            // of threads waiting on entering the lock. So here, we're incrementing the count before trying to enter,
-            // meaning that until enter has completed the thread is waiting so the queue is incremented. Once
-            // we enter the lock, then we immediately decrement it because that thread is no longer in the queue.
-            // Due to race conditions, the queue length is an estimate only.
-            Interlocked.Increment(ref _queueLength);
             UninterruptableMonitor.Enter(_lock);
-            Interlocked.Decrement(ref _queueLength);
         }
 
         // .NET Port: mimic ReentrantLock -- Monitor is re-entrant
@@ -48,7 +36,6 @@ namespace Lucene.Net.Support.Threading
 
         public bool TryLock()
         {
-            Interlocked.Increment(ref _queueLength);
             // NOTE: In Java, the ReentrantLock.tryEnter() method will "barge" to the
             // front of the queue when called, so there is not a chance (or a very small chance)
             // that it will return false. This differs from Monitor.TryEnter() in .NET, which returns
@@ -57,24 +44,8 @@ namespace Lucene.Net.Support.Threading
             // ThreadInterruptedException, but that will be handled by UninterruptableMonitor.
             bool lockTaken = false;
             UninterruptableMonitor.Enter(_lock, ref lockTaken);
-            Interlocked.Decrement(ref _queueLength);
-
             return lockTaken;
         }
-
-        public int QueueLength
-        {
-            get
-            {
-                // hold onto the estimate for the length of this method
-                int estimate = _queueLength;
-
-                // should never be < 0, but just in case, as a negative number doesn't make sense.
-                return estimate <= 0 ? 0 : estimate;
-            }
-        }
-
-        public bool HasQueuedThreads => _queueLength > 0;
 
         public bool IsHeldByCurrentThread => UninterruptableMonitor.IsEntered(_lock);
     }
