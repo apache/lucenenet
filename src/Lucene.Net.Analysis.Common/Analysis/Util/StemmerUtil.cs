ae4a3593805ff1069632e33e5248770eb13b330b
Paul Irwin <paulirwin@gmail.com>
2025-03-16 21:13:38 -0600

    Optimize StemmerUtil for ReadOnlySpan<char>, #1140

--- src/Lucene.Net.Analysis.Common/Analysis/Util/StemmerUtil.cs
+++ src/Lucene.Net.Analysis.Common/Analysis/Util/StemmerUtil.cs
@@ -1,8 +1,6 @@
 // Lucene version compatibility level 4.8.1
 using Lucene.Net.Diagnostics;
-using Lucene.Net.Support;
 using System;
-using System.Diagnostics;
 
 namespace Lucene.Net.Analysis.Util
 {
@@ -37,21 +35,34 @@ namespace Lucene.Net.Analysis.Util
         /// <param name="len"> length of input buffer </param>
         /// <param name="prefix"> Prefix string to test </param>
         /// <returns> <c>true</c> if <paramref name="s"/> starts with <paramref name="prefix"/> </returns>
-        public static bool StartsWith(char[] s, int len, string prefix)
+        /// <remarks>
+        /// LUCENENET NOTE: This method has been converted to use <see cref="ReadOnlySpan{T}"/>.
+        /// </remarks>
+        public static bool StartsWith(ReadOnlySpan<char> s, int len, string prefix)
+        {
+            return StartsWith(s, len, prefix.AsSpan());
+        }
+
+        /// <summary>
+        /// Returns true if the character array starts with the prefix.
+        /// </summary>
+        /// <param name="s"> Input Buffer </param>
+        /// <param name="len"> length of input buffer </param>
+        /// <param name="prefix"> Prefix string to test </param>
+        /// <returns> <c>true</c> if <paramref name="s"/> starts with <paramref name="prefix"/> </returns>
+        /// <remarks>
+        /// LUCENENET NOTE: This method has been converted to use <see cref="ReadOnlySpan{T}"/>.
+        /// </remarks>
+        public static bool StartsWith(ReadOnlySpan<char> s, int len, ReadOnlySpan<char> prefix)
         {
             int prefixLen = prefix.Length;
             if (prefixLen > len)
             {
                 return false;
             }
-            for (int i = 0; i < prefixLen; i++)
-            {
-                if (s[i] != prefix[i])
-                {
-                    return false;
-                }
-            }
-            return true;
+
+            // LUCENENET: use more efficient implementation in MemoryExtensions
+            return s.StartsWith(prefix);
         }
 
         /// <summary>
@@ -61,22 +72,12 @@ namespace Lucene.Net.Analysis.Util
         /// <param name="len"> length of input buffer </param>
         /// <param name="suffix"> Suffix string to test </param>
         /// <returns> <c>true</c> if <paramref name="s"/> ends with <paramref name="suffix"/> </returns>
-        public static bool EndsWith(char[] s, int len, string suffix)
+        /// <remarks>
+        /// LUCENENET NOTE: This method has been converted to use <see cref="ReadOnlySpan{T}"/>.
+        /// </remarks>
+        public static bool EndsWith(ReadOnlySpan<char> s, int len, string suffix)
         {
-            int suffixLen = suffix.Length;
-            if (suffixLen > len)
-            {
-                return false;
-            }
-            for (int i = suffixLen - 1; i >= 0; i--)
-            {
-                if (s[len - (suffixLen - i)] != suffix[i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return EndsWith(s, len, suffix.AsSpan());
         }
 
         /// <summary>
@@ -86,23 +87,22 @@ namespace Lucene.Net.Analysis.Util
         /// <param name="len"> length of input buffer </param>
         /// <param name="suffix"> Suffix string to test </param>
         /// <returns> <c>true</c> if <paramref name="s"/> ends with <paramref name="suffix"/> </returns>
-        public static bool EndsWith(char[] s, int len, char[] suffix)
+        /// <remarks>
+        /// LUCENENET NOTE: This method has been converted to use <see cref="ReadOnlySpan{T}"/>.
+        /// </remarks>
+        public static bool EndsWith(ReadOnlySpan<char> s, int len, ReadOnlySpan<char> suffix)
         {
             int suffixLen = suffix.Length;
             if (suffixLen > len)
             {
                 return false;
             }
-            for (int i = suffixLen - 1; i >= 0; i--)
-            {
-                if (s[len - (suffixLen - i)] != suffix[i])
-                {
-                    return false;
-                }
-            }
 
-            return true;
+            // LUCENENET: use more efficient implementation in MemoryExtensions
+            return s.Slice(0, len).EndsWith(suffix);
         }
+
+        // LUCENENET NOTE: char[] overload of EndsWith removed because the ReadOnlySpan<char> overload can be used instead
 
         /// <summary>
         /// Delete a character in-place
@@ -111,12 +111,16 @@ namespace Lucene.Net.Analysis.Util
         /// <param name="pos"> Position of character to delete </param>
         /// <param name="len"> length of input buffer </param>
         /// <returns> length of input buffer after deletion </returns>
-        public static int Delete(char[] s, int pos, int len)
+        /// <remarks>
+        /// LUCENENET NOTE: This method has been converted to use <see cref="Span{T}"/>.
+        /// </remarks>
+        public static int Delete(Span<char> s, int pos, int len)
         {
             if (Debugging.AssertsEnabled) Debugging.Assert(pos < len);
             if (pos < len - 1) // don't arraycopy if asked to delete last character
             {
-                Arrays.Copy(s, pos + 1, s, pos, len - pos - 1);
+                // Arrays.Copy(s, pos + 1, s, pos, len - pos - 1);
+                s.Slice(pos + 1, len - pos - 1).CopyTo(s.Slice(pos, len - pos - 1));
             }
             return len - 1;
         }
@@ -129,12 +133,16 @@ namespace Lucene.Net.Analysis.Util
         /// <param name="len"> Length of input buffer </param>
         /// <param name="nChars"> number of characters to delete </param>
         /// <returns> length of input buffer after deletion </returns>
-        public static int DeleteN(char[] s, int pos, int len, int nChars)
+        /// <remarks>
+        /// LUCENENET NOTE: This method has been converted to use <see cref="Span{T}"/>.
+        /// </remarks>
+        public static int DeleteN(Span<char> s, int pos, int len, int nChars)
         {
             if (Debugging.AssertsEnabled) Debugging.Assert(pos + nChars <= len);
             if (pos + nChars < len) // don't arraycopy if asked to delete the last characters
             {
-                Arrays.Copy(s, pos + nChars, s, pos, len - pos - nChars);
+                // Arrays.Copy(s, pos + nChars, s, pos, len - pos - nChars);
+                s.Slice(pos + nChars, len - pos - nChars).CopyTo(s.Slice(pos, len - pos - nChars));
             }
             return len - nChars;
         }
