52c36c04998f65ef2ce7950c75433fbf8e1305e8
Shad Storhaug <shad@shadstorhaug.com>
2025-06-30 20:10:13 +0700

    SWEEP: Lucene.Net.Analysis.Phonetic: Fixed whitespace formatting to match .editorconfig

--- src/Lucene.Net.Analysis.Phonetic/DoubleMetaphoneFilter.cs
+++ src/Lucene.Net.Analysis.Phonetic/DoubleMetaphoneFilter.cs
@@ -54,7 +54,7 @@ namespace Lucene.Net.Analysis.Phonetic
 
         public override bool IncrementToken()
         {
-            for (;;)
+            for (; ; )
             {
                 if (remainingTokens.TryDequeue(out State first))
                 {
