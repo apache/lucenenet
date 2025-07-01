881e72c213c0939c3c3f19fbd69c3961f0db9790
Shad Storhaug <shad@shadstorhaug.com>
2025-07-01 10:51:26 +0700

    SWEEP: Lucene.Net.Tests.Suggest: Fixed whitespace formatting to match .editorconfig

--- src/Lucene.Net.Tests.Suggest/Suggest/Analyzing/FuzzySuggesterTest.cs
+++ src/Lucene.Net.Tests.Suggest/Suggest/Analyzing/FuzzySuggesterTest.cs
@@ -1313,7 +1313,7 @@ namespace Lucene.Net.Search.Suggest.Analyzing
                 else
                 {
                     int c = CHARSEQUENCE_COMPARER.Compare(a.Key, b.Key);
-                    if (Debugging.AssertsEnabled) Debugging.Assert(c != 0,"term={0}", a.Key);
+                    if (Debugging.AssertsEnabled) Debugging.Assert(c != 0, "term={0}", a.Key);
                     return c;
                 }
             }
